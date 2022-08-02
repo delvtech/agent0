4fdebed7be2ab1f0099a9d2affb5820366dfe9a6
mihai6 <mihai@element.fi>
2022-08-02 17:00:45 -0400

    last used parameters

--- liquidity-mining.py
+++ liquidity-mining.py
@@ -25,13 +25,13 @@ from PricingModels import Element_Pricing_Model, Market, YieldsSpacev2_Pricing_m
 
 # %%
 token_price = 3
-token_supply = 10*1e7
+token_supply = 1*1e7
 market_value = token_price * token_supply
 growth_rate = 0.5 # 50% annualized growth rate
-funding_need = market_value # 6 million per year ($200k/yr for 30 staff or $100k/yr for 60 staff)
+funding_need = market_value/1 # 6 million per year ($200k/yr for 30 staff or $100k/yr for 60 staff)
 
 columns = ['token_price', 'token_supply', 'market_value', 'growth_rate', 'new_tokens', 'funding_acquired', 'old_token_price', 'new_token_price']
-df = pd.DataFrame(data=[[token_price, token_supply, market_value, growth_rate, token_supply, market_value, 0, 0]]
+df = pd.DataFrame(data=[[token_price, token_supply, market_value, growth_rate, np.nan, np.nan, 0, 0]]
     ,columns=columns)
 for t in range(1,11):
     growth_rate = growth_rate*0.8
@@ -42,10 +42,10 @@ for t in range(1,11):
     token_price = old_token_price
     # print('{} tokens needed, price from {}→{} diff={}'.format(new_tokens,token_price,market_value/(token_supply+new_tokens),funding_need - funding_acquired))
     while funding_acquired - funding_need < -1:
-        print('{} tokens needed, price from {}→{} diff={}'.format(new_tokens,token_price,market_value/(token_supply+new_tokens),funding_acquired - funding_need))
         token_price = market_value/(token_supply+new_tokens)
         new_tokens = funding_need/token_price # update tokens being printed
         funding_acquired = token_price * new_tokens
+        print('{} tokens needed, price from {}→{} diff={}'.format(new_tokens,old_token_price,market_value/(token_supply+new_tokens),funding_acquired - funding_need))
     token_supply += new_tokens
     df = pd.concat([df,pd.DataFrame(data=[[token_price, token_supply, market_value, growth_rate, new_tokens, funding_acquired, old_token_price, token_price]], columns=columns, index=[t])])
 display(df.style.format({'token_supply': '{:,.0f}', 'market_value': '{:,.0f}', 'growth_rate': '{:,.0%}', 'new_tokens': '{:,.0f}', 'funding_acquired': '{:,.0f}'}))
@@ -55,18 +55,20 @@ numPlots = 2
 fig, ax = plt.subplots(ncols=1, nrows=numPlots,gridspec_kw = {'wspace':0, 'hspace':0.1, 'height_ratios':np.ones(numPlots)}, sharex=True)
 fig.patch.set_facecolor('white')   # set fig background color to white
 
-currentPlot = 1
-df.plot(use_index=True, y='token_price', figsize=(10,5*numPlots), ax=ax[currentPlot], title='... that causes number go down')
-ax[currentPlot].grid(visible=True,linestyle='--', linewidth='1', color='grey',which='both', alpha=0.5)
-
 currentPlot = 0
 df.plot(use_index=True, y=['funding_acquired','new_tokens'], figsize=(10,5*numPlots), ax=ax[currentPlot]\
     , title='Have to print more to fund same amount of ${:,.0f} million...'.format(funding_need/1e6))
+ax[currentPlot].grid(visible=True,linestyle='--', linewidth='1', color='grey',which='both', alpha=0.5)
+# display(token_price)
+# display(np.ceil(token_price))
+# display(np.arange(0,np.ceil(token_price)+1,1))
+ax[currentPlot].set_yticks(np.arange(0,np.ceil(new_tokens/1e7)*1e7+1e7,1e7))
+
+currentPlot = 1
+df.plot(use_index=True, y='token_price', figsize=(10,5*numPlots), ax=ax[currentPlot], title='... that causes number go down')
 ax[currentPlot].grid(visible=True,linestyle='--', linewidth='1', color='grey',which='both', alpha=0.5)
 
 #for final plot
 ax[currentPlot].set_xlabel('Time (years)')
 plt.xticks(df.index)
 plt.show()
-
-# %%
