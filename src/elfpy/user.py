69338f8cc947189de11859ed83554c1228e72e71
wakamex <mihai@element.fi>
2022-10-20 23:36:58 -0400

    remove autolint before lint

--- src/elfpy/user.py
+++ src/elfpy/user.py
@@ -5,6 +5,7 @@ Implements abstract classes that control user behavior
 import numpy as np
 from scipy.stats import binomtest
 
+
 class User:
     """
     Implements abstract classes that control user behavior
@@ -19,16 +20,22 @@ class User:
         return rng.integers(low=0, high=2)  # 0 or 1
 
     @staticmethod
-    def stochastic_direction(pool_apy,pool_apy_target_range,days_trades,pool_apy_target_range_convergence_speed,rng,
-        run_trade_number,verbose=False):
+    def stochastic_direction(
+        pool_apy,
+        pool_apy_target_range,
+        days_trades,
+        pool_apy_target_range_convergence_speed,
+        rng,
+        run_trade_number,
+        verbose=False,
+    ):
         """Picks p-value-weighted direction, cutting off tails"""
         # pylint: disable=too-many-arguments
         btest = []
         expected_proportion = 0
         streak_luck = 0
         apy_distance_in_target_range = np.clip(
-            (pool_apy - pool_apy_target_range[0])
-            / (pool_apy_target_range[1] - pool_apy_target_range[0]),
+            (pool_apy - pool_apy_target_range[0]) / (pool_apy_target_range[1] - pool_apy_target_range[0]),
             0,
             1,
         )
@@ -39,14 +46,10 @@ class User:
             np.abs(apy_distance_in_target_range - 0.5) * 2, 0, 1
         )  # 0 if you're at the midpoint, 1 if you're at the edge
         actual_convergence_strength = (
-            0.5
-            + (pool_apy_target_range_convergence_speed - 0.5)
-            * apy_distance_from_mid_when_in_range
+            0.5 + (pool_apy_target_range_convergence_speed - 0.5) * apy_distance_from_mid_when_in_range
         )  # pool_apy_target_range_convergence_speed at edge or outside, scales to 0 at midpoint
         expected_proportion = (
-            actual_convergence_strength
-            if convergence_direction == 1
-            else 1 - actual_convergence_strength
+            actual_convergence_strength if convergence_direction == 1 else 1 - actual_convergence_strength
         )
         if len(days_trades) > 0:
             btest = binomtest(
@@ -59,7 +62,7 @@ class User:
             token_index = 1 - round(sum(days_trades) / len(days_trades))
             print(
                 "trade"
-                f' {run_trade_number}'
+                f" {run_trade_number}"
                 f" days_trades={days_trades}+{token_index}k={sum(days_trades)}"
                 f" n={len(days_trades)} ratio={sum(days_trades)/len(days_trades)}"
                 f" streak_luck: {streak_luck}"
@@ -67,13 +70,10 @@ class User:
         else:
             if 0 < apy_distance_from_mid_when_in_range < 1:
                 actual_convergence_strength = (
-                    actual_convergence_strength
-                    + (1 - actual_convergence_strength) * streak_luck**1.5
+                    actual_convergence_strength + (1 - actual_convergence_strength) * streak_luck**1.5
                 )  # force convergence when on bad streaks
             token_index = (
-                convergence_direction
-                if rng.random() < actual_convergence_strength
-                else 1 - convergence_direction
+                convergence_direction if rng.random() < actual_convergence_strength else 1 - convergence_direction
             )
-        return (token_index, apy_distance_in_target_range, apy_distance_from_mid_when_in_range,
+        return (token_index,apy_distance_in_target_range,apy_distance_from_mid_when_in_range,
             actual_convergence_strength,expected_proportion,btest)
