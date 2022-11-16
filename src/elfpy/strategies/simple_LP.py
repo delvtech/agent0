078433d875fc75e23fd21edece8e68bf4eb2f336
wakamex <mihai@element.fi>
2022-11-16 17:40:27 -0500

    actually add LPer files 🤦‍

---
+++ src/elfpy/strategies/simple_LP.py
@@ -0,0 +1,29 @@
+from elfpy.strategies.basic import BasicPolicy
+
+
+class Policy(BasicPolicy):
+    """
+    simple LP
+    only has one LP open at a time
+    """
+
+    def __init__(self, market, rng, wallet_address, budget=1000, verbose=False):
+        """call basic policy init then add custom stuff"""
+        budget = 1000
+        super().__init__(market=market, rng=rng, wallet_address=wallet_address, budget=budget, verbose=verbose)
+
+    def action(self):
+        """specify action"""
+        action_list = []
+        amount_to_trade = 100
+        mint_times = list(self.wallet["token_in_wallet"].keys())
+        has_opened_long = len(mint_times) > 0
+        can_open_long = self.get_max_long() >= amount_to_trade
+        if has_opened_long:
+            mint_time = mint_times[0]
+            enough_time_has_passed = self.market.time - mint_time > 0.25
+            if enough_time_has_passed:
+                action_list.append(["close_long", amount_to_trade, mint_time])
+        elif not has_opened_long and can_open_long:
+            action_list.append(["open_long", amount_to_trade])
+        return action_list
