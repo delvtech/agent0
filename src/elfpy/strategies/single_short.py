b82d13f7de2248cf6982357fc207607c9414c17f
Matthew Brown <mattfdbrown@gmail.com>
2022-11-30 17:53:34 -0800

    fix args

--- src/elfpy/strategies/single_short.py
+++ src/elfpy/strategies/single_short.py
@@ -33,7 +33,7 @@ class Policy(BasicPolicy):
         action_list = []
         block_position_list = list(self.wallet.token_in_protocol.values())
         has_opened_short = bool(any((x < -1 for x in block_position_list)))
-        can_open_short = self.get_max_pt_short(self.market.time) >= self.pt_to_short
+        can_open_short = self.get_max_pt_short() >= self.pt_to_short
         if can_open_short and not has_opened_short:
             action_list.append(self.create_agent_action(action_type="open_short", trade_amount=self.pt_to_short))
         return action_list
