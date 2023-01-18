82a815b3f1901917095fc6bc156fae5841f93b8d
Mihai <mcosma@gmail.com>
2023-01-17 20:36:10 -0500

    remove code duplication

--- tests/test_trade.py
+++ tests/test_trade.py
@@ -18,12 +18,70 @@ import elfpy.utils.outputs as output_utils
 class BaseTradeTest(unittest.TestCase):
     """Generic Trade Test class"""
 
-    # pylint: disable=too-many-arguments
-    # because we're testing lots of stuff here!
+    @staticmethod
+    def setup_simulation_entities(config_file, override_dict, agent_policies) -> tuple[Simulator, Market]:
+        """Construct and run the simulator"""
+        # create config object
+        config = config_utils.override_config_variables(
+            config_utils.load_and_parse_config_file(config_file), override_dict
+        )
+        # instantiate rng object
+        rng = np.random.default_rng(config.simulator.random_seed)
+        # run random number generators to get random simulation arguments
+        random_sim_vars = sim_utils.override_random_variables(
+            sim_utils.get_random_variables(config, rng), override_dict
+        )
+        # instantiate the pricing model
+        pricing_model = sim_utils.get_pricing_model(model_name=config.amm.pricing_model_name)
+        # instantiate the market
+        market = sim_utils.get_market(
+            pricing_model,
+            random_sim_vars.target_pool_apr,
+            random_sim_vars.fee_percent,
+            config.simulator.token_duration,
+            random_sim_vars.vault_apr,
+            random_sim_vars.init_share_price,
+        )
+        # instantiate the init_lp agent
+        init_agents = {
+            0: sim_utils.get_init_lp_agent(
+                market,
+                random_sim_vars.target_liquidity,
+                random_sim_vars.target_pool_apr,
+                random_sim_vars.fee_percent,
+            )
+        }
+        # set up simulator with only the init_lp_agent
+        simulator = Simulator(
+            config=config,
+            market=market,
+            agents=init_agents,
+            rng=rng,
+            random_simulation_variables=random_sim_vars,
+        )
+        # initialize the market using the LP agent
+        simulator.collect_and_execute_trades()
+        # get trading agent list
+        for agent_id, policy_name in enumerate(agent_policies):
+            wallet_address = len(init_agents) + agent_id
+            agent = import_module(f"elfpy.policies.{policy_name}").Policy(
+                wallet_address=wallet_address,  # first policy goes to init_lp_agent
+            )
+            agent.log_status_report()
+            simulator.agents.update({agent.wallet.address: agent})
+        return (simulator, market)
+
+    @staticmethod
+    def setup_logging():
+        """Setup test logging levels and handlers"""
+        log_filename = ".logging/test_trades.log"
+        log_level = logging.DEBUG
+        output_utils.setup_logging(log_filename, log_level=log_level)
+
     def run_base_trade_test(
         self,
         agent_policies,
-        config_file="config/example_config.toml",
+        config_file,
         delete_logs=True,
         additional_overrides=None,
         target_liquidity=None,
@@ -43,18 +101,16 @@ class BaseTradeTest(unittest.TestCase):
         }
         if additional_overrides:
             override_dict.update(additional_overrides)
-        simulator = test_utils.setup_simulation_entities(
-            config_file=config_file, override_dict=override_dict, agent_policies=agent_policies
-        )
+        simulator, market = self.setup_simulation_entities(config_file, override_dict, agent_policies)
         if target_pool_apr:  # check that apr is within 0.005 of the target
-            market_apr = simulator.market.rate
+            market_apr = market.rate
             assert np.allclose(market_apr, target_pool_apr, atol=0.005), (
                 f"test_trade.run_base_lp_test: ERROR: {target_pool_apr=} does not equal {market_apr=}"
                 f"with error of {(np.abs(market_apr - target_pool_apr)/target_pool_apr)=}"
             )
         if target_liquidity:  # check that the liquidity is within 0.001 of the target
             # TODO: This will not work with Hyperdrive PM
-            total_liquidity = simulator.market.market_state.share_reserves * simulator.market.market_state.share_price
+            total_liquidity = market.market_state.share_reserves * market.market_state.share_price
             assert np.allclose(total_liquidity, target_liquidity, atol=0.001), (
                 f"test_trade.run_base_lp_test: ERROR: {target_liquidity=} does not equal {total_liquidity=} "
                 f"with error of {(np.abs(total_liquidity - target_liquidity)/target_liquidity)=}."
@@ -71,20 +127,29 @@ class SingleTradeTests(BaseTradeTest):
     """
 
     def __init__(self, *args, **kwargs):
+        self.config_file = "config/example_config.toml"
         super().__init__(*args, **kwargs)
 
     def test_init_only(self):
         """Tests base LP setups"""
-        self.run_base_trade_test(agent_policies=[], target_liquidity=1e6, target_pool_apr=0.05)
+        self.run_base_trade_test(
+            agent_policies=[], config_file=self.config_file, target_liquidity=1e6, target_pool_apr=0.05
+        )
 
     def test_single_long(self):
         """Tests the BaseUser class"""
-        self.run_base_trade_test(agent_policies=["single_long"])
+        self.run_base_trade_test(agent_policies=["single_long"], config_file=self.config_file)
 
     def test_single_short(self):
         """Tests the BaseUser class"""
-        self.run_base_trade_test(agent_policies=["single_short"])
+        self.run_base_trade_test(agent_policies=["single_short"], config_file=self.config_file)
 
     def test_base_lps(self):
         """Tests base LP setups"""
-        self.run_base_trade_test(agent_policies=["single_lp"], target_liquidity=1e6, target_pool_apr=0.05)
+        self.run_base_trade_test(
+            agent_policies=["single_lp"], config_file=self.config_file, target_liquidity=1e6, target_pool_apr=0.05
+        )
+
+
+if __name__ == "__main__":
+    unittest.main()
