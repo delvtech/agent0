5b5d962f05ccc80ad4270f5b2805824af44e5e8c
Matthew Brown <mattfdbrown@gmail.com>
2023-01-19 11:18:58 -0800

    combine dataclasses, use enumerate

--- tests/pricing_models/hyperdrive/hd_test_calc_in_given_out.py
+++ tests/pricing_models/hyperdrive/hd_test_calc_in_given_out.py
@@ -8,50 +8,12 @@ Testing for the calc_in_given_out of the pricing models.
 # pylint: disable=attribute-defined-outside-init
 # pylint: disable=duplicate-code
 
-from dataclasses import dataclass
 import unittest
 import numpy as np
 from elfpy.pricing_models.hyperdrive import HyperdrivePricingModel
 
 from elfpy.types import MarketState, Quantity, StretchedTime, TokenType
-
-
-@dataclass
-class TestCaseCalcInGivenOutSuccess:
-    """Dataclass for calc_in_given_out test cases"""
-
-    out: Quantity
-    market_state: MarketState
-    fee_percent: float
-    days_remaining: float
-    time_stretch_apy: float
-    test_id: int
-
-    __test__ = False  # pytest: don't test this class
-
-
-@dataclass
-class TestResultCalcInGivenOutSuccess:
-    """Dataclass for calc_in_given_out test results"""
-
-    without_fee_or_slippage: float
-    without_fee: float
-    hyperdrive_fee: float
-    hyperdrive_with_fee: float
-
-    __test__ = False  # pytest: don't test this class
-
-
-@dataclass
-class TestCaseCalcInGivenOutFailure:
-    """Dataclass for calc_in_given_out test cases"""
-
-    out: Quantity
-    market_state: MarketState
-    fee_percent: float
-    time_remaining: StretchedTime
-
-    __test__ = False  # pytest: don't test this class
+from tests.pricing_models.test_dataclasses import TestCaseCalcInGivenOutSuccess, TestResultCalcInGivenOutSuccess
 
 
 class TestCalcInGivenOut(unittest.TestCase):
@@ -110,15 +72,14 @@ class TestCalcInGivenOut(unittest.TestCase):
                     fee_percent=0.1,  # fee percent (normally 10%)
                     days_remaining=182.5,  # 6.0 months remaining
                     time_stretch_apy=0.05,  # APY used to calculate time_stretch
-                    test_id=1,
                 ),
                 # in_base=97.58374088542769
                 # in_pt=102.54051519598579
                 TestResultCalcInGivenOutSuccess(
                     without_fee_or_slippage=102.53971546251678,
                     without_fee=102.54051519598579,
-                    hyperdrive_fee=0.25397154625167895,
-                    hyperdrive_with_fee=102.79448674223747,
+                    fee=0.25397154625167895,
+                    with_fee=102.79448674223747,
                 ),
             ),  # end of test 1
             (  # test 2, token BASE
@@ -138,15 +99,14 @@ class TestCalcInGivenOut(unittest.TestCase):
                     fee_percent=0.2,  # fee percent (normally 10%)
                     days_remaining=182.5,  # 6.0 months remaining
                     time_stretch_apy=0.05,  # APY used to calculate time_stretch
-                    test_id=2,
                 ),
                 # in_base=97.58374088542769
                 # in_pt=102.54051519598579
                 TestResultCalcInGivenOutSuccess(
                     without_fee_or_slippage=102.53971546251678,
                     without_fee=102.54051519598579,
-                    hyperdrive_fee=0.5079430925033579,
-                    hyperdrive_with_fee=103.04845828848914,
+                    fee=0.5079430925033579,
+                    with_fee=103.04845828848914,
                 ),
             ),  # end of test 2
             (  # test 3, token BASE
@@ -166,15 +126,14 @@ class TestCalcInGivenOut(unittest.TestCase):
                     fee_percent=0.1,  # fee percent (normally 10%)
                     days_remaining=182.5,  # 6.0 months remaining
                     time_stretch_apy=0.05,  # APY used to calculate time_stretch
-                    test_id=3,
                 ),
                 # in_base=9750.950516367564
                 # in_pt=10278.313158090226
                 TestResultCalcInGivenOutSuccess(
                     without_fee_or_slippage=10269.89849637121,
                     without_fee=10278.313158090226,
-                    hyperdrive_fee=26.989849637120926,
-                    hyperdrive_with_fee=10305.303007727347,
+                    fee=26.989849637120926,
+                    with_fee=10305.303007727347,
                 ),
             ),  # end of test 3
             (  # test 4, token BASE
@@ -194,15 +153,14 @@ class TestCalcInGivenOut(unittest.TestCase):
                     fee_percent=0.1,  # fee percent (normally 10%)
                     days_remaining=182.5,  # 6.0 months remaining
                     time_stretch_apy=0.05,  # APY used to calculate time_stretch
-                    test_id=4,
                 ),
                 # in_base=77535.00890610163
                 # in_pt=84268.97713182
                 TestResultCalcInGivenOutSuccess(
                     without_fee_or_slippage=83252.75158412871,
                     without_fee=84268.97713182,
-                    hyperdrive_fee=325.2751584128717,
-                    hyperdrive_with_fee=84594.25229023286,
+                    fee=325.2751584128717,
+                    with_fee=84594.25229023286,
                 ),
             ),  # end of test 4
             (  # test 5, token BASE
@@ -222,15 +180,14 @@ class TestCalcInGivenOut(unittest.TestCase):
                     fee_percent=0.1,  # fee percent (normally 10%)
                     days_remaining=182.5,  # 6.0 months remaining
                     time_stretch_apy=0.05,  # APY used to calculate time_stretch
-                    test_id=5,
                 ),
                 # in_base=195.6738646971353
                 # in_pt=204.52526228054194
                 TestResultCalcInGivenOutSuccess(
                     without_fee_or_slippage=204.52346839323616,
                     without_fee=204.52526228054194,
-                    hyperdrive_fee=0.45234683932361636,
-                    hyperdrive_with_fee=204.97760911986555,
+                    fee=0.45234683932361636,
+                    with_fee=204.97760911986555,
                 ),
             ),  # end of test 5
             (  # test 6, token BASE
@@ -250,15 +207,14 @@ class TestCalcInGivenOut(unittest.TestCase):
                     fee_percent=0.1,  # fee percent (normally 10%)
                     days_remaining=182.5,  # 6.0 months remaining
                     time_stretch_apy=0.05,  # APY used to calculate time_stretch
-                    test_id=6,
                 ),
                 # in_base=188.59833831203287
                 # in_pt=212.87157998187467
                 TestResultCalcInGivenOutSuccess(
                     without_fee_or_slippage=212.87017681569534,
                     without_fee=212.87157998187467,
-                    hyperdrive_fee=1.2870176815695311,
-                    hyperdrive_with_fee=214.1585976634442,
+                    fee=1.2870176815695311,
+                    with_fee=214.1585976634442,
                 ),
             ),  # end of test 6
             (  # test 7, token BASE
@@ -278,15 +234,14 @@ class TestCalcInGivenOut(unittest.TestCase):
                     fee_percent=0.1,  # fee percent (normally 10%)
                     days_remaining=91.25,  # 3.0 months remaining
                     time_stretch_apy=0.05,  # APY used to calculate time_stretch
-                    test_id=7,
                 ),
                 # in_base=194.29838049286627
                 # in_pt=206.43613336980343
                 TestResultCalcInGivenOutSuccess(
                     without_fee_or_slippage=206.4357825223524,
                     without_fee=206.43613336980343,
-                    hyperdrive_fee=0.6435782522352407,
-                    hyperdrive_with_fee=207.07971162203867,
+                    fee=0.6435782522352407,
+                    with_fee=207.07971162203867,
                 ),
             ),  # end of test 7
             (  # test 8, token BASE
@@ -306,15 +261,14 @@ class TestCalcInGivenOut(unittest.TestCase):
                     fee_percent=0.1,  # fee percent (normally 10%)
                     days_remaining=91.25,  # 3.0 months remaining
                     time_stretch_apy=0.1,  # APY used to calculate time_stretch
-                    test_id=8,
                 ),
                 # in_base=189.24688558027265
                 # in_pt=213.70075247436762
                 TestResultCalcInGivenOutSuccess(
                     without_fee_or_slippage=213.69995097820515,
                     without_fee=213.70075247436762,
-                    hyperdrive_fee=1.3699950978205144,
-                    hyperdrive_with_fee=215.07074757218814,
+                    fee=1.3699950978205144,
+                    with_fee=215.07074757218814,
                 ),
             ),  # end of test 8
         ]
@@ -337,15 +291,14 @@ class TestCalcInGivenOut(unittest.TestCase):
                     fee_percent=0.1,  # fee percent (normally 10%)
                     days_remaining=182.5,  # 6.0 months remaining
                     time_stretch_apy=0.05,  # APY used to calculate time_stretch
-                    test_id=1,
                 ),
                 # in_base=97.5866001243412
                 # in_pt=102.53735730214976
                 TestResultCalcInGivenOutSuccess(
                     without_fee_or_slippage=97.58591137152354,
                     without_fee=97.5866001243412,
-                    hyperdrive_fee=0.24140886284764632,
-                    hyperdrive_with_fee=97.82800898718885,
+                    fee=0.24140886284764632,
+                    with_fee=97.82800898718885,
                 ),
             ),  # end of test 9
             (  # test 2, token PT
@@ -365,15 +318,14 @@ class TestCalcInGivenOut(unittest.TestCase):
                     fee_percent=0.2,  # fee percent (normally 10%)
                     days_remaining=182.5,  # 6.0 months remaining
                     time_stretch_apy=0.05,  # APY used to calculate time_stretch
-                    test_id=2,
                 ),
                 # in_base=97.5866001243412
                 # in_pt=102.53735730214976
                 TestResultCalcInGivenOutSuccess(
                     without_fee_or_slippage=97.58591137152354,
                     without_fee=97.5866001243412,
-                    hyperdrive_fee=0.48281772569529263,
-                    hyperdrive_with_fee=98.0694178500365,
+                    fee=0.48281772569529263,
+                    with_fee=98.0694178500365,
                 ),
             ),  # end of test 10
             (  # test 3, token PT
@@ -393,15 +345,14 @@ class TestCalcInGivenOut(unittest.TestCase):
                     fee_percent=0.1,  # fee percent (normally 10%)
                     days_remaining=182.5,  # 6.0 months remaining
                     time_stretch_apy=0.05,  # APY used to calculate time_stretch
-                    test_id=3,
                 ),
                 # in_base=9779.197793075873
                 # in_pt=10246.109814837924
                 TestResultCalcInGivenOutSuccess(
                     without_fee_or_slippage=9772.537730069402,
                     without_fee=9779.197793075873,
-                    hyperdrive_fee=22.746226993059782,
-                    hyperdrive_with_fee=9801.944020068933,
+                    fee=22.746226993059782,
+                    with_fee=9801.944020068933,
                 ),
             ),  # end of test 11
             (  # test 4, token PT
@@ -421,15 +372,14 @@ class TestCalcInGivenOut(unittest.TestCase):
                     fee_percent=0.1,  # fee percent (normally 10%)
                     days_remaining=182.5,  # 6.0 months remaining
                     time_stretch_apy=0.05,  # APY used to calculate time_stretch
-                    test_id=4,
                 ),
                 # in_base=79269.0508947279
                 # in_pt=81569.70379803452
                 TestResultCalcInGivenOutSuccess(
                     without_fee_or_slippage=78899.37999298729,
                     without_fee=79269.0508947279,
-                    hyperdrive_fee=110.06200070127115,
-                    hyperdrive_with_fee=79379.11289542918,
+                    fee=110.06200070127115,
+                    with_fee=79379.11289542918,
                 ),
             ),  # end of test 12
             (  # test 5, token PT
@@ -449,15 +399,14 @@ class TestCalcInGivenOut(unittest.TestCase):
                     fee_percent=0.1,  # fee percent (normally 10%)
                     days_remaining=182.5,  # 6.0 months remaining
                     time_stretch_apy=0.05,  # APY used to calculate time_stretch
-                    test_id=5,
                 ),
                 # in_base=195.68033249810105
                 # in_pt=204.51819491188508
                 TestResultCalcInGivenOutSuccess(
                     without_fee_or_slippage=195.6787624057141,
                     without_fee=195.68033249810105,
-                    hyperdrive_fee=0.4321237594285876,
-                    hyperdrive_with_fee=196.11245625752963,
+                    fee=0.4321237594285876,
+                    with_fee=196.11245625752963,
                 ),
             ),  # end of test 13
             (  # test 6, token PT
@@ -477,15 +426,14 @@ class TestCalcInGivenOut(unittest.TestCase):
                     fee_percent=0.1,  # fee percent (normally 10%)
                     days_remaining=182.5,  # 6.0 months remaining
                     time_stretch_apy=0.05,  # APY used to calculate time_stretch
-                    test_id=6,
                 ),
                 # in_base=188.60269388841698
                 # in_pt=212.8660290548578
                 TestResultCalcInGivenOutSuccess(
                     without_fee_or_slippage=188.60171912017125,
                     without_fee=188.60269388841698,
-                    hyperdrive_fee=1.1398280879828715,
-                    hyperdrive_with_fee=189.74252197639984,
+                    fee=1.1398280879828715,
+                    with_fee=189.74252197639984,
                 ),
             ),  # end of test 14
             (  # test 7, token PT
@@ -505,15 +453,14 @@ class TestCalcInGivenOut(unittest.TestCase):
                     fee_percent=0.1,  # fee percent (normally 10%)
                     days_remaining=91.25,  # 3.0 months remaining
                     time_stretch_apy=0.05,  # APY used to calculate time_stretch
-                    test_id=7,
                 ),
                 # in_base=194.30164747033268
                 # in_pt=206.43197067594156
                 TestResultCalcInGivenOutSuccess(
                     without_fee_or_slippage=194.30140381272443,
                     without_fee=194.30164747033268,
-                    hyperdrive_fee=0.5698596187275567,
-                    hyperdrive_with_fee=194.87150708906023,
+                    fee=0.5698596187275567,
+                    with_fee=194.87150708906023,
                 ),
             ),  # end of test 15
             (  # test 8, token PT
@@ -533,15 +480,14 @@ class TestCalcInGivenOut(unittest.TestCase):
                     fee_percent=0.1,  # fee percent (normally 10%)
                     days_remaining=91.25,  # 3.0 months remaining
                     time_stretch_apy=0.1,  # APY used to calculate time_stretch
-                    test_id=8,
                 ),
                 # in_base=189.25267474842258
                 # in_pt=213.6913557597436
                 TestResultCalcInGivenOutSuccess(
                     without_fee_or_slippage=189.25228759556148,
                     without_fee=189.25267474842258,
-                    hyperdrive_fee=1.0747712404438503,
-                    hyperdrive_with_fee=190.32744598886643,
+                    fee=1.0747712404438503,
+                    with_fee=190.32744598886643,
                 ),
             ),  # end of test 16
         ]
@@ -550,9 +496,12 @@ class TestCalcInGivenOut(unittest.TestCase):
 
         pricing_model = HyperdrivePricingModel()
         for (
-            test_case,
-            expected_result,
-        ) in test_cases:
+            test_id,
+            (
+                test_case,
+                expected_result,
+            ),
+        ) in enumerate(test_cases):
             time_stretch = pricing_model.calc_time_stretch(test_case.time_stretch_apy)
             time_remaining = StretchedTime(days=test_case.days_remaining, time_stretch=time_stretch)
 
@@ -567,22 +516,22 @@ class TestCalcInGivenOut(unittest.TestCase):
             np.testing.assert_almost_equal(
                 trade_result.breakdown.without_fee_or_slippage,
                 expected_result.without_fee_or_slippage,
-                err_msg=f"test {test_case.test_id} unexpected without_fee_or_slippage",
+                err_msg=f"test {test_id} unexpected without_fee_or_slippage",
             )
             np.testing.assert_almost_equal(
                 trade_result.breakdown.without_fee,
                 expected_result.without_fee,
-                err_msg=f"test {test_case.test_id} unexpected without_fee",
+                err_msg=f"test {test_id} unexpected without_fee",
             )
             np.testing.assert_almost_equal(
                 trade_result.breakdown.fee,
-                expected_result.hyperdrive_fee,
-                err_msg=f"test {test_case.test_id} unexpected hyperdrive fee",
+                expected_result.fee,
+                err_msg=f"test {test_id} unexpected hyperdrive fee",
             )
             np.testing.assert_almost_equal(
                 trade_result.breakdown.with_fee,
-                expected_result.hyperdrive_with_fee,
-                err_msg=f"test {test_case.test_id} unexpected hyperdrive with_fee",
+                expected_result.with_fee,
+                err_msg=f"test {test_id} unexpected hyperdrive with_fee",
             )
 
     # # TODO: This should be refactored to be a test for check_input_assertions and check_output_assertions
