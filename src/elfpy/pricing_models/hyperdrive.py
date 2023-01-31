2b1b0baf60ebc401bad0d62827e22cc9ee3bb6a8
Alex Towle <jalextowle@gmail.com>
2023-01-30 21:17:24 -0600

    And again

--- src/elfpy/pricing_models/hyperdrive.py
+++ src/elfpy/pricing_models/hyperdrive.py
@@ -49,19 +49,17 @@ class HyperdrivePricingModel(YieldSpacePricingModel):
             \begin{cases}
             c (\frac{1}{\mu} (
             \frac{k - (2y + cz - \Delta y \cdot t)^{1-\tau}}{\frac{c}{\mu}})^{\frac{1}{1-\tau}} - z)
-            + \Delta y \cdot (1 - \tau),
+            + \Delta y \cdot (1 - \tau), \\
             &\text{ if } token\_in = \text{"base"}
             (k - \frac{c}{\mu} (\mu * (z - \Delta z \cdot t))^{1 - \tau})^{\frac{1}{1 - \tau}}
             - (2y + cz) + c \cdot \Delta z \cdot (1 - \tau),
             &\text{ if } token\_in = \text{"pt"}
-            \end{cases}
-
+            \end{cases} \\
             f =
             \begin{cases}
-            (1 - \frac{1}{(\frac{2y + cz}{\mu z})^{\tau}}) \phi \Delta y, &\text{ if } token\_in = \text{"base"}
+            (1 - \frac{1}{(\frac{2y + cz}{\mu z})^{\tau}}) \phi \Delta y, &\text{ if } token\_in = \text{"base"} \\
             (\frac{2y + cz}{\mu z})^{\tau} - 1) \phi (c \Delta z), &\text{ if } token\_in = \text{"pt"}
-            \end{cases}
-
+            \end{cases} \\
             in = in' + f
 
         Parameters
@@ -185,19 +183,17 @@ class HyperdrivePricingModel(YieldSpacePricingModel):
             \begin{cases}
             c (z - \frac{1}{\mu} (
             \frac{k - (2y + cz + \Delta y \cdot t)^{1 - \tau}}{\frac{c}{\mu}})^{\frac{1}{1 - \tau}})
-            + \Delta y \cdot (1 - \tau),
+            + \Delta y \cdot (1 - \tau), \\
             &\text{ if } token\_out = \text{"base"}
             2y + cz - (k - \frac{c}{\mu} (\mu (z + \Delta z \cdot t))^{1 - \tau})^{\frac{1}{1 - \tau}}
             + c \cdot \Delta z \cdot (1 - \tau),
             &\text{ if } token\_out = \text{"pt"}
-            \end{cases}
-
+            \end{cases} \\
             f =
             \begin{cases}
-            (1 - \frac{1}{(\frac{2y + cz}{\mu z})^{\tau}}) \phi \Delta y, &\text{ if } token\_out = \text{"base"}
+            (1 - \frac{1}{(\frac{2y + cz}{\mu z})^{\tau}}) \phi \Delta y, &\text{ if } token\_out = \text{"base"} \\
             (\frac{2y + cz}{\mu z})^{\tau} - 1) \phi (c \Delta z), &\text{ if } token\_out = \text{"pt"}
-            \end{cases}
-
+            \end{cases} \\
             out = out' + f
 
         Parameters
