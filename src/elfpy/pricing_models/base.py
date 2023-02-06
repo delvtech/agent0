9be4cbec9810e86dab6bcbfa9f9e48be40fb4c37
Dylan <dpaiton@gmail.com>
2023-02-06 10:42:40 -0800

    fixes share reserve calc to use correct normalized time variable

--- src/elfpy/pricing_models/base.py
+++ src/elfpy/pricing_models/base.py
@@ -134,9 +134,9 @@ class PricingModel(ABC):
         """
         # TODO: Package up some of these arguments into market_state
         # pylint: disable=too-many-arguments
-        time_remaining_ = time_utils.norm_days(time_remaining.days)
         bond_reserves = (share_reserves / 2) * (
-            init_share_price * (1 + target_apr * time_remaining_) ** (1 / time_remaining.stretched_time) - share_price
+            init_share_price * (1 + target_apr * time_remaining.normalized_time) ** (1 / time_remaining.stretched_time)
+            - share_price
         )  # y = z/2 * (mu * (1 + rt)**(1/tau) - c)
         return bond_reserves
 
@@ -172,9 +172,8 @@ class PricingModel(ABC):
 
         """
         # TODO: Write a test for this function
-        time_remaining_ = time_utils.norm_days(time_remaining.days)
         share_reserves = bond_reserves / (
-            init_share_price * (1 - target_apr * time_remaining_) ** (1 / time_remaining.stretched_time)
+            init_share_price * (1 - target_apr * time_remaining.normalized_time) ** (1 / time_remaining.stretched_time)
         )  # z = y / (mu * (1 - rt)**(1/tau))
         return share_reserves
 
