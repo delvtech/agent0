eb89f815e0b10699e5af5913f8bfa8ebfe772ccf
Mihai <mcosma@gmail.com>
2023-02-08 21:55:13 -0500

    use dummy value for spot price

--- src/elfpy/wallet.py
+++ src/elfpy/wallet.py
@@ -113,17 +113,10 @@ class Wallet:
 
         .. todo:: TODO: return a dataclass instead of dict to avoid having to check keys & the get_state_keys func
         """
-        lp_token_value = (
-            market.market_state.share_reserves
-            * market.market_state.share_price
-            * (self.lp_tokens / market.market_state.lp_reserves)
-            if self.lp_tokens > 0
-            else 0.0
-        )
-        # share_of_pool = self.lp_tokens / market.market_state.lp_reserves if market.market_state.lp_reserves > 0 else 0.0
-        # pool_value = market.market_state.bond_reserves * market.spot_price  # in base
-        # pool_value += market.market_state.share_reserves * market.market_state.share_price  # in base
-        # lp_token_value = pool_value * share_of_pool  # in base
+        share_of_pool = self.lp_tokens / market.market_state.lp_reserves if market.market_state.lp_reserves > 0 else 0.0
+        pool_value = market.market_state.bond_reserves * 0.96  # market.spot_price  # in base
+        pool_value += market.market_state.share_reserves * market.market_state.share_price  # in base
+        lp_token_value = pool_value * share_of_pool  # in base
         share_reserves = market.market_state.share_reserves
         # compute long values in units of base
         longs_value = 0
