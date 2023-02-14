42650bf94ba2bf821ab0c7d32ee160fb6319a950
Mihai <mcosma@gmail.com>
2023-02-13 19:00:08 -0500

    fix test

--- tests/pricing_models/test_calc_in_given_out.py
+++ tests/pricing_models/test_calc_in_given_out.py
@@ -141,7 +141,7 @@ class TestCalcInGivenOut(unittest.TestCase):
                     redemption_fee_percent=0.01,
                 ),
                 time_remaining=StretchedTime(days=91.25, time_stretch=1.1, normalizing_constant=365),
-                exception_type=AssertionError,
+                exception_type=(AssertionError, decimal.DivisionByZero),
             )
         ]
         failure_test_cases = [
