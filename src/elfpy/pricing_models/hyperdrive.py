3c2a14b36c56281e132c667309345408e626a243
Dylan <dpaiton@gmail.com>
2023-02-18 15:33:53 -0800

    fixes doc ref

--- src/elfpy/pricing_models/hyperdrive.py
+++ src/elfpy/pricing_models/hyperdrive.py
@@ -81,8 +81,10 @@ class HyperdrivePricingModel(YieldSpacePricingModel):
 
         .. note::
            The pool total supply is a function of the base and bond reserves,
-           and is modified in :ref:`calc_lp_in_given_tokens_out <elfpy.pricing_models.yieldspace.YieldSpacePricingModel.calc_lp_in_given_tokens_out>`, :func:`elfpy.pricing_models.yieldspace.calc_tokens_out_given_lp_in`,
-           and :func:`elfpy.pricing_models.yieldspace.YieldSpacePricingModel.calc_lp_in_given_tokens_out`.
+           and is modified in
+           :ref:`calc_lp_in_given_tokens_out <elfpy.pricing_models.yieldspace.YieldSpacePricingModel.calc_lp_in_given_tokens_out>`,
+           :func:`elfpy.pricing_models.yieldspace.YieldSpacePricingModel.calc_lp_in_given_tokens_out`,
+           and :func:`calc_lp_in_given_tokens_out <elfpy.pricing_models.yieldspace.YieldSpacePricingModel.calc_lp_in_given_tokens_out>`.
            
            It can be approximated as :math:`s = y + cz`.
 
