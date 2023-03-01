fc10989699647b755625d1e2b59ca014ce77c6dc
Dylan <dpaiton@gmail.com>
2023-02-28 22:34:17 -0800

    adds INITIALIZE_MARKET action type

--- elfpy/markets/hyperdrive.py
+++ elfpy/markets/hyperdrive.py
@@ -24,14 +24,16 @@ if TYPE_CHECKING:
 class MarketActionType(Enum):
     r"""The descriptor of an action in a market"""
 
+    INITIALIZE_MARKET = "initialize_market"
+
+    ADD_LIQUIDITY = "add_liquidity"
+    REMOVE_LIQUIDITY = "remove_liquidity"
+
     OPEN_LONG = "open_long"
     OPEN_SHORT = "open_short"
 
     CLOSE_LONG = "close_long"
     CLOSE_SHORT = "close_short"
-
-    ADD_LIQUIDITY = "add_liquidity"
-    REMOVE_LIQUIDITY = "remove_liquidity"
 
 
 @types.freezable(frozen=True, no_new_attribs=True)
