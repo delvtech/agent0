1b0163a9db6827b918edced64b402b7068bca53e
Mihai <mcosma@gmail.com>
2023-03-01 17:02:22 -0500

    first commit

--- elfpy/simulators/simulators.py
+++ elfpy/simulators/simulators.py
@@ -2,6 +2,7 @@
 from __future__ import annotations  # types will be strings by default in 3.11
 
 from typing import TYPE_CHECKING, Optional
+import os
 import logging
 import json
 from dataclasses import dataclass, field, make_dataclass
@@ -10,7 +11,7 @@ import pandas as pd
 import numpy as np
 from numpy.random._generator import Generator
 
-
+import elfpy
 import elfpy.types as types
 import elfpy.time as time
 import elfpy.markets.hyperdrive as hyperdrive
@@ -182,6 +183,10 @@ class Config:
             self.variable_apr = [0.05] * self.num_trading_days
         if self.init_share_price < 0:  # defaults to -1 so this should happen right after init
             self.init_share_price = (1 + self.variable_apr[0]) ** self.init_vault_age
+
+        # put the filename in the .logging folder
+        self.log_filename = os.path.join(elfpy.CWD, self.log_filename)
+
         self.disable_new_attribs()  # disallow new attributes # pylint: disable=no-member # type: ignore
 
     def __getitem__(self, key) -> None:
