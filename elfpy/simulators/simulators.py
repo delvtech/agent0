723037394b2a65e7b71691cc84618d522df89c3a
Dylan <dpaiton@gmail.com>
2023-03-02 11:12:19 -0800

    uncomments config freeze

--- elfpy/simulators/simulators.py
+++ elfpy/simulators/simulators.py
@@ -383,7 +383,7 @@ class Simulator:
         self.config.check_variable_apr()
         # NOTE: lint error false positives: This message may report object members that are created dynamically,
         # but exist at the time they are accessed.
-        # self.config.freeze()  # type: ignore
+        self.config.freeze()  # type: ignore
         self.agents: dict[int, Agent] = {}
 
         # Simulation variables
