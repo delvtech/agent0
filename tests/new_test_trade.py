45b7e2370c0e3e4dbe101984e884df5eef5d6a23
Dylan <dpaiton@gmail.com>
2023-03-02 10:42:32 -0800

    close long has a few more deltas, needs the rest to pass

--- tests/new_test_trade.py
+++ tests/new_test_trade.py
@@ -31,6 +31,7 @@ class TradeTests(unittest.TestCase):
 
     def test_single_trade(self):
         """Tests the single_long and single_short policies where the amount traded is the full liquidity of the market"""
+        output_utils.setup_logging(log_filename="test_trades", log_level=logging.DEBUG)
         test_cases = [
             {"agent_policy": "single_long", "num_position_days": 90, "pricing_model_name": "Yieldspace"},  # Test 1
             {"agent_policy": "single_long", "num_position_days": 365, "pricing_model_name": "Yieldspace"},  # Test 2
@@ -41,8 +42,6 @@ class TradeTests(unittest.TestCase):
             {"agent_policy": "single_short", "num_position_days": 90, "pricing_model_name": "Hyperdrive"},  # Test 7
             {"agent_policy": "single_short", "num_position_days": 365, "pricing_model_name": "Hyperdrive"},  # Test 8
         ]
-
-        output_utils.setup_logging(log_filename="test_trades", log_level=logging.DEBUG)
         for test_number, test_case in enumerate(test_cases):
             self.config.pricing_model_name = test_case["pricing_model_name"]
             self.config.num_position_days = test_case["num_position_days"]
@@ -84,9 +83,6 @@ class TradeTests(unittest.TestCase):
             d_base_asset=expected_result.market_result.d_base,
             d_bond_asset=expected_result.market_result.d_bonds,
             d_base_buffer=expected_result.market_result.d_bonds,
-            d_bond_buffer=0,
-            d_lp_total_supply=0,
-            d_share_price=0,
         )
         expected_agent_deltas = wallet.Wallet(
             address=wallet_address,
@@ -98,7 +94,7 @@ class TradeTests(unittest.TestCase):
         policy = sim_utils.get_policy(agent_policy)
         simulator.add_agents([policy(wallet_address=wallet_address, budget=trade.amount)])
         actual_market_deltas, actual_agent_deltas = simulator.market.open_long(
-            wallet_address=1,
+            wallet_address=wallet_address,
             base_amount=trade.amount,
         )
         # check that the deltas are the same
@@ -122,38 +118,57 @@ class TradeTests(unittest.TestCase):
             market_state=simulator.market.market_state,
             time_remaining=simulator.market.position_duration,
         )
-        expected_result_close
-        expected_market_deltas = hyperdrive_market.MarketDeltas(
-            d_base_asset=expected_result.market_result.d_base,
-            d_bond_asset=expected_result.market_result.d_bonds,
-            d_base_buffer=expected_result.market_result.d_bonds,
-            d_bond_buffer=0,
-            d_lp_total_supply=0,
-            d_share_price=0,
+        open_market_deltas = hyperdrive_market.MarketDeltas(
+            d_base_asset=expected_result_open.market_result.d_base,
+            d_bond_asset=expected_result_open.market_result.d_bonds,
+            d_base_buffer=expected_result_open.market_result.d_bonds,
         )
-        expected_agent_deltas = wallet.Wallet(
+        new_market_state = simulator.market.market_state.copy()
+        new_market_state.apply_delta(open_market_deltas)
+        expected_result_close = pricing_model.calc_out_given_in(
+            in_=types.Quantity(amount=expected_result_open.user_result.d_bonds, unit=types.TokenType.PT),
+            market_state=new_market_state,
+            time_remaining=simulator.market.position_duration,
+        )
+        base_volume = simulator.market.calculate_base_volume(
+            expected_result_close.market_result.d_base,
+            expected_result_open.user_result.d_bonds,
+            simulator.market.position_duration.normalized_time,
+        )
+        close_market_deltas = hyperdrive_market.MarketDeltas(
+            d_base_asset=expected_result_close.market_result.d_base,
+            d_bond_asset=expected_result_close.market_result.d_bonds,
+            d_base_buffer=expected_result_close.market_result.d_bonds,
+            long_base_volume=-base_volume,
+        )
+        close_agent_deltas = wallet.Wallet(
             address=wallet_address,
-            balance=types.Quantity(amount=expected_result.user_result.d_base, unit=types.TokenType.BASE),
-            longs={0: wallet.Long(expected_result.user_result.d_bonds)},
-            fees_paid=expected_result.breakdown.fee,
+            balance=types.Quantity(amount=expected_result_close.user_result.d_base, unit=types.TokenType.BASE),
+            longs={0: wallet.Long(expected_result_close.user_result.d_bonds)},
+            fees_paid=expected_result_close.breakdown.fee,
         )
         # Execute the trade using the simulator
         policy = sim_utils.get_policy(agent_policy)
         simulator.add_agents([policy(wallet_address=wallet_address, budget=trade.amount)])
-        market_deltas, agent_deltas = simulator.market.open_long(
-            wallet_address=1,
-            # in base: that's the thing in your wallet you want to sell
-            trade_amount=agent.budget,  # type: ignore
+        actual_market_deltas, actual_agent_deltas = simulator.market.open_long(
+            wallet_address=wallet_address,
+            base_amount=trade.amount,
         )
+        simulator.market.market_state.apply_delta(actual_market_deltas)
+        simulator.agents[1].update_wallet(actual_agent_deltas)
         # peek inside the agent's wallet and see how many bonds they have
-        amount_of_bonds_purchased = agent_deltas.longs[0].balance
+        amount_of_bonds_purchased = actual_agent_deltas.longs[0].balance
         # sell those bonds to close the long
         actual_market_deltas, actual_agent_deltas = simulator.market.close_long(
             mint_time=0,
-            wallet_address=1,
+            wallet_address=wallet_address,
             # in bonds: that's the thing in your wallet you want to sell
             bond_amount=amount_of_bonds_purchased,
         )
         # check that the deltas are the same
-        self.assertEqual(expected_market_deltas, actual_market_deltas, msg="market deltas did not match")
-        self.assertEqual(expected_agent_deltas, actual_agent_deltas, msg="agent deltas did not match")
+        self.assertEqual(
+            close_market_deltas,
+            actual_market_deltas,
+            msg=f"\n\n{close_market_deltas=}\n----\n{actual_market_deltas=}\nmarket deltas did not match",
+        )
+        self.assertEqual(close_agent_deltas, actual_agent_deltas, msg="agent deltas did not match")
