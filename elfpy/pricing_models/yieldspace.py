aa774581908c8465c8b1a5e1f58d137c794750d6
Dylan <dpaiton@gmail.com>
2023-03-08 17:26:22 -0800

    fixes math

--- elfpy/pricing_models/yieldspace.py
+++ elfpy/pricing_models/yieldspace.py
@@ -841,6 +841,7 @@ class YieldspacePricingModel(PricingModel):
         init_share_price: Decimal,
     ) -> Decimal:
         """Helper function to derive invariant constant K
+
         .. math::
             k = \frac{c / mu} (mu z)^{1 - \tau} + (y + s)^(1 - \tau)
 
