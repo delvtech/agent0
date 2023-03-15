2ee0fd94f3c3b85647c5c9b644154c2dc0bd0fc3
Mihai <mcosma@gmail.com>
2023-03-15 00:09:06 -0400

    leave target_apr as float in calc_initial_bond_reserves

--- elfpy/pricing_models/base.py
+++ elfpy/pricing_models/base.py
@@ -86,7 +86,7 @@ class PricingModel(ABC):
 
     def calc_initial_bond_reserves(
         self,
-        target_apr: Decimal,
+        target_apr: float,
         time_remaining: time.StretchedTime,
         market_state: hyperdrive_market.MarketState,
     ) -> Decimal:
@@ -95,7 +95,7 @@ class PricingModel(ABC):
 
         Parameters
         ----------
-        target_apr : Decimal
+        target_apr : float
             Target fixed APR in decimal units (for example, 5% APR would be 0.05)
         time_remaining : StretchedTime
             Amount of time left until bond maturity
@@ -120,7 +120,8 @@ class PricingModel(ABC):
         annualized_time = time.norm_days(time_remaining.days, Decimal(365))
         # y = z/2 * (mu * (1 + rt)**(1/tau) - c)
         return (market_state.share_reserves / 2) * (
-            market_state.init_share_price * (1 + target_apr * annualized_time) ** (1 / time_remaining.stretched_time)
+            market_state.init_share_price
+            * (1 + Decimal(target_apr) * annualized_time) ** (1 / time_remaining.stretched_time)
             - market_state.share_price
         )
 
