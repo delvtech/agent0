b0d33653f53bab8cf64c89c77a20f55053b19b1b
Dylan <dpaiton@gmail.com>
2023-03-22 10:54:56 -0700

    addresses matts comments

--- elfpy/markets/hyperdrive/hyperdrive_market.py
+++ elfpy/markets/hyperdrive/hyperdrive_market.py
@@ -3,6 +3,7 @@ from __future__ import annotations
 
 import copy
 import logging
+from enum import IntEnum
 from collections import defaultdict
 from dataclasses import dataclass, field
 from typing import Union
@@ -21,6 +22,13 @@ import elfpy.utils.price as price_utils
 
 # dataclasses can have many attributes
 # pylint: disable=too-many-instance-attributes
+
+
+class AssetIdPrefix(IntEnum):
+    r"""The asset ID is used to encode the trade type in a transaction receipt"""
+    LONG = 0
+    SHORT = 1
+    WITHDRAWAL_SHARE = 2
 
 
 @dataclass
@@ -164,6 +172,57 @@ class MarketState(base_market.BaseMarketState):
     def copy(self) -> MarketState:
         """Returns a new copy of self"""
         return MarketState(**copy.deepcopy(self.__dict__))
+
+
+def encode_asset_id(prefix: int, timestamp: int) -> int:
+    r"""Encodes a prefix and a timestamp into an asset ID.
+
+    Asset IDs are used so that LP, long, and short tokens can all be represented
+    in a single MultiToken instance. The zero asset ID indicates the LP token.
+
+    Encode the asset ID by left-shifting the prefix by 248 bits,
+    then bitwise-or-ing the result with the timestamp.
+
+    Argments
+    --------
+    prefix: int
+        A one byte prefix that specifies the asset type.
+    timestamp: int
+        A timestamp associated with the asset.
+
+    Returns
+    -------
+    int
+        The asset ID.
+    """
+    timestamp_mask = (1 << 248) - 1
+    if timestamp > timestamp_mask:
+        raise ValueError("Invalid timestamp")
+    return (prefix << 248) | timestamp
+
+
+def decode_asset_id(asset_id: int) -> tuple[int, int]:
+    r"""Decodes a transaction asset ID into its constituent parts of an identifier, data, and a timestamp.
+
+    First calculate the prefix mask by left-shifting 1 by 248 bits and subtracting 1 from the result.
+    This gives us a bit-mask with 248 bits set to 1 and the rest set to 0.
+    Then apply this mask to the input ID using the bitwise-and operator `&` to extract
+    the lower 248 bits as the timestamp.
+
+    Arguments
+    ---------
+    asset_id: int
+        Encoded ID from a transaction. It is a concatenation, [identifier: 8 bits][timestamp: 248 bits]
+
+    Returns
+    -------
+    tuple[int, int]
+        identifier, timestamp
+    """
+    prefix_mask = (1 << 248) - 1
+    prefix = asset_id >> 248  # shr 248 bits
+    timestamp = asset_id & prefix_mask  # apply the prefix mask
+    return prefix, timestamp
 
 
 class Market(
--- elfpy/utils/apeworx_integrations.py
+++ elfpy/utils/apeworx_integrations.py
@@ -2,7 +2,6 @@
 
 from __future__ import annotations
 from typing import TYPE_CHECKING, Any
-from enum import IntEnum
 
 import logging
 
@@ -11,6 +10,8 @@ import logging
 # pylint: disable=import-error
 import ape  # type: ignore[reportMissingImports]
 
+import elfpy.markets.hyperdrive.hyperdrive_market as hyperdrive_market
+
 if TYPE_CHECKING:
     from ape.types import ContractLog  # type: ignore[reportMissingImports]
     from ape.api.accounts import AccountAPI  # type: ignore[reportMissingImports]
@@ -18,66 +19,8 @@ if TYPE_CHECKING:
     from ape.contracts.base import ContractInstance  # type: ignore[reportMissingImports]
 
 
-class AssetIdPrefix(IntEnum):
-    r"""The asset ID is used to encode the trade type in a transaction receipt"""
-    LONG = 0
-    SHORT = 1
-    WITHDRAWAL_SHARE = 2
-
-
-def encode_asset_id(prefix: int, timestamp: int) -> int:
-    r"""Encodes a prefix and a timestamp into an asset ID.
-
-    Asset IDs are used so that LP, long, and short tokens can all be represented
-    in a single MultiToken instance. The zero asset ID indicates the LP token.
-
-    Encode the asset ID by left-shifting the prefix by 248 bits,
-    then bitwise-or-ing the result with the timestamp.
-
-    Argments
-    --------
-    prefix: int
-        A one byte prefix that specifies the asset type.
-    timestamp: int
-        A timestamp associated with the asset.
-
-    Returns
-    -------
-    int
-        The asset ID.
-    """
-    timestamp_mask = (1 << 248) - 1
-    if timestamp > timestamp_mask:
-        raise ValueError("Invalid timestamp")
-    return (prefix << 248) | timestamp
-
-
-def decode_asset_id(asset_id: int) -> tuple[int, int]:
-    r"""Decodes a transaction asset ID into its constituent parts of an identifier, data, and a timestamp.
-
-    First calculate the prefix mask by left-shifting 1 by 248 bits and subtracting 1 from the result.
-    This gives us a bit-mask with 248 bits set to 1 and the rest set to 0.
-    Then apply this mask to the input ID using the bitwise-and operator `&` to extract
-    the lower 248 bits as the timestamp.
-
-    Arguments
-    ---------
-    asset_id: int
-        Encoded ID from a transaction. It is a concatenation, [identifier: 8 bits][timestamp: 248 bits]
-
-    Returns
-    -------
-    tuple[int, int]
-        identifier, timestamp
-    """
-    prefix_mask = (1 << 248) - 1
-    prefix = asset_id >> 248  # shr 248 bits
-    timestamp = asset_id & prefix_mask  # apply the prefix mask
-    return prefix, timestamp
-
-
-def get_transaction_trade_event(tx_receipt: Receipt) -> ContractLog:
-    r"""Parse the transaction receipt to get the trade event
+def get_transfer_single_event(tx_receipt: Receipt) -> ContractLog:
+    r"""Parse the transaction receipt to get the "transfer single" trade event
 
     Arguments
     ---------
@@ -102,7 +45,7 @@ def get_transaction_trade_event(tx_receipt: Receipt) -> ContractLog:
 
 
 def ape_open_position(
-    trade_prefix: AssetIdPrefix,
+    trade_prefix: hyperdrive_market.AssetIdPrefix,
     hyperdrive_contract: ContractInstance,
     agent_address: AccountAPI,
     trade_amount: int,
@@ -111,7 +54,7 @@ def ape_open_position(
 
     Arguments
     ---------
-    trade_prefix: AssetIdPrefix
+    trade_prefix: hyperdrive_market.AssetIdPrefix
         IntEnum specifying whether the trade is a long (0) or a short (1).
     hyperdrive_contract: ape.contracts.base.ContractInstance
         Ape project `ContractInstance
@@ -132,8 +75,8 @@ def ape_open_position(
         <https://docs.apeworx.io/ape/stable/methoddocs/api.html#ape.api.transactions.ReceiptAPI>`_.
     """
     with ape.accounts.use_sender(agent_address):  # sender for contract calls
-        if trade_prefix == AssetIdPrefix.LONG:  # open a long
-            min_output = 0
+        if trade_prefix == hyperdrive_market.AssetIdPrefix.LONG:  # open a long
+            min_output = 0  # python sims does not support alternative min_output
             as_underlying = False  # mockHyperdriveTestNet does not support as_underlying=True
             tx_receipt = hyperdrive_contract.openLong(  # type: ignore
                 trade_amount,  # base
@@ -141,8 +84,8 @@ def ape_open_position(
                 agent_address,
                 as_underlying,
             )
-        elif trade_prefix == AssetIdPrefix.SHORT:  # open a short
-            max_deposit = trade_amount
+        elif trade_prefix == hyperdrive_market.AssetIdPrefix.SHORT:  # open a short
+            max_deposit = trade_amount  # python sims does not support alternative max_deposit
             as_underlying = False  # mockHyperdriveTestNet does not support as_underlying=True
             tx_receipt = hyperdrive_contract.openShort(  # type: ignore
                 trade_amount,  # bonds
@@ -153,21 +96,21 @@ def ape_open_position(
         else:
             raise ValueError(f"{trade_prefix=} must be 0 (long) or 1 (short).")
         # Return the updated pool state & transaction result
-        transfer_single_event = get_transaction_trade_event(tx_receipt)
+        transfer_single_event = get_transfer_single_event(tx_receipt)
         # The ID is a concatenation of the current share price and the maturity time of the trade
         token_id = int(transfer_single_event["id"])
-        prefix, maturity_timestamp = decode_asset_id(token_id)
+        prefix, maturity_timestamp = hyperdrive_market.decode_asset_id(token_id)
         pool_state = hyperdrive_contract.getPoolInfo().__dict__  # type: ignore
-        pool_state["block_number_"] = tx_receipt.block_number
+        pool_state["block_number_"] = tx_receipt.block_number  # type: ignore
         pool_state["token_id_"] = token_id
         pool_state["prefix_"] = prefix
-        pool_state["maturity_timestamp_"] = maturity_timestamp
+        pool_state["maturity_timestamp_"] = maturity_timestamp  # in seconds
         logging.debug("hyperdrive_pool_state=%s", pool_state)
     return pool_state, tx_receipt
 
 
 def ape_close_position(
-    trade_prefix: AssetIdPrefix,
+    trade_prefix: hyperdrive_market.AssetIdPrefix,
     hyperdrive_contract: ContractInstance,
     agent_address: AccountAPI,
     bond_amount: int,
@@ -177,7 +120,7 @@ def ape_close_position(
 
     Arguments
     ---------
-    trade_prefix: AssetIdPrefix
+    trade_prefix: hyperdrive_market.AssetIdPrefix
         IntEnum specifying whether the trade is a long (0) or a short (1).
     hyperdrive_contract: ape.contracts.base.ContractInstance
         Ape project `ContractInstance
@@ -190,7 +133,7 @@ def ape_close_position(
     bond_amount: int
         Unsigned int-256 representation of the bond amount that the agent wishes to sell.
     maturity_time: int
-        Unsigned int-256 representation of the maturity time of the short being sold.
+        Unsigned int-256 representation of the maturity time in seconds of the short being sold.
         This is equal to the pool position duration plus the checkpoint time that is
         closest to (but before) the corresponding open trade.
 
@@ -204,7 +147,7 @@ def ape_close_position(
     # pylint: disable=too-many-locals
     with ape.accounts.use_sender(agent_address):  # sender for contract calls
         # Ensure requested close amount is not greater than what is available in the pool
-        trade_asset_id = encode_asset_id(trade_prefix, maturity_time)
+        trade_asset_id = hyperdrive_market.encode_asset_id(trade_prefix, maturity_time)
         agent_balance = hyperdrive_contract.balanceOf(trade_asset_id, agent_address)  # type: ignore
         if bond_amount < agent_balance:
             trade_bond_amount = bond_amount
@@ -218,7 +161,7 @@ def ape_close_position(
         # Close the position
         min_output = 0
         as_underlying = False  # mockHyperdriveTestNet does not support as_underlying=True
-        if trade_prefix == AssetIdPrefix.LONG:
+        if trade_prefix == hyperdrive_market.AssetIdPrefix.LONG:
             tx_receipt = hyperdrive_contract.closeLong(  # type: ignore
                 maturity_time,
                 trade_bond_amount,
@@ -226,7 +169,7 @@ def ape_close_position(
                 agent_address,
                 as_underlying,
             )
-        elif trade_prefix == AssetIdPrefix.SHORT:
+        elif trade_prefix == hyperdrive_market.AssetIdPrefix.SHORT:
             tx_receipt = hyperdrive_contract.closeShort(  # type: ignore
                 maturity_time,
                 trade_bond_amount,
@@ -237,14 +180,14 @@ def ape_close_position(
         else:
             raise ValueError(f"{trade_prefix=} must be 0 (long) or 1 (short).")
         # Return the updated pool state & transaction result
-        transfer_single_event = get_transaction_trade_event(tx_receipt)
+        transfer_single_event = get_transfer_single_event(tx_receipt)
         # The ID is a concatenation of the current share price and the maturity time of the trade
         token_id = int(transfer_single_event["id"])
-        prefix, maturity_timestamp = decode_asset_id(token_id)
+        prefix, maturity_timestamp = hyperdrive_market.decode_asset_id(token_id)
         pool_state = hyperdrive_contract.getPoolInfo().__dict__  # type: ignore
         pool_state["block_number_"] = tx_receipt.block_number  # type: ignore
         pool_state["token_id_"] = token_id
         pool_state["prefix_"] = prefix
-        pool_state["maturity_timestamp_"] = maturity_timestamp
+        pool_state["maturity_timestamp_"] = maturity_timestamp  # in seconds
         logging.debug("hyperdrive_pool_state=%s", pool_state)
     return pool_state, tx_receipt
