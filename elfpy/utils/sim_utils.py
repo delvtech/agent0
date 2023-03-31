3144bd2995f03e14d97b3ceaec1cb492fdf7380d
Dylan <dpaiton@gmail.com>
2023-03-30 19:31:10 -0700

    removes get_pricing_model bc it's not used anymore

--- elfpy/utils/sim_utils.py
+++ elfpy/utils/sim_utils.py
@@ -159,29 +159,6 @@ def get_initialized_hyperdrive_market(
     return market, agent_deltas, market_deltas
 
 
-def get_pricing_model(model_name: str) -> yieldspace_pm.YieldspacePricingModel | hyperdrive_pm.HyperdrivePricingModel:
-    r"""Get a PricingModel object from the config passed in
-
-    Parameters
-    ----------
-    model_name : str
-        name of the desired pricing_model; can be "hyperdrive", or "yieldspace"
-
-    Returns
-    -------
-    PricingModel
-        instantiated pricing model matching the input argument
-    """
-    logging.info("%s %s %s", "#" * 20, model_name, "#" * 20)
-    if model_name.lower() == "hyperdrive":
-        pricing_model = hyperdrive_pm.HyperdrivePricingModel()
-    elif model_name.lower() == "yieldspace":
-        pricing_model = yieldspace_pm.YieldspacePricingModel()
-    else:
-        raise ValueError(f'pricing_model_name must be "Hyperdrive", or "YieldSpace", not {model_name}')
-    return pricing_model
-
-
 def get_policy(agent_type: str) -> Policy:
     """Returns an uninstantiated agent
 
