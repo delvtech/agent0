e3f19b001726fa2c81d0a273916ca2d7e5f155a3
Matthew Brown <mattfdbrown@gmail.com>
2023-04-19 16:19:52 -0700

    convert WalletFP to fp

--- elfpy/agents/wallet.py
+++ elfpy/agents/wallet.py
@@ -8,6 +8,7 @@ from typing import TYPE_CHECKING
 
 import elfpy
 import elfpy.types as types
+from elfpy.utils.math.fixed_point import FixedPoint
 
 if TYPE_CHECKING:
     from typing import Any, Iterable
@@ -273,13 +274,13 @@ class LongFP:
 
     Parameters
     ----------
-    balance : float
+    balance : FixedPoint
         The amount of bonds that the position is long.
 
     .. todo:: make balance a Quantity to enforce units
     """
 
-    balance: float  # bonds
+    balance: FixedPoint  # bonds
 
 
 @dataclass
@@ -288,14 +289,14 @@ class ShortFP:
 
     Parameters
     ----------
-    balance : float
+    balance : FixedPoint
         The amount of bonds that the position is short.
-    open_share_price: float
+    open_share_price: FixedPoint
         The share price at the time the short was opened.
     """
 
-    balance: float
-    open_share_price: float
+    balance: FixedPoint
+    open_share_price: FixedPoint
 
 
 @dataclass
@@ -306,20 +307,20 @@ class BorrowFP:
     ----------
     borrow_token : TokenType
     .. todo: add explanation
-    borrow_amount : float
+    borrow_amount : FixedPoint
     .. todo: add explanation
     collateral_token : TokenType
     .. todo: add explanation
-    collateral_amount: float
+    collateral_amount: FixedPoint
     .. todo: add explanation
     start_time : float
     .. todo: add explanation
     """
     borrow_token: types.TokenType
-    borrow_amount: float
-    borrow_shares: float
+    borrow_amount: FixedPoint
+    borrow_shares: FixedPoint
     collateral_token: types.TokenType
-    collateral_amount: float
+    collateral_amount: FixedPoint
     start_time: float
 
 
@@ -333,13 +334,13 @@ class WalletFP:
         The trader's address.
     balance : Quantity
         The base assets that held by the trader.
-    lp_tokens : float
+    lp_tokens : FixedPoint
         The LP tokens held by the trader.
     longs : Dict[float, Long]
         The long positions held by the trader.
     shorts : Dict[float, Short]
         The short positions held by the trader.
-    fees_paid : float
+    fees_paid : FixedPoint
         The fees paid by the wallet.
     """
 
@@ -350,19 +351,21 @@ class WalletFP:
     address: int
 
     # fungible
-    balance: types.Quantity = field(default_factory=lambda: types.Quantity(amount=0, unit=types.TokenType.BASE))
+    balance: types.QuantityFP = field(
+        default_factory=lambda: types.QuantityFP(amount=FixedPoint(0), unit=types.TokenType.BASE)
+    )
     # TODO: Support multiple typed balances:
     #     balance: Dict[types.TokenType, types.Quantity] = field(default_factory=dict)
-    lp_tokens: float = 0
+    lp_tokens: FixedPoint = FixedPoint(0)
 
     # non-fungible (identified by key=mint_time, stored as dict)
-    longs: dict[float, Long] = field(default_factory=dict)
-    shorts: dict[float, Short] = field(default_factory=dict)
-    withdraw_shares: float = 0
+    longs: dict[float, LongFP] = field(default_factory=dict)
+    shorts: dict[float, ShortFP] = field(default_factory=dict)
+    withdraw_shares: FixedPoint = FixedPoint(0)
     # borrow and  collateral have token type, which is not represented here
     # this therefore assumes that only one token type can be used at any given mint time
-    borrows: dict[float, Borrow] = field(default_factory=dict)
-    fees_paid: float = 0
+    borrows: dict[float, BorrowFP] = field(default_factory=dict)
+    fees_paid: FixedPoint = FixedPoint(0)
 
     def __getitem__(self, key: str) -> Any:
         return getattr(self, key)
@@ -370,11 +373,11 @@ class WalletFP:
     def __setitem__(self, key: str, value: Any) -> None:
         setattr(self, key, value)
 
-    def copy(self) -> Wallet:
+    def copy(self) -> WalletFP:
         """Returns a new copy of self"""
-        return Wallet(**copy.deepcopy(self.__dict__))
+        return WalletFP(**copy.deepcopy(self.__dict__))
 
-    def update(self, wallet_deltas: Wallet) -> None:
+    def update(self, wallet_deltas: WalletFP) -> None:
         """Update the agent's wallet
 
         Parameters
@@ -423,7 +426,7 @@ class WalletFP:
                 raise ValueError(f"wallet_key={key} is not allowed.")
             elfpy.check_non_zero(self)
 
-    def _update_borrows(self, borrows: Iterable[tuple[float, Borrow]]) -> None:
+    def _update_borrows(self, borrows: Iterable[tuple[float, BorrowFP]]) -> None:
         for mint_time, borrow_summary in borrows:
             if mint_time != borrow_summary.start_time:
                 raise ValueError(
@@ -433,12 +436,12 @@ class WalletFP:
                 self.borrows[borrow_summary.start_time].borrow_amount += borrow_summary.borrow_amount
             else:
                 self.borrows.update({borrow_summary.start_time: borrow_summary})
-            if self.borrows[borrow_summary.start_time].borrow_amount == 0:
+            if self.borrows[borrow_summary.start_time].borrow_amount == FixedPoint(0):
                 # Removing the empty borrows allows us to check existance
                 # of open borrows using `if self.borrows`
                 del self.borrows[borrow_summary.start_time]
 
-    def _update_longs(self, longs: Iterable[tuple[float, Long]]) -> None:
+    def _update_longs(self, longs: Iterable[tuple[float, LongFP]]) -> None:
         """Helper internal function that updates the data about Longs contained in the Agent's Wallet object
 
         Parameters
@@ -448,7 +451,7 @@ class WalletFP:
             and its market-relative mint time
         """
         for mint_time, long in longs:
-            if long.balance != 0:
+            if long.balance != FixedPoint(0):
                 logging.debug(
                     "agent #%g trade longs, mint_time = %g\npre-trade amount = %s\ntrade delta = %s",
                     self.address,
@@ -460,14 +463,14 @@ class WalletFP:
                     self.longs[mint_time].balance += long.balance
                 else:
                     self.longs.update({mint_time: long})
-            if self.longs[mint_time].balance == 0:
+            if self.longs[mint_time].balance == FixedPoint(0):
                 # Removing the empty borrows allows us to check existance
                 # of open longs using `if wallet.longs`
                 del self.longs[mint_time]
-            if mint_time in self.longs and self.longs[mint_time].balance < 0:
+            if mint_time in self.longs and self.longs[mint_time].balance < FixedPoint(0):
                 raise AssertionError(f"ERROR: Wallet balance should be >= 0, not {self.longs[mint_time]}.")
 
-    def _update_shorts(self, shorts: Iterable[tuple[float, Short]]) -> None:
+    def _update_shorts(self, shorts: Iterable[tuple[float, ShortFP]]) -> None:
         """Helper internal function that updates the data about Shortscontained in the Agent's Wallet object
 
         Parameters
@@ -493,18 +496,18 @@ class WalletFP:
                     # mean for the open share price.  this covers an edge case where two shorts are
                     # opened for the same account in the same block.  if the balance is negative, we
                     # don't want to update the open_short_price
-                    if short.balance > 0:
+                    if short.balance > FixedPoint(0):
                         old_share_price = self.shorts[mint_time].open_share_price
                         self.shorts[mint_time].open_share_price = (
                             short.open_share_price * short.balance + old_share_price * old_balance
                         ) / (short.balance + old_balance)
                 else:
                     self.shorts.update({mint_time: short})
-            if self.shorts[mint_time].balance == 0:
+            if self.shorts[mint_time].balance == FixedPoint(0):
                 # Removing the empty borrows allows us to check existance
                 # of open shorts using `if wallet.shorts`
                 del self.shorts[mint_time]
-            if mint_time in self.shorts and self.shorts[mint_time].balance < 0:
+            if mint_time in self.shorts and self.shorts[mint_time].balance < FixedPoint(0):
                 raise AssertionError(f"ERROR: Wallet balance should be >= 0, not {self.shorts[mint_time]}.")
 
     def get_state_keys(self) -> tuple[str, ...]:
