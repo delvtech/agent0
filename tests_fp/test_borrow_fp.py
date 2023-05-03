1b9df0696be70b3305cf8aec1b30bed8e8295f80
Dylan <dpaiton@gmail.com>
2023-05-03 19:59:26 +0200

    removes pylint disable

--- tests_fp/test_borrow_fp.py
+++ tests_fp/test_borrow_fp.py
@@ -12,9 +12,6 @@ import elfpy.types as types
 import elfpy.utils.outputs as output_utils
 from elfpy.utils.math import FixedPoint
 
-# TODO: remove this after FixedPoint PRs are finished
-# pylint: disable=duplicate-code
-
 
 class TestBorrow(unittest.TestCase):
     """Testing the Borrow Market"""
