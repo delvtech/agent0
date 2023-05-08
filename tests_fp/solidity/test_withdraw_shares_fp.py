e7e0406538319798d04d791ed9c600c904bb1117
Dylan <dpaiton@gmail.com>
2023-05-08 14:40:23 +0200

    fixpointify

--- tests_fp/solidity/test_withdraw_shares_fp.py
+++ tests_fp/solidity/test_withdraw_shares_fp.py
@@ -1,45 +1,44 @@
 """Withdraw shares tests that match those being executed in the solidity repo."""
 import unittest
 
-import numpy as np
-
 import elfpy.agents.agent as agent
 import elfpy.markets.hyperdrive.hyperdrive_market as hyperdrive_market
 import elfpy.pricing_models.hyperdrive as hyperdrive_pm
 import elfpy.time as time
+from elfpy.math import FixedPoint, FixedPointMath
 
 # pylint: disable=too-many-arguments
-
 # pylint: disable=too-many-locals
 
 
 class TestWithdrawShares(unittest.TestCase):
     """Test withdraw shares functionality when the market is in different states."""
 
-    budget: float = 500_000_000
-    initial_liquidity: float = 500_000_000
-    target_apr: float = 0.05
-    term_length: int = 365
-    alice: agent.Agent
-    bob: agent.Agent
-    celine: agent.Agent
-    hyperdrive: hyperdrive_market.Market
-    block_time: time.BlockTime
+    budget: FixedPoint = FixedPoint("500_000_000.0")
+    initial_liquidity: FixedPoint = FixedPoint("500_000_000.0")
+    target_apr: FixedPoint = FixedPoint("0.05")
+    term_length: FixedPoint = FixedPoint("365.0")
+    one_year = FixedPoint("1.0")
+    alice: agent.AgentFP
+    bob: agent.AgentFP
+    celine: agent.AgentFP
+    hyperdrive: hyperdrive_market.MarketFP
+    block_time: time.BlockTimeFP
 
     def setUp(self):
         """Set up agent, pricing model, & market for the subsequent tests.
         This function is run before each test method.
         """
-        self.alice = agent.Agent(wallet_address=0, budget=self.budget)
-        self.bob = agent.Agent(wallet_address=1, budget=self.budget)
-        self.celine = agent.Agent(wallet_address=2, budget=self.budget)
-        self.block_time = time.BlockTime()
-        pricing_model = hyperdrive_pm.HyperdrivePricingModel()
-        market_state = hyperdrive_market.MarketState()
-        self.hyperdrive = hyperdrive_market.Market(
+        self.alice = agent.AgentFP(wallet_address=0, budget=self.budget)
+        self.bob = agent.AgentFP(wallet_address=1, budget=self.budget)
+        self.celine = agent.AgentFP(wallet_address=2, budget=self.budget)
+        self.block_time = time.BlockTimeFP()
+        pricing_model = hyperdrive_pm.HyperdrivePricingModelFP()
+        market_state = hyperdrive_market.MarketStateFP()
+        self.hyperdrive = hyperdrive_market.MarketFP(
             pricing_model=pricing_model,
             market_state=market_state,
-            position_duration=time.StretchedTime(
+            position_duration=time.StretchedTimeFP(
                 days=self.term_length,
                 time_stretch=pricing_model.calc_time_stretch(self.target_apr),
                 normalizing_constant=self.term_length,
@@ -56,10 +55,10 @@ class TestWithdrawShares(unittest.TestCase):
         """Should fail to redeem withdraw shares."""
         market_state = self.hyperdrive.market_state
         # advance time and let interest accrue
-        self.block_time.set_time(0.5)
+        self.block_time.set_time(FixedPoint("0.5"), time.TimeUnit.YEARS)
         # compund interest = p * e ^(rate * time)
         # we advance by one half year, and the rate is .05 / year
-        accrued = self.budget * float(np.exp(self.target_apr * 0.5))
+        accrued = self.budget * FixedPointMath.exp(self.target_apr * FixedPoint("0.5"))
         market_state.share_price = accrued / self.budget
 
     # TODO: complete this
@@ -68,30 +67,28 @@ class TestWithdrawShares(unittest.TestCase):
         market_state = self.hyperdrive.market_state
 
         # advance time and let interest accrue
-        self.block_time.set_time(1)
+        self.block_time.set_time(self.one_year, time.TimeUnit.YEARS)
 
         # compund interest = p * e ^(rate * time)
         # we advance by one year, and the rate is .05 / year
-        accrued = self.budget * float(np.exp(self.target_apr * 1))
+        accrued = self.budget * FixedPointMath.exp(self.target_apr * self.one_year)
         market_state.share_price = accrued / self.budget
 
         # bob opens a short
-        short_amount_bonds = 50_000_000
+        short_amount_bonds = FixedPoint("50_000_000.0")
         self.hyperdrive.open_short(self.bob.wallet, short_amount_bonds)
-        # base_paid = abs(wallet_deltas.balance.amount)
-        # bond_amount = long_market_deltas.d_bond_asset
 
     def test_redeem_withdraw_shares_long(self):
         """Should receive the correct amount of withdrawal shares when there are multiple longs"""
         market_state = self.hyperdrive.market_state
 
         # TODO: fuzz the trade size betweeon 0 and 5M
-        trade_size = 5_000_000
+        trade_size = FixedPoint("5_000_000.0")
         # TODO: fuzz the variable rate between 0 and 1
-        self.target_apr = 0.05
+        self.target_apr = FixedPoint("0.05")
 
         # advance time and let interest accrue
-        self.block_time.set_time(1)
+        self.block_time.set_time(self.one_year, time.TimeUnit.YEARS)
 
         # celine opens a long
         long_market_deltas, long_agent_deltas = self.hyperdrive.open_long(self.celine.wallet, trade_size)
@@ -99,16 +96,15 @@ class TestWithdrawShares(unittest.TestCase):
         base_spent = abs(long_agent_deltas.balance.amount)
 
         # bob adds liquidity
-        contribution = 5_000_000
+        contribution = FixedPoint("5_000_000.0")
         _, lp_agent_deltas = self.hyperdrive.add_liquidity(self.bob.wallet, contribution)
         lp_shares = lp_agent_deltas.lp_tokens
 
         # compund interest = p * e ^(rate * time)
         # we advance by a year, and the rate is .05 / year
-        one_year = 1
-        self.block_time.set_time(one_year)
+        self.block_time.set_time(self.one_year, time.TimeUnit.YEARS)
         base_in_pool = self.initial_liquidity + contribution + base_spent
-        pool_value = base_in_pool * float(np.exp(self.target_apr * one_year))
+        pool_value = base_in_pool * FixedPointMath.exp(self.target_apr * self.one_year)
         market_state.share_price = pool_value / base_in_pool
 
         # calculate the portion of the pool's value (after interest) that bob contributed.
@@ -122,15 +118,15 @@ class TestWithdrawShares(unittest.TestCase):
 
         _, remove_lp_agent_deltas = self.hyperdrive.remove_liquidity(self.bob.wallet, lp_shares)
         withdrawal_proceeds = remove_lp_agent_deltas.balance.amount
-        self.assertAlmostEqual(withdrawal_proceeds, expected_withdrawal_proceeds, places=7)
+        self.assertAlmostEqual(float(withdrawal_proceeds), float(expected_withdrawal_proceeds), places=7)
 
     def test_redeem_withdraw_shares_long_long(self):
         """Should receive the correct amount of withdrawal shares when there are multiple longs"""
-        variable_rate = 0.05
+        variable_rate = FixedPoint("0.05")
         market_state = self.hyperdrive.market_state
 
         # TODO: fuzz the trade size betweeon 0 and 5M
-        trade_size = 5_000_000
+        trade_size = FixedPoint("5_000_000.0")
 
         # celine opens a long
         long_market_deltas, long_agent_deltas = self.hyperdrive.open_long(self.celine.wallet, trade_size)
@@ -138,11 +134,11 @@ class TestWithdrawShares(unittest.TestCase):
         base_spent = abs(long_agent_deltas.balance.amount)
 
         # we advance by a 1/2 year
-        half_year = 0.5
-        self.block_time.set_time(self.block_time.time + half_year)
+        half_year = self.one_year / FixedPoint("2.0")
+        self.block_time.set_time(self.block_time.time + half_year, time.TimeUnit.YEARS)
         # compund interest = p * e ^(rate * time), the rate is .05 / year, time is .5 year
         base_in_pool = self.initial_liquidity + base_spent
-        pool_value = base_in_pool * float(np.exp(variable_rate * half_year))
+        pool_value = base_in_pool * FixedPointMath.exp(variable_rate * half_year)
         total_shares = base_in_pool
         market_state.share_price = pool_value / total_shares
 
@@ -152,15 +148,15 @@ class TestWithdrawShares(unittest.TestCase):
         base_spent2 = abs(long_agent_deltas2.balance.amount)
 
         # bob adds liquidity
-        contribution = 5_000_000
+        contribution = FixedPoint("5_000_000.0")
         _, lp_agent_deltas = self.hyperdrive.add_liquidity(self.bob.wallet, contribution)
         lp_shares = lp_agent_deltas.lp_tokens
 
         # we advance by a 1/2 year
-        self.block_time.set_time(self.block_time.time + half_year)
+        self.block_time.set_time(self.block_time.time + half_year, time.TimeUnit.YEARS)
         # compund interest = p * e ^(rate * time), the rate is .05 / year
         base_in_pool2 = pool_value + contribution + base_spent2
-        pool_value2 = base_in_pool2 * float(np.exp(variable_rate * half_year))
+        pool_value2 = base_in_pool2 * FixedPointMath.exp(variable_rate * half_year)
         total_shares2 = total_shares + contribution / market_state.share_price + base_spent2 / market_state.share_price
         market_state.share_price = pool_value2 / total_shares2
 
@@ -177,4 +173,4 @@ class TestWithdrawShares(unittest.TestCase):
 
         _, remove_lp_agent_deltas = self.hyperdrive.remove_liquidity(self.bob.wallet, lp_shares)
         withdrawal_proceeds = remove_lp_agent_deltas.balance.amount
-        self.assertAlmostEqual(withdrawal_proceeds, expected_withdrawal_proceeds, delta=100)
+        self.assertAlmostEqual(float(withdrawal_proceeds), float(expected_withdrawal_proceeds), delta=100)
