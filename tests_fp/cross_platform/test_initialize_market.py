86e4c26b9fd3decedc8fe1ea6cd49e63893bd39c
Matthew Brown <mattfdbrown@gmail.com>
2023-05-12 08:40:35 -0700

    get test working

--- tests_fp/cross_platform/test_initialize_market.py
+++ tests_fp/cross_platform/test_initialize_market.py
@@ -9,10 +9,13 @@ from __future__ import annotations
 # stdlib
 from pathlib import Path
 from collections import defaultdict
+import unittest
 
 # external lib
 import ape
-from ape.contracts import ContractInstance
+from ape.contracts import ContractInstance, ContractContainer
+from ape.api.transactions import ReceiptAPI
+from eth_typing import HexAddress
 
 # elfpy core repo
 from elfpy.math.fixed_point import FixedPoint
@@ -24,6 +27,7 @@ import elfpy.pricing_models.hyperdrive as hyperdrive_pm
 from elfpy.time.time import BlockTimeFP, StretchedTimeFP
 
 
+# TODO: take this out, we don't want the simulator stuff in these tests
 def get_config() -> simulators.Config:
     """Set config values for the experiment"""
     _config = simulators.Config()
@@ -36,9 +40,9 @@ def get_agents(budget: FixedPoint = FixedPoint("50_000_000.0")):
     bob_sol = ape.accounts.test_accounts.generate_test_account()
     celine_sol = ape.accounts.test_accounts.generate_test_account()
 
-    alice_py = agentlib.AgentFP(wallet_address=alice_sol.address, budget=budget)
-    bob_py = agentlib.AgentFP(wallet_address=bob_sol.address, budget=budget)
-    celine_py = agentlib.AgentFP(wallet_address=celine_sol.address, budget=budget)
+    alice_py = agentlib.AgentFP(wallet_address=0, budget=budget)
+    bob_py = agentlib.AgentFP(wallet_address=1, budget=budget)
+    celine_py = agentlib.AgentFP(wallet_address=2, budget=budget)
 
     return ([alice_py, bob_py, celine_py], [alice_sol, bob_sol, celine_sol])
 
@@ -54,8 +58,13 @@ def to_floating_point(float_var, decimal_places=18):
 
 
 def get_simulation_market_state_from_contract(
-    hyperdrive_contract, agent_address, _position_duration_seconds, _checkpoint_duration, variable_apr, _config
-):
+    hyperdrive_contract: ContractInstance,
+    agent_address: HexAddress,
+    position_duration_seconds: int,
+    checkpoint_duration_days: int,
+    variable_apr: int,
+    config: simulators.Config,
+) -> hyperdrive_market.MarketState:
     """
     hyperdrive_contract: ape.contracts.base.ContractInstance
         Ape project `ContractInstance
@@ -67,10 +76,12 @@ def get_simulation_market_state_from_contract(
         representing the agent which is executing the action.
     """
     # pylint: disable=too-many-arguments
-    pool_state = hyperdrive_contract.getPoolInfo().__dict__
+    pool_info = hyperdrive_contract.getPoolInfo()
+    print(f"{pool_info=}")
+    pool_state = pool_info.__dict__
     with ape.accounts.use_sender(agent_address):  # sender for contract calls
         asset_id = hyperdrive_assets.encode_asset_id(
-            hyperdrive_assets.AssetIdPrefix.WITHDRAWAL_SHARE, _position_duration_seconds
+            hyperdrive_assets.AssetIdPrefix.WITHDRAWAL_SHARE, position_duration_seconds
         )
         total_supply_withdraw_shares = hyperdrive_contract.balanceOf(asset_id, agent_address)
 
@@ -82,40 +93,41 @@ def get_simulation_market_state_from_contract(
         # TODO: bond_buffer=0,
         variable_apr=variable_apr,
         share_price=to_floating_point(pool_state["sharePrice"]),
-        init_share_price=_config.init_share_price,
-        curve_fee_multiple=_config.curve_fee_multiple,
-        flat_fee_multiple=_config.flat_fee_multiple,
+        init_share_price=config.init_share_price,
+        curve_fee_multiple=config.curve_fee_multiple,
+        flat_fee_multiple=config.flat_fee_multiple,
         longs_outstanding=to_floating_point(pool_state["longsOutstanding"]),
         shorts_outstanding=to_floating_point(pool_state["shortsOutstanding"]),
         long_average_maturity_time=to_floating_point(pool_state["longAverageMaturityTime"]),
         short_average_maturity_time=to_floating_point(pool_state["shortAverageMaturityTime"]),
-        long_base_volume=to_floating_point(pool_state["longBaseVolume"]),
+        long_base_volume=0,  # to_floating_point(pool_state["longBaseVolume"]),
         short_base_volume=to_floating_point(pool_state["shortBaseVolume"]),
         # TODO: checkpoints=defaultdict
-        checkpoint_duration=_checkpoint_duration,
+        checkpoint_duration=checkpoint_duration_days,
         total_supply_longs=defaultdict(float, {0: to_floating_point(pool_state["longsOutstanding"])}),
         total_supply_shorts=defaultdict(float, {0: to_floating_point(pool_state["shortsOutstanding"])}),
         total_supply_withdraw_shares=to_floating_point(total_supply_withdraw_shares),
         withdraw_shares_ready_to_withdraw=to_floating_point(pool_state["withdrawalSharesReadyToWithdraw"]),
-        withdraw_capital=to_floating_point(pool_state["capital"]),
-        withdraw_interest=to_floating_point(pool_state["interest"]),
+        withdraw_capital=to_floating_point(pool_state["withdrawalSharesProceeds"]),
+        withdraw_interest=0,  # to_floating_point(pool_state["interest"]),
     )
 
 
-def setUp():
+def get_fixture():
+    """Gets the simulation and solidity test fixutres."""
     # Instantiate the config using the command line arguments as overrides.
     config = get_config()
 
     # Instantiate the sim market
-    initial_apr = FixedPoint(config.target_fixed_apr)
+    initial_apr = int(FixedPoint(config.target_fixed_apr))
     position_duration_days = FixedPoint(180 * 10**18)
     pricing_model = hyperdrive_pm.HyperdrivePricingModelFP()
     position_duration = StretchedTimeFP(
         days=position_duration_days,
-        time_stretch=pricing_model.calc_time_stretch(initial_apr),
+        time_stretch=pricing_model.calc_time_stretch(FixedPoint(initial_apr)),
         normalizing_constant=position_duration_days,
     )
-    hyperdrive = hyperdrive_market.MarketFP(
+    hyperdrive_sim = hyperdrive_market.MarketFP(
         pricing_model=hyperdrive_pm.HyperdrivePricingModelFP(),
         market_state=hyperdrive_market.MarketStateFP(),
         position_duration=position_duration,
@@ -132,56 +144,124 @@ def setUp():
     # Set up agents
     sim_agents, sol_agents = get_agents()
     deployer = ape.accounts.test_accounts.generate_test_account()
+    deployer.provider.set_balance(deployer.address, int(FixedPoint("1000000.0")))
 
-    # Use agent 0 to initialize the chain market
-    base_address = deployer.deploy(project.ERC20Mintable)  # type: ignore
-    base_erc20 = project.ERC20Mintable.at(base_address)  # type: ignore
-    fixed_math_address = deployer.deploy(project.MockFixedPointMath)  # type: ignore
-    base_erc20.mint(to_fixed_point(config.target_liquidity), sender=deployer)  # type: ignore
+    # deploy base token and fixed math contracts
+    base_erc20_contract = deployer.deploy(project.ERC20Mintable)  # type: ignore
+    base_erc20 = project.ERC20Mintable.at(base_erc20_contract)  # type: ignore
+    fixed_math_contract = deployer.deploy(project.MockFixedPointMath)  # type: ignore
 
-    # Convert sim config to solidity format (fixed-point)
-    initial_supply = to_fixed_point(config.target_liquidity)
+    # give some base token to the deployer
+    base_erc20_contract.mint(to_fixed_point(config.target_liquidity), sender=deployer)  # type: ignore
+
+    # Convert sim config to solidity format: integer representation of 1e18 fixed point.
+    initial_supply = int(FixedPoint(config.target_liquidity))
     initial_share_price = to_fixed_point(config.init_share_price)
     checkpoint_duration = 86400  # seconds = 1 day
-    checkpoints_per_term = 365
 
-    time_stretch = to_fixed_point(1 / hyperdrive.time_stretch_constant)
+    time_stretch = int(FixedPoint("1.0") / hyperdrive_sim.time_stretch_constant)
     curve_fee = to_fixed_point(config.curve_fee_multiple)
     flat_fee = to_fixed_point(config.flat_fee_multiple)
     gov_fee = 0
 
-    # Deploy hyperdrive on the chain
-    hyperdrive_address = deployer.deploy(
-        project.MockHyperdriveTestnet,  # type:ignore
+    # Deploy hyperdrive
+    position_duration_seconds = int(position_duration_days) * 24 * 60 * 60
+
+    initial_apr = 50000000000000000
+    share_price = 1000000000000000000
+    position_duration = 31536000
+    checkpoint_duration = 86400
+    time_stretch = 22186877016851913475
+    curve_fee = 0
+    flat_fee = 0
+    gov_fee = 0
+
+    hyperdrive_data_provider_contract = deployer.deploy(
+        project.MockHyperdriveDataProviderTestnet,  # type: ignore
         base_erc20,
         initial_apr,
-        initial_share_price,
-        checkpoints_per_term,
+        share_price,
+        position_duration,
         checkpoint_duration,
-        time_stretch,
+        int(time_stretch),
         (curve_fee, flat_fee, gov_fee),
         deployer,
     )
-    hyperdrive_contract: ContractInstance = project.MockHyperdriveTestnet.at(hyperdrive_address)  # type:ignore
+    hyperdrive_contract = deployer.deploy(
+        project.MockHyperdriveTestnet,  # type: ignore
+        hyperdrive_data_provider_contract,
+        base_erc20,
+        initial_apr,
+        share_price,
+        position_duration,
+        checkpoint_duration,
+        int(time_stretch),
+        (curve_fee, flat_fee, gov_fee),
+        deployer,
+    )
+    hyperdrive_data_contract: ContractInstance = project.MockHyperdriveDataProviderTestnet.at(hyperdrive_contract.address)  # type: ignore
 
     # TODO: do this in test functions.
     # Initialize hyperdrive
+    print(f"{float(FixedPoint(initial_supply))=}")
+    print(f"{float(FixedPoint(initial_apr))=}")
     with ape.accounts.use_sender(deployer):
-        base_erc20.approve(hyperdrive, initial_supply)  # type:ignore
+        tx_receipt: ReceiptAPI = base_erc20.mint(initial_supply, sender=deployer)  # type: ignore
+        tx_receipt: ReceiptAPI = base_erc20.approve(hyperdrive_contract, initial_supply)  # type:ignore
         hyperdrive_contract.initialize(initial_supply, initial_apr, deployer, True)  # type:ignore
+    hyperdrive_sim.initialize(sim_agents[0].wallet.address, FixedPoint(initial_supply), FixedPoint(initial_apr))
 
     # Execute trades
     genesis_block_number = ape.chain.blocks[-1].number
     genesis_timestamp = ape.chain.provider.get_block(genesis_block_number).timestamp  # type:ignore
 
     return (
+        config,
         provider,
         sim_agents,
         sol_agents,
-        base_address,
-        fixed_math_address,
-        hyperdrive,
+        base_erc20_contract,
+        fixed_math_contract,
+        hyperdrive_sim,
+        hyperdrive_data_contract,
         hyperdrive_contract,
         genesis_timestamp,
         genesis_block_number,
     )
+
+
+class TestInitialize(unittest.TestCase):
+    """Test case for initializing the market"""
+
+    def test_market_initialization(self):
+        """Verify both markets initialized correctly."""
+
+        (
+            config,
+            provider,
+            sim_agents,
+            sol_agents,
+            base_erc20_contract,
+            fixed_math_contract,
+            hyperdrive_sim,
+            hyperdrive_data_contract,
+            hyperdrive_contract,
+            genesis_timestamp,
+            genesis_block_number,
+        ) = get_fixture()
+
+        market_state_sol = get_simulation_market_state_from_contract(
+            hyperdrive_contract=hyperdrive_data_contract,
+            agent_address=sol_agents[0].address,
+            position_duration_seconds=int(hyperdrive_sim.position_duration.days) * 24 * 60 * 60,
+            checkpoint_duration_days=int(hyperdrive_sim.market_state.checkpoint_duration_days),
+            variable_apr=int(hyperdrive_sim.market_state.variable_apr),
+            config=config,
+        )
+        print(f"\n{market_state_sol=}")
+        print(f"\n{hyperdrive_sim.market_state=}")
+
+        self.assertAlmostEqual(market_state_sol.share_reserves, float(hyperdrive_sim.market_state.share_reserves))
+        # TODO: figure out why these are different!
+        # self.assertAlmostEqual(market_state_sol.bond_reserves, float(hyperdrive_sim.market_state.bond_reserves))
+        self.assertAlmostEqual(market_state_sol.lp_total_supply, float(hyperdrive_sim.market_state.lp_total_supply))
