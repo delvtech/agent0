3f1b09dd328ccf56e38e83f3034c4ae49ecb1d8c
Dylan <dpaiton@gmail.com>
2023-05-23 14:01:35 -0700

    reorganizes tests and adds simple comparison tests

--- tests/fixed_point/test_fixed_point.py
+++ tests/fixed_point/test_fixed_point.py
@@ -1,4 +1,4 @@
-"""Tests for the FixedPoint data type"""
+"""Tests for the FixedPoint class methods"""
 import math
 import unittest
 
@@ -9,20 +9,11 @@ from elfpy.math import FixedPoint
 
 
 class TestFixedPoint(unittest.TestCase):
-    r"""Unit tests to verify that the fixed-point integer implementations are correct.
-    The tests assume everything is in 1e18 precision.
+    r"""Unit tests to verify that the FixedPoint class methods are correct."""
 
-    ..note::
-        Note that `ln` and `pow` require lower precision.
-        Additionally, the approximations used for fixed-point arithmetic
-        are less accurate the closer one gets to the maximum bounds.
-
-        One common tripping point is that integer inputs result in different behavior than float.
-        So, in Python ints and floats are designed such that `1 == 1.0 # true`.
-        But with fixed-point (fp) types, `fp(1) != fp(1.0)`.
-        Instead, `fp(1) == 1e-18`, while `fp(1.0) == 1e18`.
-
-    """
+    INF = FixedPoint("inf")
+    NEG_INF = FixedPoint("-inf")
+    NAN = FixedPoint("nan")
 
     def test_init(self):
         r"""Test initialization for FixedPoint numbers"""
@@ -122,245 +113,6 @@ class TestFixedPoint(unittest.TestCase):
         assert str(FixedPoint(5.0)) == "5.0"
         assert str(FixedPoint(1.5)) == "1.5"
 
-    def test_add(self):
-        r"""Test `+` sugar for various type combos"""
-        # int + int
-        assert FixedPoint(5) + FixedPoint(5) == FixedPoint(10)
-        assert int(FixedPoint(5) + FixedPoint(5)) == 10
-        # int + float
-        assert int(FixedPoint(5) + FixedPoint(5.0)) == 5 + 5 * 10**18
-        # float + float
-        assert FixedPoint(5.0) + FixedPoint(5.0) == FixedPoint(10.0)
-        assert float(FixedPoint(5.0) + FixedPoint(5.0)) == 10.0
-        # float + int
-        assert int(FixedPoint(5.0) + FixedPoint(5)) == 5 * 10**18 + 5
-        # str + str
-        assert FixedPoint("5.0") + FixedPoint("5.025") == FixedPoint("10.025")
-        assert float(FixedPoint("5.0") + FixedPoint("5.0")) == 10.0
-
-    def test_add_fail(self):
-        r"""Test failure of `+` sugar
-
-        We are ignoring type errors -- we know they're bad, but we're looking for failure
-        """
-        with self.assertRaises(OverflowError):
-            _ = FixedPoint(2**256) + FixedPoint(1)
-        fixed_point_value = FixedPoint(1)
-        float_value = 1.0
-        int_value = 1
-        with self.assertRaises(TypeError):
-            _ = fixed_point_value + float_value  # type: ignore
-        with self.assertRaises(TypeError):
-            _ = float_value + fixed_point_value  # type: ignore
-        with self.assertRaises(TypeError):
-            _ = fixed_point_value + int_value  # type: ignore
-        with self.assertRaises(TypeError):
-            _ = int_value + fixed_point_value  # type: ignore
-
-    def test_sub(self):
-        r"""Test `-` sugar for various type combos"""
-        # int - int
-        assert FixedPoint(5) - FixedPoint(4) == FixedPoint(1)
-        assert FixedPoint(5) - FixedPoint(5) == FixedPoint(0)
-        assert int(FixedPoint(5) - FixedPoint(4)) == 1
-        # float - float
-        assert FixedPoint(5.0) - FixedPoint(4.0) == FixedPoint(1.0)
-        assert FixedPoint(5.0) - FixedPoint(5.0) == FixedPoint(0.0)
-        assert float(FixedPoint(5.0) - FixedPoint(4.0)) == 1.0
-        # str - str
-        assert FixedPoint("5.0") - FixedPoint("4.0") == FixedPoint("1.0")
-        assert float(FixedPoint("5.0") - FixedPoint("0.025")) == 4.975
-
-    def test_sub_fail(self):
-        r"""Test failure of `-` sugar
-
-        We are ignoring type errors -- we know they're bad, but we're looking for failure
-        """
-        fixed_point_value = FixedPoint(1)
-        float_value = 1.0
-        int_value = 1
-        with self.assertRaises(TypeError):
-            _ = fixed_point_value - float_value  # type: ignore
-        with self.assertRaises(TypeError):
-            _ = float_value - fixed_point_value  # type: ignore
-        with self.assertRaises(TypeError):
-            _ = fixed_point_value - int_value  # type: ignore
-        with self.assertRaises(TypeError):
-            _ = int_value - fixed_point_value  # type: ignore
-
-    def test_multiply(self):
-        r"""Test `*` sugar for various type combos"""
-        # int * int
-        # NOTE: multiply divides by 1e18, so this is 5 * 5 / 1e18 which rounds to zero
-        for scale in range(0, 8):
-            assert int(FixedPoint(5 * 10**scale) * FixedPoint(5 * 10**scale)) == 0
-        assert int(FixedPoint(5 * 10**9) * FixedPoint(5 * 10**9)) == 25
-        assert int(FixedPoint(5 * 10**10) * FixedPoint(5 * 10**10)) == 2500
-        assert int(FixedPoint(5)) * int(FixedPoint(5)) == 25
-        assert int(FixedPoint(5)) * 5 == 25
-        assert FixedPoint(5 * 10**18) * FixedPoint(5 * 10**18) == FixedPoint(25 * 10**18)
-        # int * float
-        assert int(FixedPoint(5) * FixedPoint(5.0)) == 25
-        # float * float
-        assert FixedPoint(0.1) * FixedPoint(0.1) == FixedPoint(0.01)
-        assert FixedPoint(1e-9) * FixedPoint(1e-9) == FixedPoint(1e-18)
-        assert FixedPoint(1e-10) * FixedPoint(1e-10) == FixedPoint(0)
-        assert FixedPoint(5.0) * FixedPoint(5.0) == FixedPoint(25.0)
-        assert float(FixedPoint(5.0) * FixedPoint(5.0)) == 25.0
-        # float * int
-        assert int(FixedPoint(5.0) * FixedPoint(5)) == 25  # 5e18 * 5e-18 = 25e0
-        assert float(FixedPoint(5.0)) * 5 == 25.0
-        # str * str * str
-        assert FixedPoint("5.0") * FixedPoint("5.0") * FixedPoint("0.1") == FixedPoint("2.50")
-
-    def test_multiply_fail(self):
-        r"""Test failure of `*` sugar
-
-        We are ignoring type errors -- we know they're bad, but we're looking for failure
-        """
-        fixed_point_value = FixedPoint(1)
-        float_value = 1.0
-        int_value = 1
-        with self.assertRaises(TypeError):
-            _ = fixed_point_value * float_value  # type: ignore
-        with self.assertRaises(TypeError):
-            _ = float_value * fixed_point_value  # type: ignore
-        with self.assertRaises(TypeError):
-            _ = fixed_point_value * int_value  # type: ignore
-        with self.assertRaises(TypeError):
-            _ = int_value * fixed_point_value  # type: ignore
-
-    def test_divide(self):
-        r"""Test `/` sugar for various type combos"""
-        # int / int
-        assert int(FixedPoint(5) / FixedPoint(5)) == 1 * 10**18  # 1 * 10**18 is "1" in FP world
-        assert int(FixedPoint(5) / FixedPoint(5)) == 1 * 10**18
-        assert int(FixedPoint(5)) / 5 == 1
-        assert 5 / int(FixedPoint(5)) == 1
-        assert float(FixedPoint(5)) / 5 == 1 * 10**-18
-        # scaling both numerator & denominator shouldn't change the outcome
-        assert FixedPoint(5) / FixedPoint(7) == FixedPoint(714285714285714285)
-        assert FixedPoint(50) / FixedPoint(70) == FixedPoint(714285714285714285)
-        assert FixedPoint(500) / FixedPoint(700) == FixedPoint(714285714285714285)
-        # int / float
-        assert int(FixedPoint(5) / FixedPoint(5.0)) == 1  # 5e-18 / 5e18 = (5/5) * 10 ** (-18+18) = 1
-        # float / float
-        assert FixedPoint(5.0) / FixedPoint(5.0) == FixedPoint(1.0)
-        assert float(FixedPoint(5.0) / FixedPoint(5.0)) == 1.0
-        # float / int
-        assert int(FixedPoint(1.0) / FixedPoint(1)) == 1 * 10**36  # 1e18 / 1e-18 = 1e36
-        # str / str
-        assert FixedPoint("5.0") / FixedPoint("5.0") == FixedPoint("1.0")
-        assert float(FixedPoint("6.0") / FixedPoint("2.5")) == 2.4
-        assert float(FixedPoint("6.0") / FixedPoint("100.0")) == 0.06
-        assert float(FixedPoint("0.006") / FixedPoint("0.001")) == 6
-        # div rounding
-        assert FixedPoint(2.0) / FixedPoint(1 * 10**37) == FixedPoint(0)
-        assert FixedPoint(2.0).div_up(FixedPoint(1 * 10**37)) == FixedPoint(1)
-
-    def test_divide_fail(self):
-        r"""Test failure of `/` sugar
-
-        We are ignoring type errors -- we know they're bad, but we're looking for failure
-        """
-        fixed_point_value = FixedPoint(1)
-        fixed_point_zero = FixedPoint(0)
-        float_value = 1.0
-        int_value = 1
-        with self.assertRaises(TypeError):
-            _ = fixed_point_value / float_value  # type: ignore
-        with self.assertRaises(TypeError):
-            _ = float_value / fixed_point_value  # type: ignore
-        with self.assertRaises(TypeError):
-            _ = fixed_point_value / int_value  # type: ignore
-        with self.assertRaises(TypeError):
-            _ = int_value / fixed_point_value  # type: ignore
-        with self.assertRaises(errors.DivisionByZero):
-            _ = fixed_point_value / fixed_point_zero
-
-    def test_floor_divide(self):
-        r"""Test `//` sugar"""
-        assert FixedPoint("6.3") // FixedPoint("2.0") == FixedPoint("3.0")
-        assert FixedPoint("8.0") // FixedPoint("2.0") == FixedPoint("4.0")
-        assert FixedPoint("8.0") // FixedPoint("5.0") == FixedPoint("1.0")
-        assert FixedPoint("0.5") // FixedPoint("0.2") == FixedPoint("2.0")
-
-    def test_power(self):
-        r"""Test `**` sugar"""
-        # power zero
-        assert int(FixedPoint(5.0) ** FixedPoint(0)) == 1 * 10**18
-        assert int(FixedPoint(5) ** FixedPoint(0)) == 1 * 10**18
-        assert float(FixedPoint(5.0) ** FixedPoint(0)) == 1
-        assert float(FixedPoint(5) ** FixedPoint(0)) == 1
-        # power one
-        self.assertAlmostEqual(int(FixedPoint(5.0) ** FixedPoint(1.0)), int(FixedPoint(5.0)), delta=5)
-        self.assertAlmostEqual(
-            int(FixedPoint(5 * 10**18) ** FixedPoint(1 * 10**18)), int(FixedPoint(5 * 10**18)), delta=5
-        )
-        # power two
-        self.assertAlmostEqual(int(FixedPoint(5.0) ** FixedPoint(2.0)), int(FixedPoint(25.0)), delta=30)
-        self.assertAlmostEqual(
-            int(FixedPoint(5 * 10**18) ** FixedPoint(2 * 10**18)), int(FixedPoint(25.0)), delta=30
-        )
-
-    def test_modulo(self):
-        r"""Test '%' syntax for various type combos"""
-        # int % int
-        assert FixedPoint(5) % FixedPoint(2) == FixedPoint(1)
-        assert FixedPoint(9) % FixedPoint(3) == FixedPoint(0)
-        assert FixedPoint(10) % FixedPoint(3) == FixedPoint(1)
-        assert FixedPoint(15) % FixedPoint(4) == FixedPoint(3)
-        assert FixedPoint(17) % FixedPoint(12) == FixedPoint(5)
-        assert FixedPoint(240) % FixedPoint(13) == FixedPoint(6)
-        assert FixedPoint(10) % FixedPoint(16) == FixedPoint(10)
-        assert FixedPoint(17) % FixedPoint(12) == FixedPoint(5)
-        assert FixedPoint(-17) % FixedPoint(3) == FixedPoint(1)
-        assert FixedPoint(8) % FixedPoint(-3) == FixedPoint(-1)
-        assert FixedPoint(37) % FixedPoint(-5) == FixedPoint(-3)
-        assert FixedPoint(37) % FixedPoint(1) == FixedPoint(0)
-        # int % float
-        assert float(FixedPoint(5) % FixedPoint(2.0)) == 5e-18
-        assert float(FixedPoint(9) % FixedPoint(3.5)) == 9e-18
-        # float % float
-        assert float(FixedPoint(5.0) % FixedPoint(2.0)) == 1.0
-        assert float(FixedPoint(9.0) % FixedPoint(3.5)) == 2.0
-        # float % int
-        assert float(FixedPoint(5.0) % FixedPoint(2)) == 0.0
-        assert float(FixedPoint(9.0) % FixedPoint(3)) == 0.0
-        # str % str
-        assert float(FixedPoint("37.0") % FixedPoint("1.0")) == 0.0
-        assert float(FixedPoint("5.0") % FixedPoint("2.0")) == 1.0
-        assert float(FixedPoint("9.0") % FixedPoint("3.5")) == 2.0
-        assert float(FixedPoint("6.0") % FixedPoint("2.5")) == 1.0
-        assert float(FixedPoint("6.0") % FixedPoint("100.0")) == 6.0
-        assert float(FixedPoint("0.006") % FixedPoint("0.001")) == 0.0
-        assert FixedPoint("12.5") % FixedPoint("5.5") == FixedPoint("1.5")
-        assert FixedPoint("17.0") % FixedPoint("12.0") == FixedPoint("5.0")
-        assert FixedPoint("13.3") % FixedPoint("1.1") == FixedPoint("0.1")
-        assert FixedPoint("-17.0") % FixedPoint("3.0") == FixedPoint("1.0")
-        assert FixedPoint("8.0") % FixedPoint("-3.0") == FixedPoint("-1.0")
-        assert FixedPoint("37.0") % FixedPoint("-5.0") == FixedPoint("-3.0")
-
-    def test_modulo_fail(self):
-        r"""Test failure of `%` sugar
-
-        We are ignoring type errors -- we know they're bad, but we're looking for failure
-        """
-        fixed_point_value = FixedPoint(1)
-        fixed_point_zero = FixedPoint(0)
-        float_value = 1.0
-        int_value = 1
-        with self.assertRaises(TypeError):
-            _ = fixed_point_value % float_value  # type: ignore
-        with self.assertRaises(TypeError):
-            _ = float_value % fixed_point_value  # type: ignore
-        with self.assertRaises(TypeError):
-            _ = fixed_point_value % int_value  # type: ignore
-        with self.assertRaises(TypeError):
-            _ = int_value % fixed_point_value  # type: ignore
-        with self.assertRaises(errors.DivisionByZero):
-            _ = fixed_point_value % fixed_point_zero
-
     def test_divmod(self):
         r"""Test `divmod` support"""
         assert divmod(FixedPoint(5), FixedPoint(7)) == (FixedPoint(5) // FixedPoint(7), FixedPoint(5) % FixedPoint(7))
@@ -457,3 +209,9 @@ class TestFixedPoint(unittest.TestCase):
         assert hash(FixedPoint("-2.5")) == hash(-2.5 * 10**18)
         assert hash(FixedPoint("2.5")) == hash(2.5 * 10**18)
         assert hash(FixedPoint("-200.537280")) == hash(-200.537280 * 10**18)
+
+    def test_hash_nonfinite(self):
+        """Test the hash method with nonfinite values"""
+        assert hash(self.INF) == hash(float("inf"))
+        assert hash(self.NEG_INF) == hash(float("-inf"))
+        assert hash(self.NAN) == hash(float("nan"))
--- tests/fixed_point/test_fixed_point_math.py
+++ tests/fixed_point/test_fixed_point_math.py
@@ -4,6 +4,8 @@ import unittest
 
 from elfpy.math import FixedPointIntegerMath, FixedPointMath, FixedPoint
 
+# pylint: disable=unneeded-not
+
 
 class TestFixedPointIntegerMath(unittest.TestCase):
     """Unit tests to verify that the fixed-point integer implementations are correct.
@@ -287,6 +289,12 @@ class TestFixedPointIntegerMath(unittest.TestCase):
 class TestFixedPointMath(unittest.TestCase):
     """Unit tests to verify that the fixed-point math implementations are correct."""
 
+    ONE = FixedPoint("1.0")
+    NEG_ONE = FixedPoint("-1.0")
+    INF = FixedPoint("inf")
+    NEG_INF = FixedPoint("-inf")
+    NAN = FixedPoint("nan")
+
     def test_minimum(self):
         """Test minimum function"""
         assert FixedPointMath.minimum(0, 1) == 0
@@ -296,6 +304,14 @@ class TestFixedPointMath(unittest.TestCase):
         assert FixedPointMath.minimum(1.0, 3.0) == 1.0
         assert FixedPointMath.minimum(FixedPoint(1.0), FixedPoint(3.0)) == FixedPoint(1.0)
         assert FixedPointMath.minimum(FixedPoint("3.0"), FixedPoint(int(3e18 - 1e-17))) == FixedPoint(int(3e18 - 1e-17))
+
+    def test_minimum_nonfinite(self):
+        """Test minimum method"""
+        assert FixedPointMath.minimum(self.NAN, self.NEG_ONE).is_nan() is True
+        assert FixedPointMath.minimum(self.NAN, self.INF).is_nan() is True
+        assert FixedPointMath.minimum(self.ONE, self.INF) == self.ONE
+        assert FixedPointMath.minimum(self.NEG_ONE, self.NEG_INF) == self.NEG_INF
+        assert FixedPointMath.minimum(self.INF, self.NEG_INF) == self.NEG_INF
 
     def test_maximum(self):
         """Test maximum function"""
@@ -307,30 +323,38 @@ class TestFixedPointMath(unittest.TestCase):
         assert FixedPointMath.maximum(FixedPoint(1.0), FixedPoint(3.0)) == FixedPoint(3.0)
         assert FixedPointMath.maximum(FixedPoint("3.0"), FixedPoint(int(3e18 - 1e-17))) == FixedPoint(3.0)
 
+    def test_maximum_nonfinite(self):
+        """Test maximum method"""
+        assert FixedPointMath.maximum(self.NAN, self.NEG_ONE).is_nan() is True
+        assert FixedPointMath.maximum(self.NAN, self.INF).is_nan() is True
+        assert FixedPointMath.maximum(self.ONE, self.INF) == self.INF
+        assert FixedPointMath.maximum(self.NEG_ONE, self.NEG_INF) == self.NEG_ONE
+        assert FixedPointMath.maximum(self.INF, self.NEG_INF) == self.INF
+
     def test_exp(self):
         """Test exp function"""
         tolerance = 1e-18
-
         result = FixedPointMath.exp(FixedPoint("1.0"))
         expected = FixedPoint(2718281828459045235)
         assert math.isclose(result, expected, rel_tol=tolerance), f"exp(x):\n  {result=},\n{expected=}"
-
         result = FixedPointMath.exp(FixedPoint("-1.0"))
         expected = FixedPoint(367879441171442321)
         assert math.isclose(result, expected, rel_tol=tolerance), f"exp(x):\n  {result=},\n{expected=}"
-
         result = FixedPointMath.exp(1)
         expected = int(math.exp(1))
         assert math.isclose(result, expected, rel_tol=tolerance), f"exp(x):\n  {result=},\n{expected=}"
-
         result = FixedPointMath.exp(-1)
         expected = int(math.exp(-1))
         assert math.isclose(result, expected, rel_tol=tolerance), f"exp(x):\n  {result=},\n{expected=}"
-
         result = FixedPointMath.exp(1.0)
         expected = math.exp(1.0)
         assert math.isclose(result, expected, rel_tol=tolerance), f"exp(x):\n  {result=},\n{expected=}"
-
         result = FixedPointMath.exp(-1.0)
         expected = math.exp(-1.0)
         assert math.isclose(result, expected, rel_tol=tolerance), f"exp(x):\n  {result=},\n{expected=}"
+
+    def test_exp_nonfinite(self):
+        """Test exp method"""
+        assert FixedPointMath.exp(self.NAN).is_nan() is True
+        assert FixedPointMath.exp(self.INF) == self.INF
+        assert FixedPointMath.exp(self.NEG_INF) == self.NEG_INF
