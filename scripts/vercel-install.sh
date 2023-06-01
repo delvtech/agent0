4495e56a64a5f096bdc3e0d70aac40a0d4aa8d21
Sheng Lundquist <sheng@delv.tech>
2023-06-01 12:48:34 -0700

    Trying upgrading setuptools

--- scripts/vercel-install.sh
+++ scripts/vercel-install.sh
@@ -2,12 +2,12 @@ python3 --version
 which python3
 python3 -m ensurepip
 python3 -m pip install --upgrade pip
+python3 -m pip install --upgrade setuptools
 python3 -m pip install -r requirements.txt
 python3 -m pip install -r requirements-dev.txt
 # Needed for jupyter nbconvert
 python3 -m pip install ipykernel
 # For some reason kernel is missing libsqlite3
-apt-get install libsqlite3-dev
-python3 -m pip install pysqlite
+# python3 -m pip install pysqlite3
 # Install elfpy
 python3 -m pip install .
