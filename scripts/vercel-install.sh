62e9ef787d75b599505139f1af263e41187a1d21
Sheng Lundquist <sheng@delv.tech>
2023-06-01 12:43:39 -0700

    Adding libsqlite3-dev for fixing error

--- scripts/vercel-install.sh
+++ scripts/vercel-install.sh
@@ -6,4 +6,8 @@ python3 -m pip install -r requirements.txt
 python3 -m pip install -r requirements-dev.txt
 # Needed for jupyter nbconvert
 python3 -m pip install ipykernel
+# For some reason kernel is missing libsqlite3
+apt-get install libsqlite3-dev
+python3 -m pip install pysqlite
+# Install elfpy
 python3 -m pip install .
