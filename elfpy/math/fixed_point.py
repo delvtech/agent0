5bd88798e86f33b2258e63e908aa5dad9e3fa658
Sheng Lundquist <sheng@delv.tech>
2023-05-31 19:09:07 -0700

    Using shallow copies of FixedPoint for immutable FixedPoint object for performance (#495)
    * Using shallow copies of fixed_point for immutable fixed_point objects for performance

--- elfpy/math/fixed_point.py
+++ elfpy/math/fixed_point.py
@@ -595,6 +595,11 @@ class FixedPoint:
         # act like an integer otherwise
         return hash((self.scaled_value, self.__class__.__name__))
 
+    # Since fixedpoint objects are immutable, simply return a shallow copy here for performance
+    def __deepcopy__(self, memo) -> FixedPoint:
+        memo[id(self)] = self
+        return self
+
     # additional arethmitic & helper functions
     def div_up(self, other: OtherTypes | FixedPoint) -> FixedPoint:
         r"""Divide self by other, rounding up"""
