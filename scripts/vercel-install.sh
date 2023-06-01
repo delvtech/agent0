a4f47f194d71196b6fe94dff9f72fb6f9718b496
Sheng Lundquist <sheng@delv.tech>
2023-06-01 13:03:19 -0700

    Trying installing jupyter

--- scripts/vercel-install.sh
+++ scripts/vercel-install.sh
@@ -5,9 +5,6 @@ python3 -m pip install --upgrade pip
 python3 -m pip install -r requirements.txt
 python3 -m pip install -r requirements-dev.txt
 # Needed for jupyter nbconvert
-python3 -m pip install ipykernel
-# For some reason python is missing libsqlite3
-yum install sqlite-devel -y
-python3 -m pip install pysqlite3
+python3 -m pip install jupyter
 # Install elfpy
 python3 -m pip install .
