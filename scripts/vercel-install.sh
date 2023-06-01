d43d135b7addeb9b1cd38b7d7506c4a3154688ed
Sheng Lundquist <sheng@delv.tech>
2023-06-01 12:58:27 -0700

    Trying to install sqlite

--- scripts/vercel-install.sh
+++ scripts/vercel-install.sh
@@ -2,12 +2,12 @@ python3 --version
 which python3
 python3 -m ensurepip
 python3 -m pip install --upgrade pip
-python3 -m pip install --upgrade setuptools
 python3 -m pip install -r requirements.txt
 python3 -m pip install -r requirements-dev.txt
 # Needed for jupyter nbconvert
 python3 -m pip install ipykernel
-# For some reason kernel is missing libsqlite3
-# python3 -m pip install pysqlite3
+# For some reason python is missing libsqlite3
+yum install sqlite-devel -y
+python3 -m pip install pysqlite3
 # Install elfpy
 python3 -m pip install .
