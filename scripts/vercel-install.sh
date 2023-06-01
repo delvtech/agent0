7ed31c54c3ddbb63c0b1d1dde9e47ad6c0fc8326
Sheng Lundquist <sheng@delv.tech>
2023-06-01 12:36:08 -0700

    Installing another dependency

--- scripts/vercel-install.sh
+++ scripts/vercel-install.sh
@@ -5,4 +5,5 @@ python3 -m pip install --upgrade pip
 python3 -m pip install -r requirements.txt
 python3 -m pip install -r requirements-dev.txt
 # Needed for jupyter nbconvert
+python3 -m pip install pysqlite3
 python3 -m pip install ipykernel
