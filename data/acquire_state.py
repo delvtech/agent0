183f293a6fb92b04705603ce60934bf07ab201df
Dylan Paiton <dpaiton@users.noreply.github.com>
2023-06-09 14:24:40 -0500

    adds block time to pool info output (#539)

--- data/acquire_state.py
+++ data/acquire_state.py
@@ -15,6 +15,7 @@ from web3.contract import Contract  # type: ignore=reportPrivateImportUsage
 from web3.contract.contract import ContractFunction
 from web3.datastructures import AttributeDict, MutableAttributeDict
 from web3.middleware import geth_poa
+from web3.types import BlockData
 
 from elfpy.utils import apeworx_integrations as ape_utils
 
@@ -36,6 +37,8 @@ class ExtendedJSONEncoder(json.JSONEncoder):
 
 def main(ethereum_node: URI | str, hyperdrive_abi_file_path: str, contracts_url: str, output_location: str):
     """Main entry point for accessing contract & writing pool info"""
+    # pylint: disable=too-many-locals
+
     web3 = setup_web3(ethereum_node)
 
     # Send a request to the local server to fetch the deployed contract addresses and
@@ -55,17 +58,27 @@ def main(ethereum_node: URI | str, hyperdrive_abi_file_path: str, contracts_url:
     with open(contract_file, mode="w", encoding="UTF-8") as file:
         json.dump(pool_config, file, indent=2, cls=ExtendedJSONEncoder)
 
-    block_number: int = web3.eth.get_block_number()
-    latest_block_number: int = block_number
+    # write the initial pool info
     pool_info = {}
+    block: BlockData = web3.eth.get_block("latest")
+    block_number: int = block.number
+    latest_block_number: int = block_number
+    latest_block_timestamp: BlockData = web3.eth.get_block("latest").timestamp
     block_pool_info = get_smart_contract_read_call(hyperdrive_contract, "getPoolInfo")
+    block_pool_info.update({"timestamp": latest_block_timestamp})
+    pool_info[latest_block_number] = block_pool_info
+    with open(contract_file, mode="w", encoding="UTF-8") as file:
+        json.dump(pool_info, file, indent=2, cls=ExtendedJSONEncoder)
+    # monitor for new blocks & add pool info per block
     while True:
         latest_block_number: int = web3.eth.get_block_number()
         # if we are on a new block
         if latest_block_number != block_number:
             block_number: int = latest_block_number
+            latest_block_timestamp: BlockData = web3.eth.get_block("latest").timestamp
             # get pool info from hyperdrive contract
             block_pool_info = get_smart_contract_read_call(hyperdrive_contract, "getPoolInfo")
+            block_pool_info.update({"timestamp": latest_block_timestamp})
             pool_info[latest_block_number] = block_pool_info
             contract_file = os.path.join(output_location, "hyperdrive_pool_info.json")
             with open(contract_file, mode="w", encoding="UTF-8") as file:
