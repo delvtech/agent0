cc607e277d1ff872faf71c8221bdaa4dedb44aab
Dylan <dpaiton@gmail.com>
2023-06-10 17:32:21 -0500

    move functions & classes around

--- data/acquire_state.py
+++ data/acquire_state.py
@@ -24,6 +24,16 @@ from elfpy.utils import apeworx_integrations as ape_utils
 # pyright: reportTypedDictNotRequiredAccess=false, reportUnboundVariable=false
 
 
+@attr.s
+class HyperdriveAddressesJson:
+    """Addresses for deployed Hyperdrive contracts."""
+
+    # pylint: disable=too-few-public-methods
+
+    hyperdrive: str = attr.ib()
+    base_token: str = attr.ib()
+
+
 class ExtendedJSONEncoder(json.JSONEncoder):
     """Overrides json encoder to handle hex inputs"""
 
@@ -33,59 +43,6 @@ class ExtendedJSONEncoder(json.JSONEncoder):
         if isinstance(o, (AttributeDict, MutableAttributeDict)):
             return dict(o)
         return super().default(o)
-
-
-def main(ethereum_node: URI | str, hyperdrive_abi_file_path: str, contracts_url: str, output_location: str):
-    """Main entry point for accessing contract & writing pool info"""
-    # pylint: disable=too-many-locals
-
-    web3 = setup_web3(ethereum_node)
-
-    # Send a request to the local server to fetch the deployed contract addresses and
-    # load the deployed Hyperdrive contract addresses from the server response
-    addresses = fetch_addresses(contracts_url)
-
-    # get contract instance of hyperdrive
-    with open(hyperdrive_abi_file_path, "r", encoding="UTF-8") as file:
-        abi = json.load(file)["abi"]
-    hyperdrive_contract: Contract = web3.eth.contract(
-        address=address.to_checksum_address(addresses.hyperdrive), abi=abi
-    )
-
-    # get pool config from hyperdrive contract
-    pool_config = get_smart_contract_read_call(hyperdrive_contract, "getPoolConfig")
-    contract_file = os.path.join(output_location, "hyperdrive_config.json")
-    with open(contract_file, mode="w", encoding="UTF-8") as file:
-        json.dump(pool_config, file, indent=2, cls=ExtendedJSONEncoder)
-
-    # write the initial pool info
-    pool_info = {}
-    block: BlockData = web3.eth.get_block("latest")
-    block_number: int = block.number
-    latest_block_number: int = block_number
-    latest_block_timestamp: BlockData = web3.eth.get_block("latest").timestamp
-    block_pool_info = get_smart_contract_read_call(hyperdrive_contract, "getPoolInfo")
-    block_pool_info.update({"timestamp": latest_block_timestamp})
-    pool_info[latest_block_number] = block_pool_info
-    with open(contract_file, mode="w", encoding="UTF-8") as file:
-        json.dump(pool_info, file, indent=2, cls=ExtendedJSONEncoder)
-    # monitor for new blocks & add pool info per block
-    while True:
-        latest_block_number: int = web3.eth.get_block_number()
-        # if we are on a new block
-        if latest_block_number != block_number:
-            # Backfilling for blocks that need updating
-            for block_number in range(block_number + 1, latest_block_number + 1):
-                latest_block_timestamp: BlockData = web3.eth.get_block(block_identifier=block_number).timestamp
-                # get pool info from hyperdrive contract
-                block_pool_info = get_smart_contract_read_call(
-                    hyperdrive_contract, "getPoolInfo", block_identifier=block_number
-                )
-                block_pool_info.update({"timestamp": latest_block_timestamp})
-                pool_info[block_number] = block_pool_info
-            contract_file = os.path.join(output_location, "hyperdrive_pool_info.json")
-            with open(contract_file, mode="w", encoding="UTF-8") as file:
-                json.dump(pool_info, file, indent=2, cls=ExtendedJSONEncoder)
 
 
 def get_smart_contract_read_call(contract: Contract, function_name: str, **function_args):
@@ -114,16 +71,6 @@ def setup_web3(ethereum_node: URI | str) -> Web3:
     return web3_container
 
 
-@attr.s
-class HyperdriveAddressesJson:
-    """Addresses for deployed Hyperdrive contracts."""
-
-    # pylint: disable=too-few-public-methods
-
-    hyperdrive: str = attr.ib()
-    base_token: str = attr.ib()
-
-
 def fetch_addresses(contracts_url: str) -> HyperdriveAddressesJson:
     """Fetch addresses for deployed contracts in the Hyperdrive system."""
     attempt_num = 0
@@ -144,11 +91,59 @@ def fetch_addresses(contracts_url: str) -> HyperdriveAddressesJson:
     return addresses
 
 
-# def get_contract_instance(abi: json):
-#    # Load the deployed contract addresses from the server response
-#    with open(hyperdrive_abi_file_path, "r", encoding="UTF-8") as file:
-#        abi = json.load(file)["abi"]
-#    contract = web3.eth.contract(address=address.to_checksum_address(addresses.hyperdrive), abi=abi)
+def main(ethereum_node: URI | str, hyperdrive_abi_file_path: str, contracts_url: str, output_location: str):
+    """Main entry point for accessing contract & writing pool info"""
+    # pylint: disable=too-many-locals
+
+    web3_container = setup_web3(ethereum_node)
+
+    # Send a request to the local server to fetch the deployed contract addresses and
+    # load the deployed Hyperdrive contract addresses from the server response
+    addresses = fetch_addresses(contracts_url)
+
+    # get contract instance of hyperdrive
+    with open(hyperdrive_abi_file_path, "r", encoding="UTF-8") as file:
+        abi = json.load(file)["abi"]
+    hyperdrive_contract: Contract = web3_container.eth.contract(
+        address=address.to_checksum_address(addresses.hyperdrive), abi=abi
+    )
+
+    # get pool config from hyperdrive contract
+    pool_config = get_smart_contract_read_call(hyperdrive_contract, "getPoolConfig")
+    contract_file = os.path.join(output_location, "hyperdrive_config.json")
+    with open(contract_file, mode="w", encoding="UTF-8") as file:
+        json.dump(pool_config, file, indent=2, cls=ExtendedJSONEncoder)
+
+    # write the initial pool info
+    pool_info = {}
+    block: BlockData = web3_container.eth.get_block("latest")
+    block_number: int = block.number
+    latest_block_number: int = block_number
+    latest_block_timestamp: BlockData = web3_container.eth.get_block("latest").timestamp
+    block_pool_info = get_smart_contract_read_call(hyperdrive_contract, "getPoolInfo")
+    block_pool_info.update({"timestamp": latest_block_timestamp})
+    pool_info[latest_block_number] = block_pool_info
+    with open(contract_file, mode="w", encoding="UTF-8") as file:
+        json.dump(pool_info, file, indent=2, cls=ExtendedJSONEncoder)
+    # monitor for new blocks & add pool info per block
+    while True:
+        latest_block_number: int = web3_container.eth.get_block_number()
+        # if we are on a new block
+        if latest_block_number != block_number:
+            # Backfilling for blocks that need updating
+            for block_number in range(block_number + 1, latest_block_number + 1):
+                latest_block_timestamp: BlockData = web3_container.eth.get_block(
+                    block_identifier=block_number
+                ).timestamp
+                # get pool info from hyperdrive contract
+                block_pool_info = get_smart_contract_read_call(
+                    hyperdrive_contract, "getPoolInfo", block_identifier=block_number
+                )
+                block_pool_info.update({"timestamp": latest_block_timestamp})
+                pool_info[block_number] = block_pool_info
+            contract_file = os.path.join(output_location, "hyperdrive_pool_info.json")
+            with open(contract_file, mode="w", encoding="UTF-8") as file:
+                json.dump(pool_info, file, indent=2, cls=ExtendedJSONEncoder)
 
 
 if __name__ == "__main__":
