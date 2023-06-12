4debf62dc8fc56e19990041780ad47690953e6bd
Dylan <dpaiton@gmail.com>
2023-06-12 14:24:14 -0700

    simplify main

--- elfpy/data/acquire_state.py
+++ elfpy/data/acquire_state.py
@@ -7,7 +7,7 @@ import os
 
 from eth_typing import URI
 from eth_utils import address
-from web3.contract import Contract  # type: ignore=reportPrivateImportUsage
+from web3.contract.contract import Contract
 from web3.types import BlockData
 
 from elfpy.data import contract_interface
@@ -26,7 +26,7 @@ def main(ethereum_node: URI | str, hyperdrive_abi_file_path: str, contracts_url:
     # send a request to the local server to fetch the deployed contract addresses and
     # load the deployed Hyperdrive contract addresses from the server response
     addresses = contract_interface.fetch_addresses(contracts_url)
-    # get
+    # Load the ABI from the JSON file
     with open(hyperdrive_abi_file_path, "r", encoding="UTF-8") as file:
         abi = json.load(file)["abi"]
     # get contract instance of hyperdrive
@@ -34,21 +34,18 @@ def main(ethereum_node: URI | str, hyperdrive_abi_file_path: str, contracts_url:
         address=address.to_checksum_address(addresses.hyperdrive), abi=abi
     )
     # get pool config from hyperdrive contract
-    pool_config = contract_interface.get_smart_contract_read_call(hyperdrive_contract, "getPoolConfig")
-    contract_file = os.path.join(output_location, "hyperdrive_config.json")
-    logging.info("Writing pool config.")
-    with open(contract_file, mode="w", encoding="UTF-8") as file:
-        json.dump(pool_config, file, indent=2, cls=output_utils.ExtendedJSONEncoder)
+    config_file = os.path.join(output_location, "hyperdrive_config.json")
+    contract_interface.hyperdrive_config_to_json(config_file, hyperdrive_contract)
     # write the initial pool info
-    pool_info = {}
     block: BlockData = web3_container.eth.get_block("latest")
     block_number: int = block.number
     latest_block_number: int = block_number
-    latest_block_timestamp: BlockData = web3_container.eth.get_block("latest").timestamp
-    block_pool_info = contract_interface.get_smart_contract_read_call(hyperdrive_contract, "getPoolInfo")
-    block_pool_info.update({"timestamp": latest_block_timestamp})
-    pool_info[latest_block_number] = block_pool_info
-    with open(contract_file, mode="w", encoding="UTF-8") as file:
+    pool_info = {}
+    pool_info[latest_block_number] = contract_interface.get_block_pool_info(
+        web3_container, hyperdrive_contract, block_number
+    )
+    pool_info_file = os.path.join(output_location, "hyperdrive_pool_info.json")
+    with open(pool_info_file, mode="w", encoding="UTF-8") as file:
         json.dump(pool_info, file, indent=2, cls=output_utils.ExtendedJSONEncoder)
     # monitor for new blocks & add pool info per block
     logging.info("Monitoring for pool info updates...")
@@ -59,17 +56,10 @@ def main(ethereum_node: URI | str, hyperdrive_abi_file_path: str, contracts_url:
             # Backfilling for blocks that need updating
             for block_number in range(block_number + 1, latest_block_number + 1):
                 logging.info("Block %s", block_number)
-                latest_block_timestamp: BlockData = web3_container.eth.get_block(
-                    block_identifier=block_number
-                ).timestamp
-                # get pool info from hyperdrive contract
-                block_pool_info = contract_interface.get_smart_contract_read_call(
-                    hyperdrive_contract, "getPoolInfo", block_identifier=block_number
+                pool_info[block_number] = contract_interface.get_block_pool_info(
+                    web3_container, hyperdrive_contract, block_number
                 )
-                block_pool_info.update({"timestamp": latest_block_timestamp})
-                pool_info[block_number] = block_pool_info
-            contract_file = os.path.join(output_location, "hyperdrive_pool_info.json")
-            with open(contract_file, mode="w", encoding="UTF-8") as file:
+            with open(pool_info_file, mode="w", encoding="UTF-8") as file:
                 json.dump(pool_info, file, indent=2, cls=output_utils.ExtendedJSONEncoder)
 
 
--- elfpy/data/acquire_transactions.py
+++ elfpy/data/acquire_transactions.py
@@ -10,6 +10,7 @@ import requests
 import toml
 from eth_utils import address
 from web3 import Web3
+from web3.contract.contract import Contract
 from web3.middleware import geth_poa
 
 from elfpy.data import contract_interface
@@ -21,7 +22,7 @@ from elfpy.utils import outputs as output_utils
 # pylint: disable=too-many-locals
 
 
-def main(config_file_path: str, contracts_url: str, ethereum_node: str, save_dir: str, abi_file_path: str):
+def main(start_block: int, contracts_url: str, ethereum_node: str, save_dir: str, abi_file_path: str):
     """Main execution entry point"""
     # Define necessary variables/objects
     if not os.path.exists(save_dir):  # create save_dir if necessary
@@ -31,48 +32,34 @@ def main(config_file_path: str, contracts_url: str, ethereum_node: str, save_dir
     with open(abi_file_path, "r") as file:
         abi = json.load(file)["abi"]
     # Connect to the Ethereum node
-    web3_container = Web3(Web3.HTTPProvider(ethereum_node))
-    web3_container.middleware_onion.inject(geth_poa.geth_poa_middleware, layer=0)
+    web3_container = contract_interface.setup_web3(ethereum_node)
+    # send a request to the local server to fetch the deployed contract addresses and
+    # load the deployed Hyperdrive contract addresses from the server response
+    addresses = contract_interface.fetch_addresses(contracts_url)
     # Main loop to fetch transactions continuously
     while True:
-        # Send a request to the local server to fetch the deployed contract addresses
-        response = requests.get(contracts_url, timeout=60)
-        # Check the status code and retry the request if it fails
-        if response.status_code != 200:
-            logging.warning("Request failed with status code %s @ %s", response.status_code, time.ctime())
-            time.sleep(10)
-            continue
-        # Load the deployed contract addresses from the server response
-        depl_addrs = response.json()
-        hyperdrive_address = depl_addrs["hyperdrive"]
-        contract = web3_container.eth.contract(address=address.to_checksum_address(hyperdrive_address), abi=abi)
-        # Load the starting block number from the config file
-        with open(config_file_path, "r") as file:
-            config = toml.load(file)
-            starting_block = config["settings"]["startBlock"]
+        hyperdrive_contract: Contract = web3_container.eth.contract(
+            address=address.to_checksum_address(addresses.hyperdrive), abi=abi
+        )
         # Get the current block number from the Ethereum node
         current_block = web3_container.eth.block_number
         logging.info("Fetching transactions up to block %s", current_block)
         # Fetch transactions related to the hyperdrive_address contract
         transactions = contract_interface.fetch_transactions_for_block_range(
-            web3_container, contract, starting_block, current_block
+            web3_container, hyperdrive_contract, start_block, current_block
         )
         # Save the updated transactions to the output file with custom encoder
         with open(transactions_output_file, "w", encoding="UTF-8") as file:
             json.dump(transactions, file, indent=2, cls=output_utils.ExtendedJSONEncoder)
-        # Update the starting block number in the config file
-        config["settings"]["startBlock"] = current_block
-        # Save the updated config data to the TOML file
-        # save_config(config, config_file_path)
         # Wait for 10 seconds before fetching transactions again
         time.sleep(10)
 
 
 if __name__ == "__main__":
-    CONFIG_FILE_PATH = "elfpy/data/config/dataConfig.toml"
     CONTRACTS_URL = "http://localhost:80/addresses.json"
     ETHEREUM_NODE = "http://localhost:8545"
     SAVE_DIR = ".logging"
     ABI_FILE_PATH = "./hyperdrive_solidity/.build/Hyperdrive.json"
+    START_BLOCK = 0
     output_utils.setup_logging(".logging/acquire_transactions.log", log_file_and_stdout=True)
-    main(CONFIG_FILE_PATH, CONTRACTS_URL, ETHEREUM_NODE, SAVE_DIR, ABI_FILE_PATH)
+    main(START_BLOCK, CONTRACTS_URL, ETHEREUM_NODE, SAVE_DIR, ABI_FILE_PATH)
