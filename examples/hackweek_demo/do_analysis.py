9c07ba664d390f4769b8292c8d1c203c043d827a
Mihai <mcosma@gmail.com>
2023-06-13 06:54:37 -0400

    new files

---
+++ examples/hackweek_demo/do_analysis.py
@@ -0,0 +1,88 @@
+"""Do the analysis."""
+# std lib
+from pathlib import Path
+import time
+from collections import namedtuple
+
+# third party
+import pandas as pd
+import mplfinance as mpf
+import streamlit as st
+
+# local
+from plot_ohlcv import plot_ohlcv, calc_ohlcv
+from plot_fixed_rate import calc_fixed_rate, plot_fixed_rate
+from plot_pnl import calculate_pnl, plot_pnl
+
+SAVE_FOLDER = Path(__file__).parent.parent.parent / ".logging"
+
+# set up streamlit
+st.set_page_config(
+    page_title="Bots dashboard",
+    layout="wide",
+)
+st.set_option("deprecation.showPyplotGlobalUse", False)
+ticker_col, plot_col = st.columns([0.4, 0.6])
+with ticker_col:
+    st.title("🤖 The Robot Takeover")
+    st.header("🚀 Get ready to blast off into Hyperdrive")
+placeholder = st.empty()  # creating a single-element streamlit container
+
+# set up analysis
+fig = mpf.figure(style="mike", figsize=(15, 15))
+DataAxes = namedtuple("DataAxes", ["ohlcv", "fixed_rate", "volume", "pnl"])
+ax = DataAxes(
+    ohlcv=fig.add_subplot(2, 2, 1),
+    fixed_rate=fig.add_subplot(2, 2, 2),
+    volume=fig.add_subplot(2, 2, 3),
+    pnl=fig.add_subplot(2, 2, 4),
+)
+fig.set_tight_layout(True)  # type: ignore
+
+# while True:
+pool_file = SAVE_FOLDER / "pool.csv"
+logs_file = SAVE_FOLDER / "logs.csv"
+pool_df = pd.read_csv(pool_file) if pool_file.exists() else None
+logs_df = pd.read_csv(logs_file) if logs_file.exists() else None
+if pool_df is None or logs_df is None:
+    placeholder.warning("Waiting for data...")
+else:
+    # Calculate OHLCV and fixed rate (in poolinfo)
+    ohlcv = calc_ohlcv(pool_df, freq="5T")
+    fixed_rate_x, fixed_rate_y = calc_fixed_rate(pool_df)
+    pnl_x, pnl_y = calculate_pnl(logs_df)
+    logs_df["base"] = logs_df["baseAmount"].astype(float) / 1e18
+    logs_df["bonds"] = logs_df["bondAmount"].astype(float) / 1e18
+    logs_df["shareReserves"] = round(logs_df["shareReserves"].astype(float) / 1e18)
+    logs_df["bondReserves"] = round(logs_df["bondReserves"].astype(float) / 1e18)
+    logs_df["lpTotalSupply"] = round(logs_df["lpTotalSupply"].astype(float) / 1e18)
+    # Plot reserve levels (share and bond reserves, in poolinfo)
+    # Fix axes labels
+    # Add ticker
+    logs_df.rename(columns={"blockNumber": "block"}, inplace=True)
+    with placeholder.container():
+        with ticker_col:
+            # st.dataframe(logs_df.loc[:-100:-1,["event","value"]])
+            cols = ["event", "block", "base", "bonds", "shareReserves", "bondReserves", "lpTotalSupply"]
+            trades = logs_df.loc[logs_df.event != "TransferSingle", :]
+            st.dataframe(trades.loc[:-100:-1, cols], height=850)
+        with plot_col:
+            for a in ax:  # clear all axes
+                a.clear()
+            plot_ohlcv(ohlcv, ax.ohlcv, ax.volume)
+            plot_fixed_rate(fixed_rate_x, fixed_rate_y, ax.fixed_rate)
+            plot_pnl(pnl_x, pnl_y, ax.pnl)
+            fig.autofmt_xdate()  # type: ignore
+            st.pyplot(fig=fig)  # type: ignore
+
+old_stats_pool = Path.stat(pool_file)
+old_stats_logs = Path.stat(logs_file)
+
+while True:
+    new_stats_pool = Path.stat(pool_file)
+    if new_stats_pool.st_mtime != old_stats_pool.st_mtime:
+        st.experimental_rerun()
+    new_stats_logs = Path.stat(logs_file)
+    if new_stats_logs.st_mtime != old_stats_logs.st_mtime:
+        st.experimental_rerun()
+    time.sleep(0.1)
---
+++ examples/hackweek_demo/do_demo.py
@@ -0,0 +1,127 @@
+"""Script to format on-chain hyperdrive pool, config, and transaction data post-processing"""
+from __future__ import annotations
+
+# std lib
+import logging
+import time
+
+# third party
+from eth_typing import BlockNumber, URI
+from web3 import Web3
+import pandas as pd
+
+# custom
+from extract_data_logs import calculate_spot_price
+from elfpy.data import contract_interface
+from elfpy.utils import outputs as output_utils
+
+# pylint: disable=too-many-arguments, invalid-name
+
+
+def prepare_data(trades, pool):
+    """Prepare data for analysis."""
+    # remove all empty values from dicts
+    trades = {k: v for k, v in trades.items() if v}
+    pool = {k: v for k, v in pool.items() if v}
+    # turn to dataframes
+    pool_df = pd.DataFrame(pool).T
+    pool_df["shareReserves"] = pool_df["shareReserves"].astype(float)
+    pool_df["bondReserves"] = pool_df["bondReserves"].astype(float)
+    pool_df["lpTotalSupply"] = pool_df["lpTotalSupply"].astype(float)
+    pool_df["sharePrice"] = pool_df["sharePrice"].astype(float)
+    pool_df["spot_price"] = calculate_spot_price(
+        share_reserves=pool_df["shareReserves"],
+        bond_reserves=pool_df["bondReserves"],
+        lp_total_supply=pool_df["lpTotalSupply"],
+    )
+    pool_df = pool_df.reset_index(names="blockNumber")
+    txns, logs, receipts = [], [], []
+    for block in trades.values():
+        for txn in block:
+            txns.append(txn["transaction"])  # 1 per txn
+            logs.extend(
+                {k: v for k, v in log.items() if k != "args"} | log["args"]
+                for log in txn["logs"]
+                if log["event"] not in ["Approval", "Initialize"]
+            )
+            receipts.append(txn["receipt"])  # 1 per txn
+    print(f"{len(txns)=} {len(logs)=} {len(receipts)=}")
+    # add fields from pool info to logs_df
+    fields_to_add = ["blockNumber", "timestamp", "sharePrice", "shareReserves", "bondReserves", "lpTotalSupply"]
+    logs_df = pd.merge(pd.DataFrame(logs), pool_df.loc[:, fields_to_add])
+    return logs_df, pool_df
+
+
+def save_data(logs_df, pool_df, save_folder):
+    """Save data to csv."""
+    logs_df.to_csv(f"{save_folder}/logs.csv", index=False)
+    pool_df.to_csv(f"{save_folder}/pool.csv", index=False)
+
+
+def main(
+    contracts_url: str,
+    ethereum_node: URI | str,
+    save_folder: str,
+    state_abi_file_path: str,
+    transactions_abi_file_path: str,
+    start_block: int,
+    first_deployed_block: int,
+    sleep_amount: float,
+):
+    """Main entry point for accessing contract & writing pool info"""
+    # pylint: disable=too-many-locals
+    # get web3 provider
+    web3_container: Web3 = contract_interface.setup_web3(ethereum_node)
+    # send a request to the local server to fetch the deployed contract addresses and
+    # load the deployed Hyperdrive contract addresses from the server response
+    state_hyperdrive_contract = contract_interface.get_hyperdrive_contract(
+        state_abi_file_path, contracts_url, web3_container
+    )
+    transactions_hyperdrive_contract = contract_interface.get_hyperdrive_contract(
+        transactions_abi_file_path, contracts_url, web3_container
+    )
+    # get pool config from hyperdrive contract
+    pool_config = contract_interface.get_smart_contract_read_call(state_hyperdrive_contract, "getPoolConfig")
+    # initialize records
+    pool_info = {}
+    transaction_info = {}
+
+    # monitor for new blocks & add pool info per block
+    logging.info("Monitoring for pool info updates...")
+    block_number: BlockNumber = BlockNumber(start_block)
+    while True:
+        latest_block_number = web3_container.eth.get_block_number()
+        # if we are on a new block
+        if latest_block_number != block_number:
+            # Backfilling for blocks that need updating
+            for block_int in range(block_number + 1, latest_block_number + 1):
+                block_number: BlockNumber = BlockNumber(block_int)
+                logging.info("Block %s", block_number)
+                pool_info[block_number] = (
+                    contract_interface.get_block_pool_info(web3_container, state_hyperdrive_contract, block_number)
+                    if block_number >= first_deployed_block
+                    else {}
+                )
+                block_transactions = contract_interface.fetch_transactions_for_block(
+                    web3_container, transactions_hyperdrive_contract, block_number
+                )
+                if block_transactions is not None:
+                    transaction_info[block_number] = block_transactions
+            logs_df, pool_df = prepare_data(transaction_info, pool_info)
+            save_data(logs_df, pool_df, save_folder)
+        time.sleep(sleep_amount)
+
+
+if __name__ == "__main__":
+    # setup constants
+    output_utils.setup_logging(".logging/acquire_data.log", log_file_and_stdout=True)
+    main(
+        contracts_url="http://localhost:80/addresses.json",
+        ethereum_node="http://localhost:8545",
+        save_folder=".logging",
+        state_abi_file_path="./hyperdrive_solidity/.build/IHyperdrive.json",
+        transactions_abi_file_path="./hyperdrive_solidity/.build/Hyperdrive.json",
+        start_block=0,
+        first_deployed_block=6,  # first block after full deployment
+        sleep_amount=1,
+    )
