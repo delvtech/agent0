7f0f5d21da3d9be423d922cb579bb98b7352eb7a
Mihai <mcosma@gmail.com>
2023-06-13 06:48:51 -0400

    simplify pnl

--- examples/hackweek_demo/plot_pnl.py
+++ examples/hackweek_demo/plot_pnl.py
@@ -1,13 +1,19 @@
 """Plots the pnl"""
 
 from __future__ import annotations
+
+import numpy as np
 import pandas as pd
+
 from matplotlib import ticker as mpl_ticker
 from extract_data_logs import calculate_spot_price
+
 import elfpy
 from elfpy.wallet.wallet import Wallet, Long, Short
 from elfpy import types
 from elfpy.math import FixedPoint
+from elfpy.markets.hyperdrive import hyperdrive_assets
+from elfpy.markets.hyperdrive import AssetIdPrefix
 
 
 def get_wallet_from_onchain_trade_info(
@@ -33,8 +39,6 @@ def get_wallet_from_onchain_trade_info(
      Wallet
          Wallet with Short, Long, and LP positions.
     """
-    share_price = trades.share_price
-
     # TODO: remove restriction forcing Wallet index to be an int (issue #415)
     if add_to_existing_wallet is None:
         wallet = Wallet(
@@ -51,44 +55,42 @@ def get_wallet_from_onchain_trade_info(
     negative_balance = int(trades.loc[(trades_in_position) & (trades["from"] == address), "value"].sum())
     balance = positive_balance - negative_balance
 
-    asset_type = trades["trade_enum"].iloc[0]
-    maturity = trades["maturity_timestamp"].iloc[0]
-    mint_time = int(maturity) - elfpy.SECONDS_IN_YEAR
+    asset_type = trades["asset_type"].iloc[0]
+    maturity = trades["asset_maturity"].iloc[0]
+    mint_time = maturity if np.isnan(maturity) else int(maturity) - elfpy.SECONDS_IN_YEAR
 
     # check if there's an outstanding balance
     if balance != 0:
         if asset_type == "SHORT":
-            # loop across all the positions owned by this wallet
-            sum_product_of_open_share_price_and_value, sum_value = 0, 0
-
-            # WEIGHTED AVERAGE ACROSS A BUNCH OF TRADES
-            for specific_trade in trades_in_position.index[trades_in_position]:
-                value = trades.loc[specific_trade, "value"]
-                value *= -1 if trades.loc[specific_trade, "from"] == address else 1
-                sum_value += value
-                sum_product_of_open_share_price_and_value += (
-                    value * share_price.loc[trades.loc[specific_trade, "block_number"]]
-                )
-
-            # WEIGHTED AVERAGR FROM A MARGINAL UPDATE
             previous_balance = wallet.shorts[mint_time].balance if mint_time in wallet.shorts else 0
-            previous_share_price = wallet.shorts[mint_time].open_share_price if mint_time in wallet.shorts else 0
-
             marginal_position_change = FixedPoint(scaled_value=balance)
-            marginal_open_share_price = FixedPoint(scaled_value=int(trades.share_price))
-
             new_balance = previous_balance + marginal_position_change
-
             if new_balance == 0:
                 # remove key of "mint_time" from the "wallet.shorts" dict
                 wallet.shorts.pop(mint_time, None)
             else:
-                new_open_share_price = (
-                    previous_balance * previous_share_price
-                    + marginal_position_change
-                    * marginal_open_share_price
-                    / (previous_balance + marginal_position_change)
-                )
+                # we either do a marginal update or full weighted average for open share price
+                if add_to_existing_wallet:  # marginal update
+                    previous_share_price = (
+                        wallet.shorts[mint_time].open_share_price if mint_time in wallet.shorts else 0
+                    )
+                    marginal_open_share_price = FixedPoint(scaled_value=int(trades.sharePrice))
+                    new_open_share_price = (
+                        previous_balance * previous_share_price + marginal_position_change * marginal_open_share_price
+                    )
+                    new_open_share_price /= previous_balance + marginal_position_change
+                else:  # weighted average across a bunch of trades
+                    sum_product_of_open_share_price_and_value, sum_value = 0, 0
+                    for specific_trade in trades_in_position.index[trades_in_position]:
+                        value = trades.loc[specific_trade, "value"]
+                        value *= -1 if trades.loc[specific_trade, "from"] == address else 1
+                        sum_value += value
+                        sum_product_of_open_share_price_and_value += (
+                            value * trades.loc[specific_trade, "sharePrice"] / 1e18
+                        )
+                    new_open_share_price = FixedPoint(
+                        scaled_value=int(sum_product_of_open_share_price_and_value / sum_value)
+                    )
                 wallet.shorts.update(
                     {
                         mint_time: Short(
@@ -109,23 +111,30 @@ def get_wallet_from_onchain_trade_info(
     return wallet
 
 
-def calculate_pnl(trade_data):
-    # pylint: disable=too-many-locals
+def calculate_pnl(logs_df):
     """Calculates the pnl given trade data"""
+    # pylint: disable=too-many-locals
     # Drop all rows with nan maturity timestamps
-    trade_data = trade_data[~trade_data["maturity_timestamp"].isna()]
 
-    # %% estimate position duration and add it in
-    position_duration = max(trade_data.maturity_timestamp - trade_data.block_timestamp)
-    # print(f"empirically observed position_duration in seconds: {position_duration}")
-    # print(f"empirically observed position_duration in minutes: {position_duration/60}")
-    # print(f"empirically observed position_duration in hours: {position_duration/60/60}")
-    # print(f"empirically observed position_duration in days: {position_duration/60/60/24}")
+    idx = ~logs_df["maturityTime"].isna()
+    position_duration = max(logs_df.maturityTime[idx] - logs_df.timestamp[idx])
     position_duration_days = round(position_duration / 60 / 60 / 24)
-    # print(f"assuming position_duration is {position_duration_days}")
     position_duration = position_duration_days * 60 * 60 * 24
 
-    agents = trade_data["operator"].value_counts().index.tolist()
+    def decode_id(row):
+        if row["id"] != row["id"]:
+            return np.nan, np.nan
+        return hyperdrive_assets.decode_asset_id(int(row["id"]))
+
+    def decode_prefix(row):
+        return np.nan if np.isnan(row) else AssetIdPrefix(row).name
+
+    tuple_series = logs_df.apply(func=decode_id, axis=1)
+    logs_df["prefix"], logs_df["asset_maturity"] = zip(*tuple_series)
+    logs_df["asset_type"] = logs_df["prefix"].apply(decode_prefix)
+
+    agents = logs_df["trader"].value_counts().index.tolist() + logs_df["operator"].value_counts().index.tolist()
+    agents = list(set(agents))
     agent_wallets = {
         a: Wallet(
             address=agents.index(a),
@@ -134,18 +143,15 @@ def calculate_pnl(trade_data):
         for a in agents
     }
 
-    # %% pre-define column names to store agent pnl
-    pnl_data = pd.DataFrame(index=trade_data.index)
+    # pre-define column names to store agent pnl
+    pnl_data = pd.DataFrame(index=logs_df.index)
     agent_col_names = []
     for i in range(len(agents)):
         agent_col_name = f"agent_{i}_pnl"
-        pnl_data[agent_col_name] = 0
+        pnl_data[agent_col_name] = np.nan
         agent_col_names.append(agent_col_name)
 
-    # %%
-    process_rows = len(trade_data)
-
-    for idx, row in trade_data.loc[0:process_rows, :].iterrows():
+    for idx, row in logs_df.iterrows():
         for agent in agents:
             agent_index = agents.index(agent)
 
@@ -160,20 +166,19 @@ def calculate_pnl(trade_data):
             )
 
             spot_price = calculate_spot_price(
-                row.share_reserves,
-                row.bond_reserves,
-                row.lp_total_supply,
-                row.maturity_timestamp,
-                row.block_timestamp,
+                row.shareReserves,
+                row.bondReserves,
+                row.lpTotalSupply,
+                row.maturityTime,
+                row.timestamp,
                 position_duration,
             )
-            # print(f"{spot_price=}")
 
             # LP value (TODO: check why certain agents own more than 100% of the pool)
-            total_lp_value = row.share_reserves / 1e18 * row.share_price / 1e18 + row.bond_reserves / 1e18 * spot_price
-            share_of_pool = float(agent_wallets[agent].lp_tokens) / (row.lp_total_supply / 1e18)
+            total_lp_value = row.shareReserves / 1e18 * row.sharePrice / 1e18 + row.bondReserves / 1e18 * spot_price
+            share_of_pool = float(agent_wallets[agent].lp_tokens) / (row.lpTotalSupply / 1e18)
             # print(f"{float(agent_wallets[agent].lp_tokens)=}")
-            # print(f"{row.lp_total_supply/1e18=}")
+            # print(f"{row.lpTotalSupply/1e18=}")
             pnl = share_of_pool * total_lp_value
 
             # for each LONG
@@ -186,10 +191,9 @@ def calculate_pnl(trade_data):
 
             pnl_data.loc[idx, f"agent_{agent_index}_pnl"] = pnl
 
-    # %%
-    x_data = pd.to_datetime(trade_data.loc[:, "block_timestamp"], unit="s")
+    x_data = pd.to_datetime(logs_df.loc[:, "timestamp"], unit="s")
 
-    return (x_data, pnl_data)
+    return x_data, pnl_data
 
 
 def plot_pnl(x_data, y_data, axes):
