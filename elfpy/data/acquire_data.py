61e08bf9e0f8b869aa53e3f28b5a753f3aea18eb
Sheng Lundquist <sheng@delv.tech>
2023-06-14 14:40:58 -0700

    Acquire data bug fixes (#565)
    * Fixing an error where looking up pool info with block number sometimes fails

--- elfpy/data/acquire_data.py
+++ elfpy/data/acquire_data.py
@@ -22,6 +22,7 @@ def main(
     transactions_abi_file_path: str,
     save_dir: str,
     start_block: int,
+    lookback_block_limit: int,
     sleep_amount: int,
 ):
     """Main entry point for accessing contract & writing pool info"""
@@ -41,7 +42,14 @@ def main(
     contract_interface.hyperdrive_config_to_json(config_file, state_hyperdrive_contract)
     # write the initial pool info
     block_number: BlockNumber = BlockNumber(start_block)
+    latest_block_number = web3_container.eth.get_block_number()
+    lookback_block_limit = BlockNumber(lookback_block_limit)
+    if (latest_block_number - block_number) > lookback_block_limit:
+        block_number = BlockNumber(latest_block_number - lookback_block_limit)
+        logging.warning("Starting block is past lookback block limit, starting at block %s", block_number)
+
     pool_info = []
+
     block_pool_info: dict = contract_interface.get_block_pool_info(
         web3_container, state_hyperdrive_contract, block_number
     )
@@ -56,14 +64,34 @@ def main(
     while True:
         latest_block_number = web3_container.eth.get_block_number()
         # if we are on a new block
-        if latest_block_number != block_number:
+        if latest_block_number > block_number:
             # Backfilling for blocks that need updating
             for block_int in range(block_number + 1, latest_block_number + 1):
                 block_number: BlockNumber = BlockNumber(block_int)
                 logging.info("Block %s", block_number)
-                block_pool_info = contract_interface.get_block_pool_info(
-                    web3_container, state_hyperdrive_contract, block_number
-                )
+
+                # Explicit check against loopback block limit
+                if (latest_block_number - block_number) > lookback_block_limit:
+                    logging.warning(
+                        "Querying block_number %s out of %s, unable to keep up with chain block iteration",
+                        block_number,
+                        latest_block_number,
+                    )
+                    continue
+
+                # get_block_pool_info crashes randomly with ValueError on some intermediate block,
+                # keep trying until it returns
+                while True:
+                    try:
+                        block_pool_info = contract_interface.get_block_pool_info(
+                            web3_container, state_hyperdrive_contract, block_number
+                        )
+                        break
+                    except ValueError:
+                        logging.warning("Error in get_block_pool_info, retrying")
+                        time.sleep(0.1)
+                        continue
+
                 if block_pool_info:
                     pool_info.append(block_pool_info)
                 block_transactions = contract_interface.fetch_transactions_for_block(
@@ -86,6 +114,8 @@ if __name__ == "__main__":
     STATE_ABI_FILE_PATH = "./hyperdrive_solidity/.build/IHyperdrive.json"
     TRANSACTIONS_ABI_FILE_PATH = "./hyperdrive_solidity/.build/Hyperdrive.json"
     START_BLOCK = 6
+    # Look back limit for backfilling
+    LOOKBACK_BLOCK_LIMIT = 1000
     SLEEP_AMOUNT = 1
     output_utils.setup_logging(".logging/acquire_data.log", log_file_and_stdout=True)
     main(
@@ -95,5 +125,6 @@ if __name__ == "__main__":
         TRANSACTIONS_ABI_FILE_PATH,
         SAVE_DIR,
         START_BLOCK,
+        LOOKBACK_BLOCK_LIMIT,
         SLEEP_AMOUNT,
     )
