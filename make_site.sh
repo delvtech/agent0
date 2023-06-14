e127deec65cd537735931a22bf805418aec202e7
Mihai <mcosma@gmail.com>
2023-06-14 17:09:01 -0400

    make public folder if it doesn't exist, add comments to make_site.sh

--- make_site.sh
+++ make_site.sh
@@ -1,5 +1,17 @@
+if [ ! -d "$DIRECTORY" ]; then
+  mkdir public  # make ./public if it doesn't exist
+fi
+
+# use sphinx to build docs
 cd docs
 make html
+
+# move built html files to ./public
 cp -r build/html/* ../public/
+echo "moved html pages from ./docs/build/html to ./public"
+
+# delete build folder
 rm -rf build
+
+# return to elf-simulations root
 cd ..
