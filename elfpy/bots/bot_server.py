b2268f07ba4f5f6f35941705e01629dd4a1a24e6
Mihai <mcosma@gmail.com>
2023-06-15 21:51:56 -0400

    change bot server port to 5001

--- elfpy/bots/bot_server.py
+++ elfpy/bots/bot_server.py
@@ -70,4 +70,4 @@ if __name__ == "__main__":
     SCRIPT_PATH = sys.argv[1]
     app.config["SCRIPT_PATH"] = SCRIPT_PATH
 
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=5001)
