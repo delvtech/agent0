23dbd56d15b8b603d73b0b2b598f0381007563c9
Mihai <mcosma@gmail.com>
2023-06-30 17:14:34 -0400

    fix comment

--- elfpy/bots/bot_config.py
+++ elfpy/bots/bot_config.py
@@ -48,7 +48,7 @@ class BotConfig(types.FrozenClass):
     # In general the bot will be more risk averse as it grows to infinity.
     # A value of 0 will usually disable it.
     risk_threshold: float = 0.0
-    # whether or not to dump state at every block
+    # whether or not to dump state on a crash
     dump_state: bool = False
     # whether to initialize state from a specific point
     load_state_id: str | None = None
