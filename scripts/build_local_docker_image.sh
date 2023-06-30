6be202802238e3c78f5ded0c0acc35c74964f1ac
Dylan <dpaiton@gmail.com>
2023-06-30 13:14:02 -0700

    rewrites script to take a tag argument

--- scripts/build_local_docker_image.sh
+++ scripts/build_local_docker_image.sh
@@ -1,1 +1,18 @@
-docker build --no-cache -t elf-sims -f Dockerfile .
+#!/bin/bash
+
+if [ $# -ne 1 ]; then
+  echo "Usage: $0 <tag>"
+  exit 1
+fi
+
+# GitHub repository details
+repo_owner="delvtech"
+repo_name="elf-simulations"
+
+# Docker image details
+image_name="elfpy"
+tag=$1
+
+docker build --no-cache -t $image_name:$tag https://github.com/$repo_owner/$repo_name.git#$tag
+
+echo "Docker image built and tagged as $image_name:$tag."
