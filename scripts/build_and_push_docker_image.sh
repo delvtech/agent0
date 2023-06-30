44db250f27d8ae9c81a67ee49dc489f9320e6857
Matthew Brown <mattfdbrown@gmail.com>
2023-06-30 15:02:42 -0700

    add script

--- scripts/build_and_push_docker_image.sh
+++ scripts/build_and_push_docker_image.sh
@@ -1,29 +1,28 @@
 #!/bin/bash
 
-TAG=""
+TAG="default"
 
 if [ $# -eq 0 ]; then
-  echo "Using Image ID as the tag for the image."
+  echo "Using 'default' as the tag for the image."
 else
   TAG="$1"
   echo "Using $TAG as the tag for the image."
 fi
 
-TAG_OPTION=""
-if [[ -n $TAG ]]; then
-    TAG_OPTION="-t $TAG"
-fi
+echo "Tag: $TAG"
 
-IMAGE_ID=$(docker build --no-cache $TAG_OPTION -f Dockerfile ../ | tail -n 1 | awk '{ print $NF }')
+docker build --no-cache -t $TAG -f Dockerfile .
+IMAGE_ID=$(docker image ls -q | awk 'NR==1{print}')
 echo "Image Id: $IMAGE_ID"
 
-if [[ -n $TAG ]]; then
+if [[ "$TAG" == "default" ]]; then
     TAG=$IMAGE_ID
 fi
+echo "Tag: $TAG"
 
 docker tag $IMAGE_ID ghcr.io/delvtech/elf-simulations/$TAG
 docker image push ghcr.io/delvtech/elf-simulations/$TAG
 
-echo "Tag pushed to ghcr.io/delvtech/elf-simulations$TAG"
+echo "Tag pushed to ghcr.io/delvtech/elf-simulations/$TAG"
 echo "Run the following command to retrieve the image:"
-echo "docker image pull ghcr.io/delvtech/elf-simulations$TAG"
+echo "docker image pull ghcr.io/delvtech/elf-simulations/$TAG"
