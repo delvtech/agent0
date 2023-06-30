cca1c3047a78612d5280a1eeea62de0913a45c28
Mihai <mcosma@gmail.com>
2023-06-30 17:54:37 -0400

    use existing market fixture and set trade chance to 1

--- tests/test_dumps.py
+++ tests/test_dumps.py
@@ -9,11 +9,8 @@ import pytest
 from fixedpointmath import FixedPoint
 
 # elfpy core repo
-from elfpy import time
 from elfpy.agents.agent import Agent
 from elfpy.agents.policies import RandomAgent
-from elfpy.markets.hyperdrive.hyperdrive_market import HyperdriveMarket, HyperdriveMarketState
-from elfpy.markets.hyperdrive.hyperdrive_pricing_model import HyperdrivePricingModel
 
 RAND_SEED = 123
 
@@ -29,21 +26,9 @@ def rng():
 @pytest.fixture
 def agent(rng):
     """Agent object initialized with RandomAgent policy."""
-    policy = RandomAgent(budget=FixedPoint(50_000), trade_chance=FixedPoint(0.1), rng=rng)
+    print(f"{rng=}")
+    policy = RandomAgent(budget=FixedPoint(50_000), trade_chance=FixedPoint(1), rng=rng)
     return Agent(wallet_address=0, policy=policy)
-
-
-@pytest.fixture
-def market():
-    """HyperdriveMarket initialized with defaults, a 365 day term, and a time-stretch of 10."""
-    return HyperdriveMarket(
-        pricing_model=HyperdrivePricingModel(),
-        market_state=HyperdriveMarketState(),
-        position_duration=time.StretchedTime(
-            days=FixedPoint("365.0"), time_stretch=FixedPoint("10.0"), normalizing_constant=FixedPoint("365.0")
-        ),
-        block_time=time.BlockTime(),
-    )
 
 
 def test_rng(rng):
@@ -66,18 +51,36 @@ def test_rng(rng):
     assert np.array_equal(rand_seq, rand_seq2)
 
 
-def test_agent(rng, agent, market, tmp_path):
+def test_agent(agent, hyperdrive_sim, tmp_path):
     """Ensure pickling an agent then reloading it returns the same next trade."""
+    # roll forward 10 trades
+    for _ in range(10):
+        _ = agent.get_trades(market=hyperdrive_sim)
+
+    # get the state we want to save
+    state = agent.policy.rng.bit_generator.state
+
+    # save the agent
     with open(tmp_path / "agent.pkl", "wb") as file:
         pickle.dump(agent, file)
+    # save the state
+    with open(tmp_path / "state.pkl", "wb") as file:
+        pickle.dump(state, file)
+        
+    # get the agent's 11th trade
+    trade1 = agent.get_trades(market=hyperdrive_sim)
+    print(f"{trade1=}")
 
-    trade1 = agent.get_trades(market=market)
-
+    # reload the agent and call it agent2
     agent2 = None
     with open(tmp_path / "agent.pkl", "rb") as file:
         agent2 = pickle.load(file)
-        agent2.policy.rng = rng
+        agent2.policy.rng = np.random.default_rng(RAND_SEED)
+        agent2.policy.rng.bit_generator.state = state
 
-    trade2 = agent2.get_trades(market=market)
+    # get the loaded agent's next trade
+    trade2 = agent2.get_trades(market=hyperdrive_sim)
+    print(f"{trade2=}")
 
+    # compare the two trades
     assert trade1 == trade2
