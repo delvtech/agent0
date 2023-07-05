fd77a545d15fce22ec4d1ba038ed79ab88ad0e86
Sheng Lundquist <sheng@delv.tech>
2023-07-05 14:23:19 -0700

    Updating streamlit demo to read pool_config data from postgres

--- examples/hackweek_demo/run_demo.py
+++ examples/hackweek_demo/run_demo.py
@@ -2,7 +2,6 @@
 
 from __future__ import annotations
 
-import os
 import time
 
 import mplfinance as mpf
@@ -37,9 +36,6 @@ def get_ticker(data):
     return out
 
 
-curr_file_dir = os.path.dirname(os.path.abspath(__file__))
-config_file = curr_file_dir + "/../../.logging/hyperdrive_config.json"
-
 fig = mpf.figure(style="mike", figsize=(15, 15))  # type: ignore
 ax_ohlcv = fig.add_subplot(2, 2, 1)
 ax_fixed_rate = fig.add_subplot(2, 2, 2)
@@ -55,10 +51,10 @@ if view_window is not None:
 else:
     start_block = None
 
-while True:
-    # TODO currently still reading/writing to config file, add this to postgres
-    config_data = read_json_to_pd(config_file)
+# pool config data is static, so just read once
+config_data = postgres.get_pool_config(session)
 
+while True:
     txn_data = postgres.get_transactions(session, start_block=start_block)
     pool_info_data = postgres.get_pool_info(session, start_block=start_block)
 
