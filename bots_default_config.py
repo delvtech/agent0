c5f5f0bc43509b7b4826686ea887ad0f15f557ac
Sheng Lundquist <sheng@delv.tech>
2023-07-06 14:01:23 -0700

    Using bots python script to generate dfault config

---
+++ bots_default_config.py
@@ -0,0 +1,9 @@
+"""Example script for writing a custom bot config to json"""
+from elfpy.bots import BotConfig
+
+if __name__ == "__main__":
+    # load the config file, which loads defaults
+    config = BotConfig()
+
+    # Write config to json
+    config.save_as_json("bots_config.json")
