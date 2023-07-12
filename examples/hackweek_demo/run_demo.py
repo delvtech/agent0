3673954a74779e385d21f2f0f7aa53c62b24f2b0
Sheng Lundquist <sheng@delv.tech>
2023-07-12 14:32:52 -0700

    Adding selection of agents to view pnl

--- examples/hackweek_demo/run_demo.py
+++ examples/hackweek_demo/run_demo.py
@@ -20,9 +20,6 @@ from elfpy.data import postgres
 # None to view all
 view_window = None
 
-# set up streamlit
-# creating a single-element container
-placeholder = st.empty()
 
 # %%
 # near real-time / live feed simulation
@@ -30,12 +27,7 @@ placeholder = st.empty()
 ## Get transactions from data
 
 
-def get_ticker(data):
-    """Given transaction data, return a subset of the dataframe"""
-    # Return reverse of methods to put most recent transactions at the top
-    out = data[["blockNumber", "input_method"]].set_index("blockNumber").iloc[::-1]
-    return out
-
+# TODO should these all be seperate figures instead of one figure?
 
 fig = mpf.figure(style="mike", figsize=(15, 15))  # type: ignore
 ax_ohlcv = fig.add_subplot(2, 2, 1)
@@ -56,11 +48,39 @@ else:
 # pool config data is static, so just read once
 config_data = postgres.get_pool_config(session)
 
+# Set up agent selection state list outside of live updates
+st.session_state.options = []
+
+
+def get_ticker(data):
+    """Given transaction data, return a subset of the dataframe"""
+    # Return reverse of methods to put most recent transactions at the top
+    out = data[["blockNumber", "input_method"]].set_index("blockNumber").iloc[::-1]
+    return out
+
+
+agent_list = postgres.get_agents(session, start_block=start_block)
+
+filter_agents = st.multiselect("PNL Agents", agent_list, key="agent_select")
+
+
+# set up streamlit
+input_placeholder = st.empty()
+# creating a single-element container
+main_placeholder = st.empty()
+# creating a single-element sidebar
+sidebar_placeholder = st.sidebar.empty()
+
+
 while True:
     txn_data = postgres.get_transactions(session, start_block=start_block)
     pool_info_data = postgres.get_pool_info(session, start_block=start_block)
 
     combined_data = get_combined_data(txn_data, pool_info_data)
+
+    if len(combined_data) == 0:
+        time.sleep(0.1)
+        continue
 
     ohlcv = calc_ohlcv(combined_data, freq="5T")
 
@@ -68,7 +88,7 @@ while True:
 
     (fixed_rate_x, fixed_rate_y) = calc_fixed_rate(combined_data)
 
-    pnl_x, pnl_y = calculate_pnl(combined_data)
+    pnl_x, pnl_y = calculate_pnl(combined_data, filter_agents)
 
     # Plot reserve levels (share and bond reserves, in poolinfo)
 
@@ -76,24 +96,24 @@ while True:
 
     # Add ticker
 
-    with placeholder.container():
-        ticker_col, plots_col = st.columns([0.2, 1], gap="small")
-        with ticker_col:
-            st.dataframe(ticker.iloc[:100], height=900, width=170)
+    # Selection options for filtering agent, defined outside of update loop
+    # Session state gets updated inside loop
+    # with plots_col:
+    with sidebar_placeholder.container():
+        st.dataframe(ticker.iloc[:100], height=900, use_container_width=True)
 
-        with plots_col:
-            # Clears all axes
-            ax_ohlcv.clear()
-            ax_fixed_rate.clear()
-            ax_vol.clear()
-            ax_pnl.clear()
+    with main_placeholder.container():
+        # Clears all axes
+        ax_vol.clear()
+        ax_pnl.clear()
+        ax_ohlcv.clear()
+        ax_fixed_rate.clear()
 
-            # TODO add in volume
-            plot_ohlcv(ohlcv, ax_ohlcv, ax_vol)
-            plot_fixed_rate(fixed_rate_x, fixed_rate_y, ax_fixed_rate)
-            plot_pnl(pnl_x, pnl_y, ax_pnl)
+        plot_ohlcv(ohlcv, ax_ohlcv, ax_vol)
+        plot_fixed_rate(fixed_rate_x, fixed_rate_y, ax_fixed_rate)
+        plot_pnl(pnl_x, pnl_y, ax_pnl)
 
-            fig.autofmt_xdate()  # type: ignore
-            st.pyplot(fig=fig)  # type: ignore
+        fig.autofmt_xdate()  # type: ignore
+        st.pyplot(fig=fig)  # type: ignore
 
     time.sleep(0.1)
