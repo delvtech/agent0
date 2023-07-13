28ccca158d5491c6beb294ff8f562b97fad7b7ed
Sheng Lundquist <sheng@delv.tech>
2023-07-13 14:17:52 -0700

    Adding explicit default username to bots_default_config.py

--- elfpy/bots/bots_default_config.py
+++ elfpy/bots/bots_default_config.py
@@ -1,9 +1,12 @@
 """Example script for writing a custom bot config to json"""
-from elfpy.bots import BotConfig
+from elfpy.bots import DEFAULT_USERNAME, BotConfig
 
 if __name__ == "__main__":
     # load the config file, which loads defaults
     config = BotConfig()
+
+    # Add username here if adjusting configurations in this script
+    config.username = DEFAULT_USERNAME
 
     # Write config to json
     config.save_as_json("bots_config.default.json")
