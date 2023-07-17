e090533139c288886b0151ec08ed3b009c0a9b11
Dylan <dpaiton@gmail.com>
2023-07-17 15:15:18 -0700

    config fixup

--- elfpy/bots/__init__.py
+++ elfpy/bots/__init__.py
@@ -1,5 +1,6 @@
 """Utilities and classes for Agent.0 bots"""
 
-from .bot_config import DEFAULT_USERNAME, BotConfig
+from .bot_config import BotConfig
 from .bot_info import BotInfo
 from .budget import Budget
+from .environment_config import DEFAULT_USERNAME, EnvironmentConfig
