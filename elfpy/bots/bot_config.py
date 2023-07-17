b05e8ba49faa7c052eb6471edb99154466f983a2
Dylan <dpaiton@gmail.com>
2023-07-17 15:11:47 -0700

    put bot config back to how it was

--- elfpy/bots/bot_config.py
+++ elfpy/bots/bot_config.py
@@ -1,11 +1,11 @@
-"""State object for setting bot configuration"""
+"""State object for setting experiment configuration"""
 from __future__ import annotations
 
 import json
-from dataclasses import dataclass
+from dataclasses import dataclass, field
+from typing import Any
 
-from elfpy import types
-from elfpy.bots import BotInfo
+from elfpy import DEFAULT_LOG_LEVEL, DEFAULT_LOG_MAXBYTES, types
 from elfpy.utils import json as output_utils
 
 DEFAULT_USERNAME = "changeme"
@@ -19,8 +19,41 @@ class BotConfig(types.FrozenClass):
     # lots of configs!
     # pylint: disable=too-many-instance-attributes
 
-    # list of details for the desired agents
-    agents: list[BotInfo] | None = None
+    # Logical username for who is running bots
+    username: str = DEFAULT_USERNAME
+    # whether to run on alchemy
+    alchemy: bool = False
+    # url for retrieving the contract artifacts
+    artifacts_url: str = "http://localhost:80"
+    # whether to run on devnet
+    devnet: bool = True
+    # if true, stop executing when trade errors occur
+    halt_on_errors: bool = False
+    # optional output filename for logging
+    log_filename: str = "agent0-bots"
+    # log level; should be in [logging.DEBUG, logging.INFO, logging.WARNING]
+    log_level: int = DEFAULT_LOG_LEVEL  # INFO
+    # delete_previous_logs; if True, delete existing logs at the start of the run
+    delete_previous_logs: bool = False
+    # log log_file_and_stdout; if True, save to file and write to stdout, else just save to file
+    log_file_and_stdout: bool = False
+    # log_formatter; specifies the format in which the logger saves the logs
+    # see https://docs.python.org/3/library/logging.html#logrecord-attributes for which attributes can be used
+    log_formatter: str = "\n%(asctime)s: %(levelname)s: %(module)s.%(funcName)s:\n%(message)s"
+    # maximum log file output size, in bytes
+    max_bytes: int = DEFAULT_LOG_MAXBYTES  # int(2e6) or 2MB
+    # location of RPC
+    rpc_url: str = "http://localhost:8545"
+    # chance for a bot to execute a trade
+    trade_chance: float = 0.1
+    # int to be used for the random seed
+    random_seed: int = 1
+    # risk_threshold has different interpretations for different bots.
+    # In general the bot will be more risk averse as it grows to infinity.
+    # A value of 0 will usually disable it.
+    risk_threshold: float = 0.0
+    # scratch space for any application-specific & extraneous parameters
+    scratch: dict[Any, Any] = field(default_factory=dict)
 
     def __getitem__(self, attrib) -> None:
         return getattr(self, attrib)
