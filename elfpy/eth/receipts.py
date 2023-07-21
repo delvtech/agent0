c56e70eb641465f2fde374fe7b18e15ca7371f94
Dylan <dpaiton@gmail.com>
2023-07-20 20:38:58 -0700

    cleans up type annotations

--- elfpy/eth/receipts.py
+++ elfpy/eth/receipts.py
@@ -4,7 +4,7 @@ from __future__ import annotations
 from typing import Any, Sequence
 
 from web3 import Web3
-from web3.contract.contract import Contract, ContractEvent
+from web3.contract.contract import Contract
 from web3.types import ABIEvent, EventData, LogReceipt, TxReceipt
 
 
@@ -32,7 +32,7 @@ def get_transaction_logs(
         If event_names is not None, then the returned dict will only
         include logs that have a corresponding "event" entry.
     """
-    logs: list[dict[Any, Any]] = []
+    logs: list[dict[str, Any]] = []
     if tx_receipt.get("logs"):
         for log in tx_receipt["logs"]:
             event_data, event = get_event_object(web3, contract, log, tx_receipt)
@@ -64,22 +64,24 @@ def get_event_object(
     Returns
     -------
     tuple[EventData, ABIEvent] | tuple[None, None]
-        If faile, return (None, None). Otherwise, return the decoded event information as (data, abi).
+        If the event is not found, return (None, None).
+        Otherwise, return the decoded event information as (data, abi).
     """
-    abi_events = [abi for abi in contract.abi if abi["type"] == "event"]  # type: ignore
-    for event in abi_events:  # type: ignore
+    abi_events: list[ABIEvent] = [
+        abi for abi in contract.abi if isinstance(abi, ABIEvent) and hasattr(abi, "type") and abi.get("type") == "event"
+    ]
+    for event in abi_events:
         # Get event signature components
-        name = event["name"]  # type: ignore
-        inputs = [param["type"] for param in event["inputs"]]  # type: ignore
-        inputs = ",".join(inputs)
+        name = event.get("name")
+        inputs: str = ",".join([param.get("type", "") for param in event.get("inputs", [])])
         # Hash event signature
         event_signature_text = f"{name}({inputs})"
         event_signature_hex = web3.keccak(text=event_signature_text).hex()
         # Find match between log's event signature and ABI's event signature
         receipt_event_signature_hex = log["topics"][0].hex()
-        if event_signature_hex == receipt_event_signature_hex:
+        if event_signature_hex == receipt_event_signature_hex and name is not None:
             # Decode matching log
-            contract_event: ContractEvent = contract.events[event["name"]]()  # type: ignore
+            contract_event = contract.events[name]()
             event_data: EventData = contract_event.process_receipt(tx_receipt)[0]
-            return event_data, event  # type: ignore
+            return event_data, event
     return (None, None)
