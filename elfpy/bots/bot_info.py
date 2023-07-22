4d444c37209783eb2a8cf367880f96d7accffa53
Dylan <dpaiton@gmail.com>
2023-07-21 17:42:08 -0700

    removes evm_bots config

--- elfpy/bots/bot_info.py
+++ elfpy/bots/bot_info.py
@@ -1,7 +1,6 @@
 """Information for creating a bot"""
 from __future__ import annotations
 
-from collections import namedtuple
 from dataclasses import dataclass, field
 from typing import Type
 
@@ -16,31 +15,23 @@ class AgentConfig:
 
     Attributes
     ----------
-    name : str
-        The name of the agent
     policy : str
         The agent's policy; should match the class name
-    number_of_bots : int
-        The number of bots of this type to spin up
-    trade_chance : float
-        Percent chance that a agent gets to trade on a given block
+    name : str
+        The name of the agent
     budget : Budget
         The budget for the agent
-    scratch : dict
-        Any parameters for custom bots should go here
+    number_of_agents : int
+        The number of bots of this type to spin up
+    init_kwargs : dict
+        A dictionary of keyword arguments for the policy constructor
     """
 
-    # TODO: We should be able to enable this again once we remove evm_bots
+    # lots of configs!
     # pylint: disable=too-many-instance-attributes
 
     policy: Type[BasePolicy]
-    index: int | None = None  # TODO: Make this required when we remove evm_bots
     name: str = "BoringBotty"
     budget: Budget = Budget()
     number_of_agents: int = 1
     init_kwargs: dict = field(default_factory=dict)
-    # TODO: Remove below once we remove evm_bots
-    trade_chance: float = 0.8
-    risk_threshold: float = 0.8
-    Risk = namedtuple("Risk", ["mean", "std", "min", "max"])
-    risk: Risk = Risk(mean=0.02, std=0.01, min=0.0, max=0.06)
