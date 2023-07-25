9d9f1c183cbfe829bd3904075eb9280a82b9b5e6
Dylan <dpaiton@gmail.com>
2023-07-25 11:56:10 -0700

    gives eth agents an option to init from private key

--- elfpy/eth/accounts/eth_account.py
+++ elfpy/eth/accounts/eth_account.py
@@ -1,6 +1,8 @@
 """Empty test accounts for testing smart contracts"""
 from __future__ import annotations
 
+import logging
+
 from eth_account import Account
 from eth_account.signers.local import LocalAccount
 from eth_typing import ChecksumAddress
@@ -16,12 +18,27 @@ class EthAccount:
     # If not, we can delete it at the end of the refactor.
     # pylint: disable=too-few-public-methods
 
-    def __init__(self, agent: Agent, extra_entropy: str = "TEST ACCOUNT"):
+    def __init__(self, agent: Agent | None = None, private_key: str | None = None, extra_entropy: str = "TEST ACCOUNT"):
         """Initialize an account"""
-        self.account: LocalAccount = Account().create(extra_entropy=extra_entropy)
-        self.agent = agent
+        if private_key is None:
+            self.account: LocalAccount = Account().create(extra_entropy=extra_entropy)
+        else:
+            self.account: LocalAccount = Account().from_key(private_key)
+        self._agent = agent
+
+    @property
+    def agent(self) -> Agent:
+        """Return the elfpy agent object if it exists, otherwise throw an error"""
+        if self._agent is None:
+            raise AttributeError(f"EthAccount with address {self.account.address} has no Agent member")
+        return self._agent
 
     @property
     def checksum_address(self) -> ChecksumAddress:
         """Return the checksum address of the account"""
         return Web3.to_checksum_address(self.account.address)
+
+    @property
+    def _private_key(self) -> str:
+        logging.warning("accessing agent private key")
+        return str(self.account._private_key)  # pylint: disable=protected-access
