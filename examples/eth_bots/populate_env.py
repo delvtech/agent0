dc614caee81b560b77e0b7044ce02b8f74d2321b
Dylan <dpaiton@gmail.com>
2023-07-26 08:16:53 -0700

    easier to use populate_env.py

--- examples/eth_bots/populate_env.py
+++ examples/eth_bots/populate_env.py
@@ -1,4 +1,5 @@
 """Helper script to generate a random private key."""
+import argparse
 import json
 import os
 
@@ -20,6 +21,16 @@ def make_key() -> str:
 
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        prog="populate_env",
+        description="Script for generating a .env file for eth_bots.",
+        epilog=(
+            "Run the script with a user's private key as argument to include it in the output."
+            "See the README on https://github.com/delvtech/elf-simulations/eth_bots/ for more implementation details"
+        ),
+    )
+    parser.add_argument("user_key", nargs="?", help="Provide the user's private key to include it")
+    args = parser.parse_args()
     environment_config, agent_config = get_eth_bots_config()
     rng = np.random.default_rng(environment_config.random_seed)
     agent_private_keys = []
@@ -30,6 +41,8 @@ if __name__ == "__main__":
             agent_private_keys.append(make_key())
             agent_base_budgets.append(agent_info.base_budget.sample_budget(rng).scaled_value)
             agent_eth_budgets.append(agent_info.eth_budget.sample_budget(rng).scaled_value)
+    if args.user_key is not None:
+        print('export USER_KEY="' + str(parser.parse_args().user_key) + '"')
     print("export AGENT_KEYS='" + json.dumps(agent_private_keys) + "'")
     print("export AGENT_BASE_BUDGETS='" + json.dumps(agent_base_budgets) + "'")
     print("export AGENT_ETH_BUDGETS='" + json.dumps(agent_eth_budgets) + "'")
