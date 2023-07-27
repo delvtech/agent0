315f2ddfe32231b258ff9673316d09fbf4017a11
Sheng Lundquist <sheng@delv.tech>
2023-07-26 18:21:15 -0700

    Adding leaderboard to streamlit

--- examples/hackweek_demo/plot_pnl.py
+++ examples/hackweek_demo/plot_pnl.py
@@ -4,6 +4,7 @@ from __future__ import annotations
 import logging
 
 import pandas as pd
+from extract_data_logs import calculate_spot_price
 
 from elfpy.data.postgres import AgentPosition
 
@@ -14,7 +15,6 @@ def calculate_spot_price_from_state(state, maturity_timestamp, block_timestamp, 
     return calculate_spot_price_for_position(
         state.shareReserves,
         state.bondReserves,
-        state.lpTotalSupply,
         config_data["invTimeStretch"],
         config_data["initialSharePrice"],
         config_data["positionDuration"],
@@ -27,22 +27,18 @@ def calculate_spot_price_from_state(state, maturity_timestamp, block_timestamp, 
 def calculate_spot_price_for_position(
     share_reserves,
     bond_reserves,
-    lp_total_supply,
-    stretch_time,
+    time_stretch,
     initial_share_price,
-    position_duration=None,
-    maturity_timestamp=None,
-    block_timestamp=None,
+    position_duration,
+    maturity_timestamp,
+    block_timestamp,
 ):
     """Calculate the spot price given the pool info data."""
     # pylint: disable=too-many-arguments
 
     # TODO this calculation is broken
+    full_term_spot_price = calculate_spot_price(share_reserves, bond_reserves, initial_share_price, time_stretch)
 
-    full_term_spot_price = ((initial_share_price * share_reserves) / (bond_reserves + lp_total_supply)) ** stretch_time
-
-    if maturity_timestamp is None or block_timestamp is None or position_duration is None:
-        return full_term_spot_price
     time_left_seconds = maturity_timestamp - block_timestamp
     if isinstance(time_left_seconds, pd.Timedelta):
         time_left_seconds = time_left_seconds.total_seconds()
@@ -55,6 +51,55 @@ def calculate_spot_price_for_position(
     )
 
     return full_term_spot_price * time_left_in_years + 1 * (1 - time_left_in_years)
+
+
+def calculate_current_pnl(pool_config: pd.Series, pool_info: pd.DataFrame, current_wallet: pd.DataFrame) -> pd.Series:
+    """Calculates the most current pnl values"""
+    # Most current block timestamp
+    latest_pool_info = pool_info.loc[pool_info.index.max()]
+    block_timestamp = latest_pool_info["timestamp"].timestamp()
+
+    # Calculate for base
+    base_pnl = current_wallet[current_wallet["baseTokenType"] == "BASE"]["tokenValue"]
+
+    # Calculate for lp
+    wallet_lps = current_wallet[current_wallet["baseTokenType"] == "LP"]
+    lp_pnl = wallet_lps["tokenValue"] * latest_pool_info["sharePrice"]
+
+    # Calculate for shorts
+    wallet_shorts = current_wallet[current_wallet["baseTokenType"] == "SHORT"]
+    short_spot_prices = calculate_spot_price_for_position(
+        latest_pool_info["shareReserves"],
+        latest_pool_info["bondReserves"],
+        pool_config["invTimeStretch"],
+        pool_config["initialSharePrice"],
+        pool_config["positionDuration"],
+        wallet_shorts["maturityTime"],
+        block_timestamp,
+    )
+    shorts_pnl = wallet_shorts["tokenValue"] * (1 - short_spot_prices)
+
+    # Calculate for longs
+    wallet_longs = current_wallet[current_wallet["baseTokenType"] == "LONG"]
+    long_spot_prices = calculate_spot_price_for_position(
+        latest_pool_info["shareReserves"],
+        latest_pool_info["bondReserves"],
+        pool_config["invTimeStretch"],
+        pool_config["initialSharePrice"],
+        pool_config["positionDuration"],
+        wallet_longs["maturityTime"],
+        block_timestamp,
+    )
+    long_pnl = wallet_longs["tokenValue"] * long_spot_prices
+
+    # Add pnl to current_wallet information
+    # Index should match, so it's magic
+    current_wallet.loc[base_pnl.index, "pnl"] = base_pnl
+    current_wallet.loc[lp_pnl.index, "pnl"] = lp_pnl
+    current_wallet.loc[shorts_pnl.index, "pnl"] = shorts_pnl
+    current_wallet.loc[long_pnl.index, "pnl"] = long_pnl
+    pnl = current_wallet.reset_index().groupby("walletAddress")["pnl"].sum()
+    return pnl
 
 
 def calculate_pnl(
@@ -160,4 +205,5 @@ def plot_pnl(agent_positions: dict[str, AgentPosition], axes) -> None:
     axes.yaxis.tick_right()
     axes.set_title("pnl over time")
 
+    # %%
     # %%
--- examples/hackweek_demo/run_demo.py
+++ examples/hackweek_demo/run_demo.py
@@ -10,11 +10,9 @@ from dotenv import load_dotenv
 from extract_data_logs import get_combined_data
 from plot_fixed_rate import calc_fixed_rate, plot_fixed_rate
 from plot_ohlcv import calc_ohlcv, plot_ohlcv
+from plot_pnl import calculate_current_pnl
 
 from elfpy.data import postgres
-
-# from plot_pnl import calculate_pnl, plot_pnl
-
 
 # pylint: disable=invalid-name
 
@@ -43,7 +41,7 @@ def get_ticker(data: pd.DataFrame, lookup: pd.DataFrame) -> pd.DataFrame:
     """
     # Return reverse of methods to put most recent transactions at the top
 
-    usernames = username_to_address(lookup, data[["operator"]])
+    usernames = username_to_address(lookup, data["operator"])
     ticker_data = data.reset_index()[["timestamp", "blockNumber", "operator", "trade_type", "value"]].copy()
     ticker_data.insert(2, "username", usernames.values.tolist())
     ticker_data.columns = ["Timestamp", "Block", "User", "Wallet", "Method", "Amount"]
@@ -51,6 +49,60 @@ def get_ticker(data: pd.DataFrame, lookup: pd.DataFrame) -> pd.DataFrame:
     ticker_data["Wallet"] = ticker_data["Wallet"].str[:6] + "..." + ticker_data["Wallet"].str[-4:]
     ticker_data = ticker_data.set_index("Timestamp").sort_index(ascending=False)
     return ticker_data
+
+
+def combine_usernames(username: pd.Series) -> pd.DataFrame:
+    # Hard coded mapping:
+    user_mapping = {
+        "Charles St. Louis (click)": "Charles St. Louis",
+        "Alim Khamisa (click)": "Alim Khamisa",
+        "Danny Delott (click)": "Danny Delott",
+        "Gregory Lisa (click)": "Gregory Lisa",
+        "Jonny Rhea (click)": "Jonny Rhea",
+        "Matt Brown (click)": "Matt Brown",
+        "Giovanni Effio (click)": "Giovanni Effio",
+        "Mihai Cosma (click)": "Mihai Cosma",
+        "Ryan Goree (click)": "Ryan Goree",
+        "Alex Towle (click)": "Alex Towle",
+        "Adelina Ruffolo (click)": "Adelina Ruffolo",
+        "Jacob Arruda (click)": "Jacob Arruda",
+        "Dylan Paiton (click)": "Dylan Paiton",
+        "Sheng Lundquist (click)": "Sheng Lundquist",
+        "ControlC Schmidt (click)": "ControlC Schmidt",
+        "George Towle (click)": "George Towle",
+        "Jack Burrus (click)": "Jack Burrus",
+        # Bot accounts
+        "slundquist (bots)": "Sheng Lundquist",
+    }
+    user_mapping = pd.DataFrame.from_dict(user_mapping, orient="index")
+    user_mapping.columns = ["user"]
+    # Use merge in case mapping doesn't exist
+    username_column = username.name
+    user = username.to_frame().merge(user_mapping, how="left", left_on=username_column, right_index=True)
+    return user
+
+
+def get_leaderboard(pnl: pd.Series, lookup: pd.DataFrame) -> tuple[pd.DataFrame, pd.DataFrame]:
+    """Given PNL, does lookup against usernames and ranks the leaderboard"""
+    pnl = pnl.reset_index()
+    usernames = username_to_address(lookup, pnl["walletAddress"])
+    pnl.insert(1, "username", usernames.values.tolist())
+    # Hard coded funding provider from migration account
+    migration_addr = "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266"
+    # Don't show this account
+    pnl = pnl[pnl["walletAddress"] != migration_addr]
+    # Rank based on pnl
+    user = combine_usernames(pnl["username"])
+    pnl["user"] = user["user"].values
+
+    ind_leaderboard = (
+        pnl[["username", "walletAddress", "pnl"]].sort_values("pnl", ascending=False).reset_index(drop=True)
+    )
+    comb_leaderboard = (
+        pnl[["user", "pnl"]].groupby("user")["pnl"].sum().reset_index().sort_values("pnl", ascending=False)
+    ).reset_index(drop=True)
+
+    return (comb_leaderboard, ind_leaderboard)
 
 
 def get_click_addresses() -> pd.DataFrame:
@@ -112,7 +164,7 @@ def get_user_lookup() -> pd.DataFrame:
     return options_map.reset_index()
 
 
-def username_to_address(lookup: pd.DataFrame, selected_list: pd.DataFrame) -> pd.Series:
+def username_to_address(lookup: pd.DataFrame, selected_list: pd.Series) -> pd.Series:
     """Helper function to lookup selected users/addrs to all addresses
 
     Arguments
@@ -128,7 +180,8 @@ def username_to_address(lookup: pd.DataFrame, selected_list: pd.DataFrame) -> pd
     list[str]
         A list of addresses based on selected_list
     """
-    out = selected_list.merge(lookup, how="left", left_on="operator", right_on="address")
+    selected_list_column = selected_list.name
+    out = selected_list.to_frame().merge(lookup, how="left", left_on=selected_list_column, right_on="address")
     return out["username"]
 
 
@@ -169,12 +222,21 @@ while True:
     pool_info_data = postgres.get_pool_info(session, -max_live_blocks)
     combined_data = get_combined_data(txn_data, pool_info_data)
     ticker = get_ticker(combined_data, user_lookup)
+    wallets = postgres.get_current_wallet_info(session)
 
     (fixed_rate_x, fixed_rate_y) = calc_fixed_rate(combined_data, config_data)
     ohlcv = calc_ohlcv(combined_data, config_data, freq="5T")
 
+    curr_pnl = calculate_current_pnl(config_data, pool_info_data, wallets)
+    comb_rank, ind_rank = get_leaderboard(curr_pnl, user_lookup)
+
     with ticker_placeholder.container():
+        st.header("Ticker")
         st.dataframe(ticker, height=200, use_container_width=True)
+        st.header("Total Leaderboard")
+        st.dataframe(comb_rank, height=500, use_container_width=True)
+        st.header("Wallet Leaderboard")
+        st.dataframe(ind_rank, height=500, use_container_width=True)
 
     with main_placeholder.container():
         # Clears all axes
