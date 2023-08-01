e78131c0a52c1f67bd314fa0126413ec6be82ca0
Dylan <dpaiton@gmail.com>
2023-07-31 22:56:57 -0700

    adds get_agent_account

--- examples/eth_bots/get_agent_accounts.py
+++ examples/eth_bots/get_agent_accounts.py
@@ -13,9 +13,8 @@ from web3 import Web3
 from web3.contract.contract import Contract
 
 from elfpy import eth
-from elfpy.agents.agent import Agent
 from elfpy.bots import AgentConfig
-from elfpy.eth.accounts import EthAccount
+from elfpy.eth.accounts import EthAgent
 
 # pylint: disable=too-many-locals
 
@@ -26,7 +25,7 @@ def get_agent_accounts(
     base_token_contract: Contract,
     hyperdrive_address: str,
     rng: NumpyGenerator,
-) -> list[EthAccount]:
+) -> list[EthAgent]:
     """Get agents according to provided config, provide eth, base token and approve hyperdrive.
 
     Arguments
@@ -44,14 +43,14 @@ def get_agent_accounts(
 
     Returns
     -------
-    list[EthAccount]
-        A list of EthAccount objects that contain a wallet address and Elfpy Agent for determining trades
+    list[EthAgent]
+        A list of EthAgent objects that contain a wallet address and Elfpy Agent for determining trades
     """
     # load user dotenv variables
     load_dotenv()
     # TODO: raise issue on failure by looking at `rpc_response`, `tx_receipt` returned from function
     #   Do this for `set_anvil_account_balance`, `smart_contract_transact(mint)`, `smart_contract_transact(approve)`
-    agents: list[EthAccount] = []
+    agents: list[EthAgent] = []
     num_agents_so_far: list[int] = []  # maintains the total number of agents for each agent type
     key_string = os.environ.get("AGENT_KEYS")
     if key_string is None:
@@ -75,35 +74,34 @@ def get_agent_accounts(
             else:
                 kwargs["budget"] = agent_info.base_budget.sample_budget(rng)
                 # TODO: This is where we would fund the bots if we wanted to mint money from nothing
-            # the Agent object holds the policy, which makes decisions based
+            # the agent object holds the policy, which makes decisions based
             # on the market and can produce a list of trades
-            # NOTE: the wallet_address argument is a throwaway right now; the eth_account will have the actual address
-            agent = Agent(wallet_address=agent_count, policy=agent_info.policy(**kwargs))
-            # the eth_account holds an agent (where the smarts lies) as well as a wallet (an address used by contracts)
             if len(agent_private_keys) < agent_count:
                 raise AssertionError(
                     "Private keys must be specified for the eth_bots demo. Did you list enough in your .env?"
                 )
-            eth_account = eth.accounts.EthAccount(agent=agent, private_key=agent_private_keys[agent_count])
-            agent_eth_funds = eth.rpc_interface.get_account_balance(web3, eth_account.checksum_address)
+            eth_agent = eth.accounts.EthAgent(
+                policy=agent_info.policy(**kwargs), private_key=agent_private_keys[agent_count]
+            )
+            agent_eth_funds = eth.rpc_interface.get_account_balance(web3, eth_agent.checksum_address)
             if agent_eth_funds == 0:
                 raise AssertionError(
-                    f"Agent needs Ethereum to operate! The agent {eth_account.checksum_address=} has a "
+                    f"Agent needs Ethereum to operate! The agent {eth_agent.checksum_address=} has a "
                     f"balance of {agent_eth_funds=}.\nDid you fund their accounts?"
                 )
-            agent_base_funds = eth.smart_contract_read(base_token_contract, "balanceOf", eth_account.checksum_address)
+            agent_base_funds = eth.smart_contract_read(base_token_contract, "balanceOf", eth_agent.checksum_address)
             if agent_base_funds["value"] == 0:
                 raise AssertionError("Agent needs Base tokens to operate! Did you fund their accounts?")
             # establish max approval for the hyperdrive contract
             _ = eth.smart_contract_transact(
                 web3,
                 base_token_contract,
-                eth_account,
+                eth_agent,
                 "approve",
                 hyperdrive_address,
                 eth_utils.conversions.to_int(eth_utils.currency.MAX_WEI),
             )
-            agents.append(eth_account)
+            agents.append(eth_agent)
         num_agents_so_far.append(agent_info.number_of_agents)
     logging.info("Added %d agents", sum(num_agents_so_far))
     return agents
