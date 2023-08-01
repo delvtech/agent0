34885bf8f477ffdc45496c5b445625f1e3fc1f26
Matthew Brown <mattfdbrown@gmail.com>
2023-08-01 03:44:21 -0700

    Add redeem withdraw shares to HyperdriveMarketAction (#767)

--- examples/eth_bots/execute_agent_trades.py
+++ examples/eth_bots/execute_agent_trades.py
@@ -400,6 +400,29 @@ async def async_match_contract_call_to_trade(
                 lp_tokens=-trade_result.lp_amount,
                 withdraw_shares=trade_result.withdrawal_share_amount,
             )
+        case MarketActionType.REDEEM_WITHDRAW_SHARE:
+            # for now, assume an underlying vault share price of at least 1, should be higher by a bit
+            min_output = FixedPoint(1)
+
+            # NOTE: This is not guaranteed to redeem all shares.  The pool will try to redeem as
+            # many as possible, up to the withdrawPool.readyToRedeem limit, without reverting.  Only
+            # a min_output that is too high will cause a revert here, or trying to withdraw more
+            # shares than the user has obviously.
+            fn_args = (trade_amount, min_output, account.checksum_address, as_underlying)
+            trade_result = await async_transact_and_parse_logs(
+                web3,
+                hyperdrive_contract,
+                account,
+                "redeemWithdrawalShares",
+                *fn_args,
+            )
+            wallet_deltas = WalletDeltas(
+                balance=Quantity(
+                    amount=trade_result.base_amount,
+                    unit=TokenType.BASE,
+                ),
+                withdraw_shares=-trade_result.withdrawal_share_amount,
+            )
         case MarketActionType.INITIALIZE_MARKET:
             raise ValueError(f"{trade.action_type} not supported!")
         case _:
