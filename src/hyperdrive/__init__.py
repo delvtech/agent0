bc933a7adca6def48fe7c21f9c83973ef2ddaaad
Dylan <dpaiton@gmail.com>
2023-08-03 12:01:50 -0700

    import contract interface

--- src/hyperdrive/__init__.py
+++ src/hyperdrive/__init__.py
@@ -1,4 +1,5 @@
 """Interfaces for bots and hyperdrive smart contracts."""
 
+from . import contract_interface
 from .addresses import HyperdriveAddresses
 from .assets import AssetIdPrefix, decode_asset_id, encode_asset_id
