b12a7f5145db820d4d1a859e7d831bc0489011e9
Dylan <dpaiton@gmail.com>
2023-08-04 15:33:30 -0700

    nvm fixedpoint gone again :(

--- lib/fixedpointmath/fixedpointmath/errors.py
+++
@@ -1,8 +0,0 @@
-"""Define Python user-defined exceptions"""
-from __future__ import annotations
-
-
-class DivisionByZero(Exception):
-    """
-    For FixedPoint type; thrown if trying to divide any FixedPoint number by zero.
-    """
