f12faf6cf704d8d3f3239ebe3cbfa1488a07d8e8
Dylan <dpaiton@gmail.com>
2023-08-04 23:49:02 -0700

    changes username to my temp

--- lib/agent0/agent0/hyperdrive/config/runner_config.py
+++ lib/agent0/agent0/hyperdrive/config/runner_config.py
@@ -31,7 +31,7 @@ def get_eth_bots_config() -> tuple[EnvironmentConfig, list[AgentConfig]]:
         username_register_url="http://localhost:5002",
         artifacts_url="http://localhost:8080",
         rpc_url="http://localhost:8545",
-        username="matt",
+        username="changem3",
     )
 
     agent_config: list[AgentConfig] = [
