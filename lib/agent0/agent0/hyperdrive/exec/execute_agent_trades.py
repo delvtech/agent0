70eedae042462d4973133fa4860b3d8df61b6089
Dylan <dpaiton@gmail.com>
2023-08-07 10:09:21 -0700

    adds docs

--- lib/agent0/agent0/hyperdrive/exec/execute_agent_trades.py
+++ lib/agent0/agent0/hyperdrive/exec/execute_agent_trades.py
@@ -32,7 +32,12 @@ if TYPE_CHECKING:
 
 
 def assert_never(arg: NoReturn) -> NoReturn:
-    """Helper function for exhaustive matching on ENUMS"""
+    """Helper function for exhaustive matching on ENUMS.
+
+    .. note::
+        This ensures that all ENUM values are checked, via an exhaustive match:
+        https://github.com/microsoft/pyright/issues/2569
+    """
     assert False, f"Unhandled type: {type(arg).__name__}"
 
 
