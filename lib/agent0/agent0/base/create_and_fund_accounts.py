77d43249ff975641b266dc874d8ccbeb606c5959
Dylan <dpaiton@gmail.com>
2023-08-07 11:17:29 -0700

    reworks some of the run scripts

---
+++ lib/agent0/agent0/base/create_and_fund_accounts.py
@@ -0,0 +1,6 @@
+"""Create agent accounts and fund them."""
+from __future__ import annotations
+
+from agent0.base.make_key import make_private_key
+
+user_private_key = make_private_key()
--- lib/agent0/bin/run_hyperdrive_agents.py
+++ lib/agent0/bin/run_hyperdrive_agents.py
@@ -1,6 +1,7 @@
 """Main script for running bots on Hyperdrive"""
 from __future__ import annotations
 
+import argparse
 import logging
 import warnings
 
@@ -8,14 +9,35 @@ from agent0.hyperdrive.exec import setup_experiment, trade_if_new_block
 from eth_typing import BlockNumber
 
 
+def parse_args():
+    """Define and parse arguments from stdin"""
+    parser = argparse.ArgumentParser(
+        prog="run_hyperdrive_agents",
+        description="Example execution script for running agents on Hyperdrive",
+        epilog="See the agent0 README in https://github.com/delvtech/elf-simulations/ for more details",
+    )
+    parser.add_argument(
+        "--develop",
+        default=False,
+        type=bool,
+        help="If set, then bots will get funded automatically by minting Ethereum and Base",
+    )
+    return parser.parse_args()
+
+
 def main():
     """Entrypoint to load all configurations and run agents."""
-    # exposing the base_token_contract for debugging purposes.
-    # pylint: disable=unused-variable
-    # sane defaults to avoid spam from libraries
+    # Set sane logging defaults to avoid spam from dependencies
     logging.getLogger("urllib3").setLevel(logging.WARNING)
     logging.getLogger("web3").setLevel(logging.WARNING)
     warnings.filterwarnings("ignore", category=UserWarning, module="web3.contract.base_contract")
+    # Grab stdin args, fund bots if requested
+    args = parse_args()
+    if args.develop: # we need to fund the bots
+
+
+    # exposing the base_token_contract for debugging purposes.
+    # pylint: disable=unused-variable
     web3, hyperdrive_contract, base_token_contract, environment_config, agent_accounts = setup_experiment()
     last_executed_block = BlockNumber(0)
     while True:
