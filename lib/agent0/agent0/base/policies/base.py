fdc40fab8ed4707306c6a5f178afe940ffced70e
Sheng Lundquist <sheng@delv.tech>
2023-08-14 13:54:43 -0700

    Adding abstract hyperdrive policy class

--- lib/agent0/agent0/base/policies/base.py
+++ lib/agent0/agent0/base/policies/base.py
@@ -25,6 +25,7 @@ class BasePolicy(Generic[MarketState, Wallet]):
     def __init__(
         self, budget: FixedPoint, rng: NumpyGenerator | None = None, slippage_tolerance: FixedPoint | None = None
     ):
+        # TODO budget should have a flag to allow for "the budget is however much this wallet has"
         if not isinstance(budget, FixedPoint):
             raise TypeError(f"{budget=} must be of type `FixedPoint`")
         self.budget: FixedPoint = budget
