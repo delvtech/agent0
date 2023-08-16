8d87bae8e322660b854685273fb222b7db74772d
Dylan <dpaiton@gmail.com>
2023-08-16 13:47:16 -0700

    bots -> agents

--- lib/agent0/bin/fund_agents_from_user_key.py
+++ lib/agent0/bin/fund_agents_from_user_key.py
@@ -19,7 +19,7 @@ if __name__ == "__main__":
             "Run the script with a user's private key as argument to include it in the output."
             "Make sure you set the config variables in lib/agent0/agent0/hyperdrive/config/runner_config.py "
             "before running this script."
-            "See the README on https://github.com/delvtech/elf-simulations/eth_bots/ for more implementation details"
+            "See the README on https://github.com/delvtech/elf-simulations/agent0/ for more implementation details"
         ),
     )
     parser.add_argument(
