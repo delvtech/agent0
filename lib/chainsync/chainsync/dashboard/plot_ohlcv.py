87ca4bb045eed40a8d748b601572fe638964b504
Sheng Lundquist <sheng@delv.tech>
2023-08-22 15:56:44 -0700

    Fixing y axis of ohlcv plot

--- lib/chainsync/chainsync/dashboard/plot_ohlcv.py
+++ lib/chainsync/chainsync/dashboard/plot_ohlcv.py
@@ -2,6 +2,7 @@
 from __future__ import annotations
 
 import mplfinance as mpf
+from matplotlib import ticker as mpl_ticker
 
 
 def plot_ohlcv(ohlcv, ohlcv_ax):
@@ -9,6 +10,7 @@ def plot_ohlcv(ohlcv, ohlcv_ax):
     if len(ohlcv > 0):
         mpf.plot(ohlcv, type="candle", ax=ohlcv_ax)
 
+        ohlcv_ax.yaxis.set_major_formatter(mpl_ticker.FuncFormatter(lambda x, p: format(x, "0.6")))
         ohlcv_ax.set_xlabel("block timestamp")
         ohlcv_ax.set_title("OHLCV")
         ohlcv_ax.yaxis.set_label_position("right")
