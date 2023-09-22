06296d2ea6340b44c850ef3b831845afbbda0abd
Sheng Lundquist <sheng@delv.tech>
2023-09-21 17:47:19 -0700

    Adding function structure for building wallet positions from data

--- lib/agent0/agent0/hyperdrive/exec/run_agents.py
+++ lib/agent0/agent0/hyperdrive/exec/run_agents.py
@@ -5,11 +5,14 @@ import logging
 import os
 import warnings
 
+import pandas as pd
 from agent0 import AccountKeyConfig
 from agent0.base.config import DEFAULT_USERNAME, AgentConfig, EnvironmentConfig
 from eth_typing import BlockNumber
 from ethpy import EthConfig, build_eth_config
+from ethpy.base import smart_contract_read
 from ethpy.hyperdrive import HyperdriveAddresses, fetch_hyperdrive_address_from_uri
+from web3.contract.contract import Contract
 
 from .create_and_fund_user_account import create_and_fund_user_account
 from .fund_agents import fund_agents
@@ -68,7 +71,7 @@ def run_agents(
             user_account, eth_config, account_key_config, contract_addresses
         )  # uses env variables created above as inputs
 
-    web3, _, hyperdrive_contract, agent_accounts = setup_experiment(
+    web3, base_contract, hyperdrive_contract, agent_accounts = setup_experiment(
         eth_config, environment_config, agent_config, account_key_config, contract_addresses
     )
 
@@ -84,10 +87,12 @@ def run_agents(
         # Register wallet addresses to username
         register_username(environment_config.database_api_uri, wallet_addrs, environment_config.username)
 
-    # Get existing open positions from db
+    # Get existing open positions from db api server
     balances = balance_of(environment_config.database_api_uri, wallet_addrs)
 
     # TODO Set balances of wallets based on db
+    for agent in agent_accounts:
+        build_wallet_positions_from_data(agent.checksum_address, balances, base_contract)
 
     last_executed_block = BlockNumber(0)
     while True:
@@ -98,3 +103,27 @@ def run_agents(
             environment_config.halt_on_errors,
             last_executed_block,
         )
+
+
+def build_wallet_positions_from_data(wallet_addr: str, db_balances: pd.DataFrame, base_contract: Contract):
+    # Contract call to get base balance
+    base_amount = smart_contract_read(base_contract, "balanceOf", wallet_addr)
+    # TODO build base object
+
+    # TODO We can also get lp and withdraw shares from chain?
+    wallet_balances = db_balances[db_balances["walletAddress"] == wallet_addr]
+
+    # Get longs
+    long_balances = wallet_balances[wallet_balances["baseTokenType" == "LONG"]]
+    # TODO iterate through long balances and build wallet object
+
+    short_balances = wallet_balances[wallet_balances["baseTokenType" == "SHORT"]]
+    # TODO iterate through short balances and build wallet object
+
+    lp_balances = wallet_balances[wallet_balances["baseTokenType" == "LP"]]
+    assert len(lp_balances) <= 1
+    # TODO Build LP balances object
+
+    withdraw_balances = wallet_balances[wallet_balances["baseTokenType" == "WITHDRAWAL_SHARE"]]
+    assert len(withdraw_balances) <= 1
+    # TODO Build withdraw share object
