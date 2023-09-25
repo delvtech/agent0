831fbe19b11705dec35b56fb548cdb595a30b94a
Dylan <dpaiton@gmail.com>
2023-09-25 10:04:06 -0700

    new gen files

--- lib/hyperdrive_types/hyperdrive_types/ForwarderFactoryTypes.py
+++ lib/hyperdrive_types/hyperdrive_types/ForwarderFactoryTypes.py
@@ -1,5 +1,10 @@
-# pylint: disable=invalid-name
 """Dataclasses for all structs in the ForwarderFactory contract."""
+# super() call methods are generic, while our version adds values & types
+# pylint: disable=arguments-differ
+# contracts have PascalCase names
+# pylint: disable=invalid-name
+# unable to control how many instance attributes we have in generated code
+# pylint: disable=too-many-instance-attributes
 from __future__ import annotations
 
 from dataclasses import dataclass
