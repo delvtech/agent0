ac2d3047d9c6be755dbad37aa31eb73dad868afa
Sheng Lundquist <sheng@delv.tech>
2023-10-02 14:57:00 -0700

    lint

--- lib/agent0/agent0/hyperdrive/crash_report/crash_report.py
+++ lib/agent0/agent0/hyperdrive/crash_report/crash_report.py
@@ -4,7 +4,7 @@ from __future__ import annotations
 import json
 import logging
 from datetime import datetime
-from typing import Any, Generator
+from typing import Any
 
 import numpy as np
 from elfpy.utils import logs
