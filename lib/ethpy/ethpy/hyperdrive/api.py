25a1f68158ed845ac3cef0cba31283f4b01529b2
Mihai <mcosma@gmail.com>
2023-10-17 16:39:10 -0400

    define maturity_time in case it's not passed in

--- lib/ethpy/ethpy/hyperdrive/api.py
+++ lib/ethpy/ethpy/hyperdrive/api.py
@@ -289,10 +289,9 @@ class HyperdriveInterface(BaseInterface[HyperdriveAddresses]):
                 Governance fee, in shares.
         """
         if maturity_time is None:
-            normalized_time_remaining = FixedPoint(1)
-        else:
-            time_remaining_in_seconds = FixedPoint(maturity_time - self.current_block_time)
-            normalized_time_remaining = time_remaining_in_seconds / self.pool_config["positionDuration"]
+            maturity_time = self.current_block_time + int(self.pool_config["positionDuration"])
+        time_remaining_in_seconds = FixedPoint(maturity_time - self.current_block_time)
+        normalized_time_remaining = time_remaining_in_seconds / self.pool_config["positionDuration"]
         curve_fee = (
             (FixedPoint(1) - self.spot_price) * self.pool_config["curveFee"] * bonds_in * normalized_time_remaining
         ) / self.pool_config["initialSharePrice"]
@@ -332,10 +331,9 @@ class HyperdriveInterface(BaseInterface[HyperdriveAddresses]):
                 Governance fee, in shares.
         """
         if maturity_time is None:
-            normalized_time_remaining = FixedPoint(1)
-        else:
-            time_remaining_in_seconds = FixedPoint(maturity_time - self.current_block_time)
-            normalized_time_remaining = time_remaining_in_seconds / self.pool_config["positionDuration"]
+            maturity_time = self.current_block_time + int(self.pool_config["positionDuration"])
+        time_remaining_in_seconds = FixedPoint(maturity_time - self.current_block_time)
+        normalized_time_remaining = time_remaining_in_seconds / self.pool_config["positionDuration"]
         curve_fee = (
             ((FixedPoint(1) / self.spot_price) - FixedPoint(1))
             * self.pool_config["curveFee"]
