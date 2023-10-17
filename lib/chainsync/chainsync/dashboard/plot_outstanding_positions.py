9e0fd729aa895f0345ad583657a644b69b97a0b5
Sheng Lundquist <sheng@delv.tech>
2023-10-17 11:06:08 -0700

    Adjusting labels and legends of plots

--- lib/chainsync/chainsync/dashboard/plot_outstanding_positions.py
+++ lib/chainsync/chainsync/dashboard/plot_outstanding_positions.py
@@ -2,17 +2,20 @@
 from __future__ import annotations
 
 import matplotlib.pyplot as plt
+from ethpy.hyperdrive import BASE_TOKEN_SYMBOL
 
 
 def plot_outstanding_positions(data, axes):
     """Returns the fixed rate plot"""
-    axes.plot(data["timestamp"], data["longsOutstanding"], label="longs")
-    axes.plot(data["timestamp"], data["shortsOutstanding"], label="shorts")
+    bonds_symbol = "Hy" + BASE_TOKEN_SYMBOL
+
+    axes.plot(data["timestamp"], data["longsOutstanding"], label="Longs")
+    axes.plot(data["timestamp"], data["shortsOutstanding"], label="Shorts")
     # change y-axis unit format to 0.1%
     axes.yaxis.set_label_position("right")
     axes.yaxis.tick_right()
-    axes.set_xlabel("block timestamp")
-    axes.set_ylabel("bonds")
+    axes.set_xlabel("Block Timestamp")
+    axes.set_ylabel(bonds_symbol)
     axes.set_title("Open Positions")
     axes.legend()
     return plt.gcf()
