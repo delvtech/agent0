4fa82874295fe30cd6beb576c3094660299400e5
Sheng Lundquist <sheng@delv.tech>
2023-10-19 11:57:14 -0700

    Adjusting withdrawal shares value

--- lib/chainsync/chainsync/analysis/calc_pnl.py
+++ lib/chainsync/chainsync/analysis/calc_pnl.py
@@ -86,9 +86,8 @@ def calc_single_closeout(
             )
             out_pnl = Decimal(
                 preview_result["baseProceeds"]
-                + preview_result["withdrawalShares"]
-                * pool_info["sharePrice"].values[-1]
-                * pool_info["lpSharePrice"].values[-1]
+                # We assume all withdrawal shares are redeemable
+                + preview_result["withdrawalShares"] * pool_info["lpSharePrice"].values[-1]
             ) / Decimal(1e18)
         except Exception as exception:  # pylint: disable=broad-except
             logging.warning("Exception caught, ignoring: %s", exception)
@@ -99,9 +98,7 @@ def calc_single_closeout(
             # For PNL, we assume all withdrawal shares are redeemable
             # even if there are no withdrawal shares available to withdraw
             # Hence, we don't use preview transaction here
-            out_pnl = Decimal(
-                amount * pool_info["sharePrice"].values[-1] * pool_info["lpSharePrice"].values[-1]
-            ) / Decimal(1e18)
+            out_pnl = Decimal(amount * pool_info["lpSharePrice"].values[-1]) / Decimal(1e18)
         except Exception as exception:  # pylint: disable=broad-except
             logging.warning("Exception caught, ignoring: %s", exception)
     else:
