75bc72c706f8f29163e6dc9190b928daad8dfa4e
Sheng Lundquist <sheng@delv.tech>
2023-11-13 10:03:17 -0800

    lint

--- lib/agent0/bin/get_all_events.py
+++ lib/agent0/bin/get_all_events.py
@@ -2,6 +2,9 @@
 import pandas as pd
 from ethpy.hyperdrive.api import HyperdriveInterface
 
+# This is meant to be a standalone script, no need for global upper_case naming style
+# pylint: disable=invalid-name
+
 # TODO parameterize this if we're trying to connect to something other than local chain
 hyperdrive = HyperdriveInterface()
 
@@ -12,8 +15,7 @@ hyperdrive_event_objs = hyperdrive.hyperdrive_contract.events
 hyperdrive_events = [event for key, event in hyperdrive_event_objs.__dict__.items() if key not in ("abi", "_events")]
 
 # Iterate through each event class and get previous logs for each event
-# This is meant to be a standalone script, no need for global upper_case naming style
-out_events = []  # pylint: disable=invalid-name
+out_events = []
 for event_obj in hyperdrive_events:
     events = event_obj.get_logs(fromBlock=0)
     # Parse through event data into dict
