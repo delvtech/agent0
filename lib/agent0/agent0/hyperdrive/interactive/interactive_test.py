bc2e06fed6d2466305805a0cbcd847d8734fbe56
Sheng Lundquist <sheng@delv.tech>
2023-11-16 17:44:13 -0800

    Allowing for multiple agents

--- lib/agent0/agent0/hyperdrive/interactive/interactive_test.py
+++ lib/agent0/agent0/hyperdrive/interactive/interactive_test.py
@@ -1,15 +1,19 @@
 import pandas as pd
 from fixedpointmath import FixedPoint
 
-from agent0.hyperdrive.interactive import InitialPool, InteractiveHyperdrive
+from agent0.hyperdrive.interactive import InitialPoolConfig, InteractiveHyperdrive
 
 # Parameters for pool initialization. If empty, defaults to default values, allows for custom values if needed
-initial_pool_config = InitialPool()
+initial_pool_config = InitialPoolConfig()
 
 # Initialize the interactive object with specified initial pool parameters
 # This launches a local chain in a subprocess (or docker) and a database
-hyperdrive_agent0 = InteractiveHyperdrive(initial_pool_config)
-# One agent is created and funded ready to go, likely using something similar to the "deterministic" policy
+# A "admin" user is launched/funded here for deploying hyperdrive
+interactive_hyperdrive = InteractiveHyperdrive(initial_pool_config)
+
+# Generate funded trading agents from the interactive object
+hyperdrive_agent0 = interactive_hyperdrive.init_agent(eth=FixedPoint(100), base=FixedPoint(100000))
+hyperdrive_agent1 = interactive_hyperdrive.init_agent(eth=FixedPoint(100), base=FixedPoint(100000))
 
 # Here, we execute a trade, where it's calling agent0 + gather data from data pipeline
 # under the hood to allow for error handling and data management
@@ -29,7 +33,7 @@ print(hyperdrive_agent0.wallet)
 hyperdrive_agent0.close_long(maturity_time=long_event_1.maturity_time, value=long_event_1.value)
 
 # Get data from database under the hood
-pool_info_history: pd.DataFrame = hyperdrive_agent0.get_pool_info_history()
+pool_info_history: pd.DataFrame = interactive_hyperdrive.get_pool_info_history()
 
 # Plot pretty plots
 pool_info_history.plot(x="block_time", y="outstanding_longs", kind="line")
