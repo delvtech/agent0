168e2b5b57328299e944aa9fecce800b65df5e80
Matthew Brown <mattfdbrown@gmail.com>
2023-11-20 12:17:38 -0700

    fix file

--- lib/agent0/agent0/hyperdrive/policies/lpandarb.py
+++ lib/agent0/agent0/hyperdrive/policies/lpandarb.py
@@ -10,7 +10,6 @@ from typing import TYPE_CHECKING
 
 from ethpy.hyperdrive.state import PoolState
 from fixedpointmath import FixedPoint
-from hypertypes.fixedpoint_types import PoolInfoFP
 
 from agent0.base import MarketType, Trade
 from agent0.hyperdrive.policies.hyperdrive_policy import HyperdrivePolicy
@@ -18,7 +17,6 @@ from agent0.hyperdrive.state import HyperdriveActionType, HyperdriveMarketAction
 
 if TYPE_CHECKING:
     from ethpy.hyperdrive.api import HyperdriveInterface
-    from hypertypes.fixedpoint_types import PoolConfigFP
     from numpy.random._generator import Generator as NumpyGenerator
 
     from agent0.hyperdrive.state import HyperdriveWallet
@@ -146,36 +144,22 @@ def apply_step(
 ) -> PoolState:
     """Save a single convergence step into the pool info.
 
-        Arguments
-        ---------
+    Arguments
+    ---------
+    pool_state : PoolState
+        The current pool state.
+    bonds_needed : FixedPoint
+        The amount of bonds that is going to be traded.
+    shares_needed : FixedPoint
+        The amount of shares that is going to be traded.
+    gov_fee : FixedPoint
+        The associated governance fee.
 
 
-
-
-    # TODO: These dataclasses are similar to pypechain except for
-    #  - snake_case attributes instead of camelCase
-    #  - FixedPoint types instead of int
-    #  - nested dataclasses (PoolConfig) include a __post_init__ that allows for
-    #  instantiation with a nested dictionary
-    #
-    # We'd like to rely on the pypechain classes as much as possible.
-    # One solution could be to build our own interface wrapper that pulls in the pypechain
-    # dataclass and makes this fixed set of changes?
-    # pylint: disable=too-many-instance-attributes
-        pool_state : PoolState
-            The current pool state.
-        bonds_needed : FixedPoint
-            The amount of bonds that is going to be traded.
-        shares_needed : FixedPoint
-            The amount of shares that is going to be traded.
-        gov_fee : FixedPoint
-            The associated governance fee.
-
-
-        Returns
-        -------
-        pool_state : PoolState
-            The updated pool state.
+    Returns
+    -------
+    pool_state : PoolState
+        The updated pool state.
     """
     if bonds_needed > 0:  # short case
         # shares_needed is what the user takes OUT: curve_fee less due to fees.
