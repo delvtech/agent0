78424f5e58f851d2fe8988e04f373b120b3ecf86
Sheng Lundquist <sheng@delv.tech>
2023-11-30 13:19:47 -0800

    Removing budget from no action policy

--- lib/agent0/agent0/base/policies/no_action.py
+++ lib/agent0/agent0/base/policies/no_action.py
@@ -19,11 +19,8 @@ if TYPE_CHECKING:
 class NoActionPolicy(BasePolicy[MarketInterface, Wallet]):
     """NoOp class policy"""
 
-    def __init__(self, budget: FixedPoint | None = None, rng: NumpyGenerator | None = None):
-        if budget is None:
-            super().__init__(FixedPoint("0.0"), rng)
-        else:
-            super().__init__(budget, rng)
+    def __init__(self, rng: NumpyGenerator | None = None):
+        super().__init__(rng)
 
     def action(self, interface: MarketInterface, wallet: Wallet) -> tuple[list[Trade], bool]:
         """Returns an empty list, indicating no action
