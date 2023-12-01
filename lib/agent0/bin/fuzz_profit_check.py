e776a574963b18f7218e92b7d5fd83137f4875ae
Dylan <dpaiton@gmail.com>
2023-11-30 17:02:20 -0800

    path independence check

--- lib/agent0/bin/fuzz_profit_check.py
+++ lib/agent0/bin/fuzz_profit_check.py
@@ -1,4 +1,4 @@
-"""Example script for using interactive hyperdrive."""
+"""Script for fuzzing profit values on immediately opening & closing a long or short."""
 # %%
 # Variables by themselves print out dataframes in a nice format in interactive mode
 # pylint: disable=pointless-statement
@@ -19,7 +19,6 @@ if LOCAL:
 else:
     chain_config = Chain.Config(db_port=5004, remove_existing_db_container=True)
     chain = Chain(rpc_uri="http://localhost:8545", config=chain_config)
-rng = np.random.default_rng()  # No seed, we want this to be random every time it is executed
 
 # %%
 # Parameters for pool initialization.
@@ -27,25 +26,51 @@ initial_pool_config = InteractiveHyperdrive.Config()
 interactive_hyperdrive = InteractiveHyperdrive(chain, initial_pool_config)
 
 # %%
+# Get a random trade amount
+rng = np.random.default_rng()  # No seed, we want this to be random every time it is executed
 trade_amount = rng.uniform(
     low=interactive_hyperdrive.hyperdrive_interface.pool_config.minimum_transaction_amount, high=int(1e23)
 )
 
 # %%
-# Generate funded trading agents from the interactive object
+# Generate funded trading agent
 hyperdrive_agent0 = interactive_hyperdrive.init_agent(
     base=FixedPoint(scaled_value=trade_amount * 2), eth=FixedPoint(100), name="alice"
 )
 
 # %%
 # Open a long and close it immediately
-open_long_event_1 = hyperdrive_agent0.open_long(base=FixedPoint(scaled_value=trade_amount))
-close_long_event_1 = hyperdrive_agent0.close_long(
-    maturity_time=open_long_event_1.maturity_time, bonds=open_long_event_1.bond_amount
+open_long_event = hyperdrive_agent0.open_long(base=FixedPoint(scaled_value=trade_amount))
+close_long_event = hyperdrive_agent0.close_long(
+    maturity_time=open_long_event.maturity_time, bonds=open_long_event.bond_amount
 )
 # %%
 # TODO:
 # Ensure that the prior trades did not result in a profit
-open_long_event_1
-close_long_event_1
+open_long_event
+close_long_event
+hyperdrive_agent0.wallet
+
+# %%
+# Open a short and close it immediately
+open_short_event = hyperdrive_agent0.open_short(bonds=FixedPoint(scaled_value=trade_amount))
+close_short_event = hyperdrive_agent0.close_short(
+    maturity_time=open_short_event.maturity_time, bonds=open_short_event.bond_amount
+)
+# %%
+# TODO:
+# Ensure that the prior trades did not result in a profit
+open_short_event
+close_short_event
+hyperdrive_agent0.wallet
+
+# %%
+# Add liquidity and redeem it immediately
+add_liquidity_event = hyperdrive_agent0.add_liquidity(base=FixedPoint(scaled_value=trade_amount))
+remove_liquidity_event = hyperdrive_agent0.remove_liquidity(shares=add_liquidity_event.lp_amount)
+# %%
+# TODO:
+# Ensure that the prior trades did not result in a profit
+add_liquidity_event
+remove_liquidity_event
 hyperdrive_agent0.wallet
