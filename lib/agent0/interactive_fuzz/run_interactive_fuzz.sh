7bc3a45ab7abeeefd969168f1fb4f7d33350b897
Sheng Lundquist <sheng@delv.tech>
2023-12-04 13:12:20 -0800

    Directory structure of interactive fuzz testing and adding shell script to run all

---
+++ lib/agent0/interactive_fuzz/run_interactive_fuzz.sh
@@ -0,0 +1,4 @@
+# TODO Run these in the background, assuming these will handle how to notify or log errors
+python lib/agent0/interactive_fuzz/fuzz_hyperdrive_balance.py;
+python lib/agent0/interactive_fuzz/fuzz_path_independence.py;
+python lib/agent0/interactive_fuzz/fuzz_profit_check.py;
