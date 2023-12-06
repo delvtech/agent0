fd43877847dfc1fcdbd625e1581572e1bfdaf6d9
Dylan <dpaiton@gmail.com>
2023-12-05 16:03:09 -0800

    adds logging

--- lib/agent0/interactive_fuzz/fuzz_path_independence.py
+++ lib/agent0/interactive_fuzz/fuzz_path_independence.py
@@ -2,29 +2,44 @@
 # %%
 from __future__ import annotations
 
+import json
+import logging
+from dataclasses import asdict
+
 import numpy as np
 import pandas as pd
 from fixedpointmath import FixedPoint
+from hyperlogs import ExtendedJSONEncoder, setup_logging
 
 from agent0.hyperdrive.interactive import InteractiveHyperdrive, LocalChain
 from agent0.hyperdrive.interactive.event_types import OpenLong, OpenShort
 from agent0.hyperdrive.interactive.interactive_hyperdrive_agent import InteractiveHyperdriveAgent
 from agent0.hyperdrive.state.hyperdrive_actions import HyperdriveActionType
 
+# Pylint UPPER_CASE name conventions shouldn't always apply in notebooks.
+# pylint: disable=invalid-name
+
+
 # %%
-# Variables by themselves print out dataframes in a nice format in interactive mode
-# pylint: disable=pointless-statement
-
-
+# Set global defaults
 NUM_TRADES = 3
 NUM_PATHS_CHECKED = 10
+
+# %%
+# Setup logging
+log_filename = ".logging/fuzz_path_independence.log"
+setup_logging(
+    log_filename=log_filename,
+    delete_previous_logs=True,
+    log_stdout=False,
+)
 
 # %%
 # Parameters for local chain initialization, defines defaults in constructor
 chain_config = LocalChain.Config()
 chain = LocalChain(config=chain_config)
-# TODO generate a random seed and store the seed in fuzz test report when it fails
-rng = np.random.default_rng()  # No seed, we want this to be random every time it is executed
+random_seed = np.random.randint(low=1, high=99999999)  # No seed, we want this to be random every time it is executed
+rng = np.random.default_rng(random_seed)
 
 # %%
 # Parameters for pool initialization.
@@ -85,13 +100,13 @@ for trade in trade_list:
     trade_events.append((agent, trade_event))
 
 # %%
-# snapshot the chain, so we can load the snapshot & close in different orders
+# Snapshot the chain, so we can load the snapshot & close in different orders
 chain.save_snapshot()
-
 check_data: dict | None = None
 
 
 # %%
+# Close the trades randomly & verify that the final state is unchanged
 for iteration in range(NUM_PATHS_CHECKED):
     print(f"{iteration=}")
     # TODO:
@@ -126,21 +141,65 @@ for iteration in range(NUM_PATHS_CHECKED):
 
     # On subsequent run, check against the saved final state
     else:
-        # This checks that the subset of columns in initial pool info and the latest pool info are identical.
-        pd.testing.assert_series_equal(
-            check_data["check_pool_state_df"], pool_state_df[check_columns].iloc[-1], check_names=False
-        )
-
+        # Check values not provided in the database
         pool_state = interactive_hyperdrive.hyperdrive_interface.get_hyperdrive_state()
-        assert (
-            check_data["hyperdrive_base_balance"] == pool_state.hyperdrive_base_balance
-        ), f"{check_data['hyperdrive_base_balance']=} != {pool_state.hyperdrive_base_balance}"
-        assert (
-            check_data["gov_fees_accrued"] == pool_state.gov_fees_accrued
-        ), f"{check_data['gov_fees_accrued']=} != {pool_state.gov_fees_accrued}"
-        assert (
-            check_data["vault_shares"] == pool_state.vault_shares
-        ), f"{check_data['vault_shares']=} != {pool_state.vault_shares}"
-        assert (
-            check_data["minimum_share_reserves"] == pool_state.pool_config.minimum_share_reserves
-        ), f"{check_data['minimum_share_reserves']=} != {pool_state.pool_config.minimum_share_reserves}"
+
+        failed = False
+        if check_data["hyperdrive_base_balance"] != pool_state.hyperdrive_base_balance:
+            logging.critical(
+                "check_data['hyperdrive_base_balance']=%s != pool_state.hyperdrive_base_balance=%s",
+                check_data["hyperdrive_base_balance"],
+                pool_state.hyperdrive_base_balance,
+            )
+            failed = True
+        if check_data["gov_fees_accrued"] != pool_state.gov_fees_accrued:
+            logging.critical(
+                "check_data['gov_fees_accrued']=%s != pool_state.gov_fees_accrued=%s",
+                check_data["gov_fees_accrued"],
+                pool_state.gov_fees_accrued,
+            )
+            failed = True
+        if check_data["vault_shares"] != pool_state.vault_shares:
+            logging.critical(
+                "check_data['vault_shares']=%s != pool_state.vault_shares=%s",
+                check_data["vault_shares"],
+                pool_state.vault_shares,
+            )
+            failed = True
+        if check_data["minimum_share_reserves"] != pool_state.pool_config.minimum_share_reserves:
+            logging.critical(
+                "check_data['minimum_share_reserves']=%s != pool_state.pool_config.minimum_share_reserves=%s",
+                check_data["minimum_share_reserves"],
+                pool_state.pool_config.minimum_share_reserves,
+            )
+            failed = True
+        # Check that the subset of columns in initial db pool state and the latest pool state are equal
+        if not check_data["check_pool_state_df"].equals(pool_state_df[check_columns].iloc[-1]):
+            try:
+                pd.testing.assert_series_equal(
+                    check_data["check_pool_state_df"], pool_state_df[check_columns].iloc[-1], check_names=False
+                )
+            except AssertionError as err:
+                logging.critical("Database pool info is not equal\n%s", err)
+            failed = True
+
+        if failed:
+            logging.info(
+                "pool_config = %s\n\npool_info = %s\n\nlatest_checkpoint = %s\n\nadditional_info = %s",
+                json.dumps(asdict(pool_state.pool_config), indent=2, cls=ExtendedJSONEncoder),
+                json.dumps(asdict(pool_state.pool_info), indent=2, cls=ExtendedJSONEncoder),
+                json.dumps(asdict(pool_state.checkpoint), indent=2, cls=ExtendedJSONEncoder),
+                json.dumps(
+                    {
+                        "hyperdrive_address": interactive_hyperdrive.hyperdrive_interface.hyperdrive_contract.address,
+                        "base_token_address": interactive_hyperdrive.hyperdrive_interface.base_token_contract.address,
+                        "spot_price": interactive_hyperdrive.hyperdrive_interface.calc_spot_price(pool_state),
+                        "fixed_rate": interactive_hyperdrive.hyperdrive_interface.calc_fixed_rate(pool_state),
+                        "variable_rate": pool_state.variable_rate,
+                        "vault_shares": pool_state.vault_shares,
+                    },
+                    indent=2,
+                    cls=ExtendedJSONEncoder,
+                ),
+            )
+            raise AssertionError(f"Testing failed; see logs in {log_filename}")
