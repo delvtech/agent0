ab5e25ddff77043e03da6b3490852662cbad6fec
Mihai <mcosma@gmail.com>
2023-12-08 12:40:51 -0500

    use docker logic from main

--- lib/agent0/agent0/utilities/docker.py
+++ lib/agent0/agent0/utilities/docker.py
@@ -1,9 +1,10 @@
 """Utilities associated to manipulating Docker."""
-import subprocess
 import logging
 import os
+import subprocess
 import time
 from pathlib import Path
+
 import docker
 from docker.errors import DockerException
 
@@ -19,28 +20,30 @@ def check_docker(infra_folder: Path, restart: bool = False) -> None:
         Restart docker even if it is running.
     """
     try:
-        home_dir = os.path.expanduser("~")
-        socket_path = Path(f"{home_dir}/.docker/desktop/docker.sock")
-        if socket_path.exists():
-            logging.debug("The socket exists at %s.. using it to connect to docker", socket_path)
-            _ = docker.DockerClient(base_url=f"unix://{socket_path}")
-        else:
-            logging.debug("No socket found at %s.. using default socket", socket_path)
+        try:
             _ = docker.from_env()
-    except DockerException as exc:
-        raise DockerException("Failed to connect to docker.") from exc
-    dockerps = _get_docker_ps_and_log()
-    number_of_running_services = dockerps.count("\n") - 1
-    if number_of_running_services > 0:
-        preamble_str = f"Found {number_of_running_services} running services"
-        if restart:
-            _start_docker(f"{preamble_str}, restarting docker...", infra_folder)
+        except Exception:  # pylint: disable=broad-exception-caught
+            home_dir = os.path.expanduser("~")
+            socket_path = Path(f"{home_dir}") / ".docker" / "desktop" / "docker.sock"
+            if socket_path.exists():
+                logging.debug("Docker not found at default socket, using %s..", socket_path)
+                _ = docker.DockerClient(base_url=f"unix://{socket_path}")
+            else:
+                logging.debug("Docker not found.")
+                _ = docker.from_env()
+        dockerps = _get_docker_ps_and_log()
+        number_of_running_services = dockerps.count("\n") - 1
+        if number_of_running_services > 0:
+            preamble_str = f"Found {number_of_running_services} running services"
+            if restart:
+                _start_docker(f"{preamble_str}, restarting docker...", infra_folder)
+            else:
+                logging.info("%s, using them.", preamble_str)
         else:
-            logging.info("%s, using them.", preamble_str)
-    else:
-        _start_docker("Starting docker.", infra_folder)
-    dockerps = os.popen("docker ps --format 'table {{.Names}}\t{{.Status}}\t{{.Ports}}'").read()
-    logging.info(dockerps)
+            _start_docker("Starting docker.", infra_folder)
+        logging.info(dockerps)
+    except DockerException as exc:
+        raise DockerException("Docker not found.") from exc
 
 
 def _start_docker(startup_str: str, infra_folder: Path) -> None:
@@ -67,7 +70,7 @@ def _start_docker(startup_str: str, infra_folder: Path) -> None:
 
 def _run_cmd(infra_folder: Path, cmd: str, timing_str: str) -> None:
     """Run a command inside infra_folder, printing out the timing.
-    
+
     Arguments
     ---------
     infra_folder : Path
@@ -76,7 +79,7 @@ def _run_cmd(infra_folder: Path, cmd: str, timing_str: str) -> None:
         Command to run.
     timing_str : str
         String to print out alonside timing.
-        
+
     Returns
     -------
     None
@@ -89,7 +92,7 @@ def _run_cmd(infra_folder: Path, cmd: str, timing_str: str) -> None:
 
 def _get_docker_ps_and_log() -> str:
     """Get docker ps using custom table format and log it.
-    
+
     Returns
     -------
     str
