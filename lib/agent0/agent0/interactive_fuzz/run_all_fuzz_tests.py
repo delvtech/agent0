d176a93d213dc20aca34c9e2d522b1b457170e31
Dylan <dpaiton@gmail.com>
2023-12-08 15:39:51 -0800

    add optional command line arg to run_all_fuzz_tests

--- lib/agent0/agent0/interactive_fuzz/run_all_fuzz_tests.py
+++ lib/agent0/agent0/interactive_fuzz/run_all_fuzz_tests.py
@@ -1,6 +1,10 @@
 """Runs all fuzz tests forever."""
 from __future__ import annotations
 
+import argparse
+import sys
+from typing import NamedTuple, Sequence
+
 from hyperlogs.rollbar_utilities import initialize_rollbar
 
 from agent0.hyperdrive.interactive.chain import LocalChain
@@ -12,13 +16,22 @@ from agent0.interactive_fuzz import (
 )
 
 
-def main():
-    """Runs all fuzz tests"""
+def main(argv: Sequence[str] | None = None):
+    """Runs all fuzz tests.
+
+    Arguments
+    ---------
+    argv: Sequence[str]
+        The argv values returned from argparser.
+    """
+    parsed_args = parse_arguments(argv)
+
     initialize_rollbar("interactivefuzz")
 
     num_trades = 10
     num_paths_checked = 10
 
+    num_checks = 0
     while True:
         try:
             print("Running hyperdrive balance test")
@@ -47,6 +60,59 @@ def main():
             fuzz_profit_check(chain_config)
         except AssertionError:
             pass
+        num_checks += 1
+        if parsed_args.number_of_runs > 0 and num_checks > parsed_args.number_of_runs:
+            break
+
+
+class Args(NamedTuple):
+    """Command line arguments for the invariant checker."""
+
+    number_of_runs: int
+
+
+def namespace_to_args(namespace: argparse.Namespace) -> Args:
+    """Converts argprase.Namespace to Args.
+
+    Arguments
+    ---------
+    namespace: argparse.Namespace
+        Object for storing arg attributes.
+
+    Returns
+    -------
+    Args
+        Formatted arguments
+    """
+    return Args(
+        number_of_runs=namespace.number_of_runs,
+    )
+
+
+def parse_arguments(argv: Sequence[str] | None = None) -> Args:
+    """Parses input arguments.
+
+    Arguments
+    ---------
+    argv: Sequence[str]
+        The argv values returned from argparser.
+
+    Returns
+    -------
+    Args
+        Formatted arguments
+    """
+    parser = argparse.ArgumentParser(description="Runs a loop to check Hyperdrive invariants at each block.")
+    parser.add_argument(
+        "--number_of_runs",
+        type=int,
+        default=0,
+        help="The number times to run the tests. If not set, will run forever.",
+    )
+    # Use system arguments if none were passed
+    if argv is None:
+        argv = sys.argv
+    return namespace_to_args(parser.parse_args())
 
 
 if __name__ == "__main__":
