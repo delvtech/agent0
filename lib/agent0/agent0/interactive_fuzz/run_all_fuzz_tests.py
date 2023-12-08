95bc8cffccb1a53b2db4949be1f2d7a7a8e9e31e
Sheng Lundquist <sheng@delv.tech>
2023-12-07 16:10:08 -0800

    Adding output logs to rollbar payload

--- lib/agent0/agent0/interactive_fuzz/run_all_fuzz_tests.py
+++ lib/agent0/agent0/interactive_fuzz/run_all_fuzz_tests.py
@@ -15,6 +15,13 @@ from agent0.interactive_fuzz import (
 )
 
 
+def _filename_to_payload(log_filename: str):
+    out = {}
+    with open(log_filename, "r", encoding="utf-8") as f:
+        out["logs"] = f.read()
+    return out
+
+
 def main():
     """Runs all fuzz tests"""
     initialize_rollbar("interactivefuzz")
@@ -27,25 +34,29 @@ def main():
             chain_config = LocalChain.Config(db_port=5433, chain_port=10000)
             fuzz_hyperdrive_balance(num_trades, chain_config)
         except AssertionError:
-            rollbar.report_exc_info(sys.exc_info(), "error", payload={})
+            log_filename = ".logging/fuzz_hyperdrive_balance.log"
+            rollbar.report_exc_info(sys.exc_info(), "error", payload=_filename_to_payload(log_filename))
 
         try:
             chain_config = LocalChain.Config(db_port=5434, chain_port=10001)
             fuzz_long_short_maturity_values(num_trades, chain_config)
         except AssertionError:
-            rollbar.report_exc_info(sys.exc_info(), "error", payload={})
+            log_filename = ".logging/fuzz_long_short_maturity_values.log"
+            rollbar.report_exc_info(sys.exc_info(), "error", payload=_filename_to_payload(log_filename))
 
         try:
             chain_config = LocalChain.Config(db_port=5435, chain_port=10002)
             fuzz_path_independence(num_trades, num_paths_checked, chain_config)
         except AssertionError:
-            rollbar.report_exc_info(sys.exc_info(), "error", payload={})
+            log_filename = ".logging/fuzz_path_independence.log"
+            rollbar.report_exc_info(sys.exc_info(), "error", payload=_filename_to_payload(log_filename))
 
         try:
             chain_config = LocalChain.Config(db_port=5436, chain_port=10003)
             fuzz_profit_check(chain_config)
         except AssertionError:
-            rollbar.report_exc_info(sys.exc_info(), "error", payload={})
+            log_filename = ".logging/fuzz_profit_check.log"
+            rollbar.report_exc_info(sys.exc_info(), "error", payload=_filename_to_payload(log_filename))
 
 
 if __name__ == "__main__":
