7759b2dabb1a07e9083696a3e7eb134475fdcd63
Mihai <mcosma@gmail.com>
2023-12-09 20:41:37 -0500

    implement smarter wait in retry utils

--- lib/ethpy/ethpy/base/retry_utils.py
+++ lib/ethpy/ethpy/base/retry_utils.py
@@ -1,24 +1,32 @@
-"""Wrapper functions for retrying"""
+"""Wrapper functions for retrying."""
 from __future__ import annotations
 
 import asyncio
 import inspect
 import logging
 import time
-from typing import Awaitable, Callable, ParamSpec, TypeVar
+from typing import Any, Awaitable, Callable, TypeVar
 
-P = ParamSpec("P")
+from web3._utils.threads import Timeout
+
 R = TypeVar("R")
+
+# we need lots of arguments for the exponential backoff
+# pylint: disable=too-many-arguments
+# ruff: noqa: PLR0913
 
 
 async def async_retry_call(
     retry_count: int,
     retry_exception_check: Callable[[Exception], bool] | None,
-    func: Callable[P, Awaitable[R]],
-    *args: P.args,
-    **kwargs: P.kwargs,
+    func: Callable[..., Awaitable[R]],
+    *args: Any,
+    timeout: float = 2,
+    start_latency: float = 0.01,
+    backoff_multiplier: float = 2,
+    **kwargs: Any,
 ) -> R:
-    """Helper function to retry an async function call
+    """Retry an async function call.
 
     Arguments
     ---------
@@ -27,11 +35,17 @@ async def async_retry_call(
     retry_exception_check: Callable[[type[Exception]], bool] | None
         A function that takes as an argument an exception and returns True if we want to retry on that exception
         If None, will retry for all exceptions
-    func: Callable[P, Awaitable[R]]
+    func: Callable[..., Awaitable[R]]
         The function to call.
-    *args: P.args
+    *args: Any
         The positional arguments to call func with
-    **kwargs: P.kwargs
+    timeout: float
+        The number of seconds to wait before timing out
+    start_latency: float
+        The starting amount of time in seconds to wait between polls
+    backoff_multiplier: float
+        The backoff factor for the exponential backoff
+    **kwargs: Any
         The keyword arguments to call the func with
 
     Returns
@@ -44,7 +58,8 @@ async def async_retry_call(
     exception = None
     for attempt_number in range(retry_count):
         try:
-            out = await func(*args, **kwargs)
+            with Timeout(timeout) as _timeout:
+                out = await func(*args, **kwargs)
             return out
         # Catching general exception but throwing if fails
         except Exception as exc:  # pylint: disable=broad-exception-caught
@@ -55,15 +70,14 @@ async def async_retry_call(
             caller = inspect.stack()[1][3]
             logging.warning(
                 "Retry attempt %s out of %s: Function %s called from %s failed with %s",
-                attempt_number,
+                attempt_number + 1,
                 retry_count,
                 func,
                 caller,
                 repr(exc),
             )
             exception = exc
-            # TODO implement smarter wait here
-            await asyncio.sleep(0.1)
+            await asyncio.sleep(start_latency * backoff_multiplier**attempt_number)
     assert exception is not None
     raise exception
 
@@ -71,11 +85,14 @@ async def async_retry_call(
 def retry_call(
     retry_count: int,
     retry_exception_check: Callable[[Exception], bool] | None,
-    func: Callable[P, R],
-    *args: P.args,
-    **kwargs: P.kwargs,
+    func: Callable[..., R],
+    *args: Any,
+    timeout: float = 2,
+    start_latency: float = 0.01,
+    backoff_multiplier: float = 2,
+    **kwargs: Any,
 ) -> R:
-    """Helper function to retry a function call
+    """Retry a function call.
 
     Arguments
     ---------
@@ -84,11 +101,17 @@ def retry_call(
     retry_exception_check: Callable[[type[Exception]], bool] | None
         A function that takes as an argument an exception and returns True if we want to retry on that exception
         If None, will retry for all exceptions
-    func: Callable[P, Awaitable[R]]
+    func: Callable[..., Awaitable[R]]
         The function to call
-    *args: P.args
+    *args: Any
         The positional arguments to call func with
-    **kwargs: P.kwargs
+    timeout: float
+        The number of seconds to wait before timing out
+    start_latency: float
+        The starting amount of time in seconds to wait between polls
+    backoff_multiplier: float
+        The backoff factor for the exponential backoff
+    **kwargs: Any
         The keyword arguments to call the func with
 
     Returns
@@ -96,12 +119,15 @@ def retry_call(
     R
         Returns the value of the called function
     """
+    # we need lots of arguments for exponential backoff
+    # pylint: disable=too-many-arguments
     # TODO can't make a default for `retry_exception_check` due to *args and **kwargs,
     # so we need to explicitly pass in this parameter
     exception = None
     for attempt_number in range(retry_count):
         try:
-            out = func(*args, **kwargs)
+            with Timeout(timeout) as _timeout:
+                out = func(*args, **kwargs)
             return out
         # Catching general exception but throwing if fails
         except Exception as exc:  # pylint: disable=broad-exception-caught
@@ -112,14 +138,13 @@ def retry_call(
             caller = inspect.stack()[1][3]
             logging.warning(
                 "Retry attempt %s out of %s: Function %s called from %s failed with %s",
-                attempt_number,
+                attempt_number + 1,
                 retry_count,
                 func,
                 caller,
                 repr(exc),
             )
             exception = exc
-            # TODO implement smarter wait here
-            time.sleep(0.1)
+            time.sleep(start_latency * backoff_multiplier**attempt_number)
     assert exception is not None
     raise exception
