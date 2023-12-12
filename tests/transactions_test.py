2ea3ebc957c526d6c5031c7d2ccce38bd818074f
Dylan <dpaiton@gmail.com>
2023-12-11 17:44:59 -0800

    start new test; switch to logging

--- tests/transactions_test.py
+++ tests/transactions_test.py
@@ -1,22 +1,41 @@
 """Test executing transactions."""
-# import os
+import logging
 import time
 
 import pytest
+from ethpy.base import retry_call
 from ethpy.base.errors.errors import ContractCallException
+from ethpy.hyperdrive.interface import HyperdriveInterface
 from fixedpointmath import FixedPoint
 
-from agent0.hyperdrive.interactive import InteractiveHyperdrive, LocalChain
+from agent0.hyperdrive.interactive import InteractiveHyperdrive
 
 # ruff: noqa: PLR2004 (comparison against magic values (literals like numbers))
 
 
 @pytest.mark.anvil
-def test_retry_call(chain: LocalChain, caplog: pytest.LogCaptureFixture):
-    """Retry a bogus call."""
+def test_retry_call_success(interface: HyperdriveInterface, caplog: pytest.LogCaptureFixture):
+    """Verify that a bogus call produces the correct number of retries."""
+    retry_count = 5
+    # getting the block should always work
+    successful_return = retry_call(retry_count, None, interface.web3.eth.get_block, "latest", full_transactions=True)
+    retries = [r for r in caplog.records if r.message.startswith("Retry")]
+    assert len(retries) == 0
+
+
+@pytest.mark.anvil
+def test_retry_call_success(interface: HyperdriveInterface, caplog: pytest.LogCaptureFixture):
+    """Verify that a bogus call produces the correct number of retries."""
+    for read_retry_count in [0, 1, 4]:
+        # getting the block should always work
+        successful_return = retry_call(
+            read_retry_count, None, interface.web3.eth.get_block, "latest", full_transactions=True
+        )
+        retries = [r for r in caplog.records if r.message.startswith("Retry")]
+        assert len(retries) == 0
+
     interactive_hyperdrive = InteractiveHyperdrive(chain)
     larry = interactive_hyperdrive.init_agent(base=FixedPoint(100), name="larry")
-    # os.environ["_PYTEST_RAISE"] = "0"  # let pytest raise exceptions
     # This should fail
     start_time = time.time()
     with pytest.raises(ContractCallException):
@@ -25,13 +44,13 @@ def test_retry_call(chain: LocalChain, caplog: pytest.LogCaptureFixture):
     # Check the captured log messages
     retries = [r for r in caplog.records if r.message.startswith("Retry")]
     assert len(retries) == 5
-    print("")
+    logging.info("")
     for retry in retries:
-        print(f"{retry.created}: {retry.message}")
+        logging.info("%s: %s", retry.created, retry.message)
     attempt_timestamps = [r.created for r in retries]
     diffs = [attempt_timestamps[i + 1] - attempt_timestamps[i] for i in range(len(attempt_timestamps) - 1)]
     diffs = [round(d, 3) for d in diffs]
-    print(f"Attempt timestamps: {', '.join([str(ts) for ts in attempt_timestamps])}")
-    print(f"diffs: {', '.join([str(d) for d in diffs])}")
-    print(f"Total wait time: {sum(diffs):.2f} seconds.")
-    print(f"Time to fail: {end_time - start_time:.2f} seconds.")
+    logging.info("Attempt timestamps: %s", ", ".join([str(ts) for ts in attempt_timestamps]))
+    logging.info("diffs: %s", ", ".join([str(d) for d in diffs]))
+    logging.info("Total wait time: %2f seconds.", sum(diffs))
+    logging.info("Time to fail: %2f seconds.", end_time - start_time)
