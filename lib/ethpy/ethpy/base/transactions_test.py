dfaff5c3d7836de5771f71ad16c8e031913c5a1e
Mihai <mcosma@gmail.com>
2023-12-12 13:56:14 -0500

    add failure test

--- lib/ethpy/ethpy/base/transactions_test.py
+++ lib/ethpy/ethpy/base/transactions_test.py
@@ -1,15 +1,14 @@
 """Test executing transactions."""
-from ethpy.base.transactions import smart_contract_transact
-
 import pytest
 from fixedpointmath import FixedPoint
 
 from agent0.hyperdrive.interactive import InteractiveHyperdrive, LocalChain
+from ethpy.base.transactions import smart_contract_transact
 
 
 @pytest.mark.anvil
-def test_smart_contract_transact(chain: LocalChain):
-    """Verify that a bogus call produces the correct number of retries."""
+def test_smart_contract_transact_success(chain: LocalChain):
+    """Transact with a known correct function."""
     interactive_hyperdrive = InteractiveHyperdrive(chain)
     alice = interactive_hyperdrive.init_agent(base=FixedPoint(100), name="alice")
     _deployed_hyperdrive = interactive_hyperdrive._deployed_hyperdrive  # pylint: disable=protected-access
@@ -18,6 +17,22 @@ def test_smart_contract_transact(chain: LocalChain):
         _deployed_hyperdrive.base_token_contract,
         _deployed_hyperdrive.deploy_account,
         "mint(address,uint256)",
+        alice.agent.checksum_address,
+        FixedPoint(100).scaled_value,
+    )
+
+
+@pytest.mark.anvil
+def test_smart_contract_transact_failure(chain: LocalChain):
+    """Transact with a known incorrect function name."""
+    interactive_hyperdrive = InteractiveHyperdrive(chain)
+    alice = interactive_hyperdrive.init_agent(base=FixedPoint(100), name="alice")
+    _deployed_hyperdrive = interactive_hyperdrive._deployed_hyperdrive  # pylint: disable=protected-access
+    smart_contract_transact(
+        chain._web3,  # pylint: disable=protected-access
+        _deployed_hyperdrive.base_token_contract,
+        _deployed_hyperdrive.deploy_account,
+        "mintx(address,uint256)",
         alice.agent.checksum_address,
         FixedPoint(100).scaled_value,
     )
