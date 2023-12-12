682d823d4eccaea56d882de2d941bbff1464732a
Mihai <mcosma@gmail.com>
2023-12-12 18:38:34 -0500

    simplify lp pnl

--- lib/chainsync/chainsync/analysis/calc_pnl.py
+++ lib/chainsync/chainsync/analysis/calc_pnl.py
@@ -8,11 +8,14 @@ import pandas as pd
 from eth_typing import ChecksumAddress, HexAddress, HexStr
 from ethpy.base import smart_contract_preview_transaction
 from ethpy.hyperdrive import BASE_TOKEN_SYMBOL
+from ethpy.hyperdrive.interface.interface import HyperdriveInterface
 from fixedpointmath import FixedPoint
 from web3.contract.contract import Contract
 
 
-def calc_single_closeout(position: pd.Series, contract: Contract, pool_info: pd.DataFrame, min_output: int) -> Decimal:
+def calc_single_closeout(
+    position: pd.Series, contract: Contract, min_output: int, lp_share_price: FixedPoint
+) -> Decimal:
     """Calculate the closeout pnl for a single position.
 
     Arguments
@@ -21,10 +24,10 @@ def calc_single_closeout(position: pd.Series, contract: Contract, pool_info: pd.
         The position to calculate the closeout pnl for (one row in current_wallet)
     contract: Contract
         The contract object
-    pool_info: pd.DataFrame
-        The pool info
     min_output: int
         The minimum output to be accepted, as part of slippage tolerance
+    lp_share_price: FixedPoint
+        The price of an LP share in units of the base token
 
     Returns
     -------
@@ -91,46 +94,9 @@ def calc_single_closeout(position: pd.Series, contract: Contract, pool_info: pd.
         except Exception as exception:  # pylint: disable=broad-except
             logging.warning("Exception caught, ignoring: %s", exception)
 
-    elif tokentype == "LP":
-        fn_args = (
-            amount,
-            min_output,
-            (  # IHyperdrive.Options
-                address,  # destination
-                True,  # asBase
-                bytes(0),  # extraData
-            ),
-        )
-        # If this fails, keep as nan and continue iterating
-        try:
-            preview_result = smart_contract_preview_transaction(
-                contract, sender, "removeLiquidity", *fn_args, block_number=position["block_number"]
-            )
-            out_pnl = Decimal(
-                preview_result["baseProceeds"]
-                # We assume all withdrawal shares are redeemable
-                + preview_result["withdrawalShares"] * pool_info["lp_share_price"].values[-1]
-            ) / Decimal(1e18)
-        except Exception as exception:  # pylint: disable=broad-except
-            logging.warning("Exception caught, ignoring: %s", exception)
-
-    elif tokentype == "WITHDRAWAL_SHARE":
-        fn_args = (
-            amount,
-            min_output,
-            (  # IHyperdrive.Options
-                address,  # destination
-                True,  # asBase
-                bytes(0),  # extraData
-            ),
-        )
-        try:
-            # For PNL, we assume all withdrawal shares are redeemable
-            # even if there are no withdrawal shares available to withdraw
-            # Hence, we don't use preview transaction here
-            out_pnl = Decimal(amount * pool_info["lp_share_price"].values[-1]) / Decimal(1e18)
-        except Exception as exception:  # pylint: disable=broad-except
-            logging.warning("Exception caught, ignoring: %s", exception)
+    elif tokentype in ["LP", "WITHDRAWAL_SHARE"]:
+        out_pnl = amount * lp_share_price
+        out_pnl = Decimal(out_pnl.scaled_value) / Decimal(1e18)
     else:
         # Should never get here
         raise ValueError(f"Unexpected token type: {tokentype}")
@@ -138,7 +104,9 @@ def calc_single_closeout(position: pd.Series, contract: Contract, pool_info: pd.
 
 
 def calc_closeout_pnl(
-    current_wallet: pd.DataFrame, pool_info: pd.DataFrame, hyperdrive_contract: Contract
+    current_wallet: pd.DataFrame,
+    hyperdrive_contract: Contract,
+    hyperdrive_interface: HyperdriveInterface,
 ) -> pd.DataFrame:
     """Calculate closeout value of agent positions.
 
@@ -146,23 +114,20 @@ def calc_closeout_pnl(
     ---------
     current_wallet: pd.DataFrame
         A dataframe resulting from `get_current_wallet` that describes the current wallet position.
-    pool_info: pd.DataFrame
-        The pool info object.
     hyperdrive_contract: Contract
         The hyperdrive contract object.
+    hyperdrive_interface: HyperdriveInterface
+        The Hyperdrive interface object.
 
     Returns
     -------
     Decimal
         The closeout pnl
     """
-    # Define a function to handle the calculation for each group
-    out_pnl = current_wallet.apply(
+    return current_wallet.apply(
         calc_single_closeout,  # type: ignore
         contract=hyperdrive_contract,
-        pool_info=pool_info,
         min_output=0,
+        lp_share_price=hyperdrive_interface.current_pool_state.pool_info.lp_share_price,
         axis=1,
     )
-
-    return out_pnl
