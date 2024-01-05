9fdc48a31275f0b748c15e89911c1eb858a601a4
Dylan <dpaiton@gmail.com>
2024-01-05 15:06:48 -0800

    resolve feedback

--- lib/agent0/agent0/hyperdrive/interactive/interactive_hyperdrive_test.py
+++ lib/agent0/agent0/hyperdrive/interactive/interactive_hyperdrive_test.py
@@ -126,11 +126,10 @@ def test_funding_and_trades(chain: LocalChain):
     assert (FixedPoint(343) - chain_eth_balance) < FixedPoint("0.0001")
 
     # Test trades
-    # Add liquidity - approx 12k total now
+    # Add liquidity to 112_111 total
     add_liquidity_event = hyperdrive_agent0.add_liquidity(base=FixedPoint(111_111))
     assert add_liquidity_event.base_amount == FixedPoint(111_111)
     assert hyperdrive_agent0.wallet.lp_tokens == add_liquidity_event.lp_amount
-    assert interactive_hyperdrive.hyperdrive_interface.current_pool_state.pool_info.share_reserves > 1_000
     _ensure_db_wallet_matches_agent_wallet(interactive_hyperdrive, hyperdrive_agent0.wallet)
 
     # Open long
