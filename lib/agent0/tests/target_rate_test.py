67de808ccfad9074450770dbb268b23a7dd399a5
Mihai <mcosma@gmail.com>
2024-01-10 17:07:35 -0500

    add test already at target

--- lib/agent0/tests/target_rate_test.py
+++ lib/agent0/tests/target_rate_test.py
@@ -77,3 +77,30 @@ def test_open_short(chain: LocalChain, trade_amount: float):
     abs_diff = abs(fixed_rate - variable_rate)
     logging.warning("difference is %s", abs_diff)
     assert abs_diff < PRECISION
+
+@pytest.mark.anvil
+def test_already_at_target(chain: LocalChain):
+    """Hit target rate when opening a short to hit the target rate."""
+    interactive_config = InteractiveHyperdrive.Config()
+    interactive_hyperdrive = InteractiveHyperdrive(chain, interactive_config)
+
+    # report starting fixed rate
+    logging.warning("starting fixed rate is %s", interactive_hyperdrive.hyperdrive_interface.calc_fixed_rate())
+
+    # arbitrage it back
+    andy_base = FixedPoint(1e9)
+    andy_config = Zoo.lp_and_arb.Config(
+        lp_portion=FixedPoint(0),
+        minimum_trade_amount=interactive_hyperdrive.hyperdrive_interface.pool_config.minimum_transaction_amount,
+    )
+    andy = interactive_hyperdrive.init_agent(
+        base=andy_base, name="andy", policy=Zoo.lp_and_arb, policy_config=andy_config
+    )
+    andy.execute_policy_action()
+    fixed_rate = interactive_hyperdrive.hyperdrive_interface.calc_fixed_rate()
+    variable_rate = interactive_hyperdrive.hyperdrive_interface.current_pool_state.variable_rate
+    logging.warning("ending fixed rate is %s", fixed_rate)
+    logging.warning("variable rate is %s", variable_rate)
+    abs_diff = abs(fixed_rate - variable_rate)
+    logging.warning("difference is %s", abs_diff)
+    assert abs_diff < PRECISION
