86c71be666c8c9e8aed413eefeb3947e26cb9a71
Dylan <dpaiton@gmail.com>
2024-01-10 15:52:52 -0800

    rename file

--- lib/agent0/agent0/interactive_fuzz/helpers/__init__.py
+++ lib/agent0/agent0/interactive_fuzz/helpers/__init__.py
@@ -1,6 +1,6 @@
 """Shared functions for interactive fuzz testing."""
 
 from .close_random_trades import close_random_trades
+from .execute_random_trades import execute_random_trades
 from .fuzz_assertion_exception import FuzzAssertionException, fp_isclose
-from .open_random_trades import execute_random_trades
 from .setup_fuzz import setup_fuzz
