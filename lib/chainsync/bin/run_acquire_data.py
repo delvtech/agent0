f3657a72b1eeea86ef14fa4edbd2496e5a444f88
Sheng Lundquist <sheng@delv.tech>
2024-01-18 15:02:12 -0800

    Adjusting start block to be the block that hyperdrive was deployed in the devnet image, due to anvil update having load state load the dumped block number

--- lib/chainsync/bin/run_acquire_data.py
+++ lib/chainsync/bin/run_acquire_data.py
@@ -6,4 +6,9 @@ from hyperlogs import setup_logging
 
 if __name__ == "__main__":
     setup_logging(".logging/acquire_data.log", log_stdout=True)
-    acquire_data()
+    acquire_data(
+        # This is the start block needed based on the devnet image, which corresponds
+        # to the block that the contract was deployed.
+        # TODO ideally would gather this from the deployer
+        start_block=30,
+    )
