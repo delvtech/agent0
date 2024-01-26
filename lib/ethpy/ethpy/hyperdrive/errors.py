23beb7f8a49a7b467a5c4a79754ce6fe3e03fd8f
Sheng Lundquist <sheng@delv.tech>
2024-01-26 12:52:56 -0800

    Updating hyperdrive errors and their selectors

--- lib/ethpy/ethpy/hyperdrive/errors.py
+++ lib/ethpy/ethpy/hyperdrive/errors.py
@@ -25,77 +25,45 @@ def lookup_hyperdrive_error_selector(error_selector: str) -> str:
 class HyperdriveErrors(NamedTuple):
     """A collection of error selectors by the name."""
 
-    # TODO need to update these, ideally use pypechain generated errors
+    # TODO Ideally use pypechain generated errors
+    # Gathered from IHyperdrive.json abi
 
-    ##################
-    ### Hyperdrive ###
-    ##################
-    BaseBufferExceedsShareReserves: str = "0x18846de9"
+    BatchInputLengthMismatch: str = "0xba430d38"
+    BelowMinimumContribution: str = "0xabed41c4"
+    ExpInvalidExponent: str = "0x73a2d6b1"
+    ExpiredDeadline: str = "0xf87d9271"
+    InsufficientLiquidity: str = "0x780daf16"
     InvalidApr: str = "0x76c22a22"
     InvalidBaseToken: str = "0x0e442a4a"
+    InvalidCheckpointDuration: str = "0x5428734d"
     InvalidCheckpointTime: str = "0xecd29e81"
-    InvalidInitialVaultSharePrice: str = "0x094b19ad"
-    InvalidMaturityTime: str = "0x987dadd3"
-    InvalidPositionDuration: str = "0x4a7fff9e"
-    InvalidFeeAmounts: str = "0x45ee5986"
-    NegativeInterest: str = "0x512095c7"
-    OutputLimit: str = "0xc9726517"
-    Paused: str = "0x9e87fac8"
-    PoolAlreadyInitialized: str = "0x7983c051"
-    TransferFailed: str = "0x90b8ec18"
-    UnexpectedAssetId: str = "0xe9bf5433"
-    UnsupportedToken: str = "0x6a172882"
-    ZeroAmount: str = "0x1f2a2005"
-    ZeroLpTotalSupply: str = "0x252c3a3e"
-    ZeroLpTotalSupply: str = "0x252c3a3e"
-
-    ############
-    ### TWAP ###
-    ############
-    QueryOutOfRange: str = "0xa89817b0"
-
-    ####################
-    ### DataProvider ###
-    ####################
-    UnexpectedSuccess: str = "0x8bb0a34b"
-
-    ###############
-    ### Factory ###
-    ###############
-    Unauthorized: str = "0x82b42900"
-    InvalidContribution: str = "0x652122d9"
-    InvalidToken: str = "0xc1ab6dc1"
-
-    ######################
-    ### ERC20Forwarder ###
-    ######################
-    BatchInputLengthMismatch: str = "0xba430d38"
-    ExpiredDeadline: str = "0xf87d9271"
-    InvalidSignature: str = "0x8baa579f"
     InvalidERC20Bridge: str = "0x2aab8bd3"
-    RestrictedZeroAddress: str = "0xf0dd15fd"
-
-    ###################
-    ### BondWrapper ###
-    ###################
-    AlreadyClosed: str = "0x9acb7e52"
-    BondMatured: str = "0x3f8e46bc"
-    BondNotMatured: str = "0x915eceb1"
-    InsufficientPrice: str = "0xd5481703"
-
-    ###############
-    ### AssetId ###
-    ###############
+    InvalidFeeAmounts: str = "0x45ee5986"
+    InvalidFeeDestination: str = "0x2b44eccc"
+    InvalidInitialVaultSharePrice: str = "0x094b19ad"
+    InvalidMinimumShareReserves: str = "0x49db44f5"
+    InvalidPositionDuration: str = "0x4a7fff9e"
+    InvalidShareReserves: str = "0xb0bfcdbe"
+    InvalidSignature: str = "0x8baa579f"
     InvalidTimestamp: str = "0xb7d09497"
-
-    ######################
-    ### FixedPointMath ###
-    ######################
-    FixedPointMath_AddOverflow: str = "0x2d59cfbd"
-    FixedPointMath_SubOverflow: str = "0x35ba1440"
-    FixedPointMath_InvalidExponent: str = "0xdf92cc9d"
-    FixedPointMath_NegativeOrZeroInput: str = "0xac5f1b8e"
-    FixedPointMath_NegativeInput: str = "0x2c7949f5"
+    LnInvalidInput: str = "0xe61b4975"
+    MinimumSharePrice: str = "0x42af972b"
+    MinimumTransactionAmount: str = "0x423bbb46"
+    NegativePresentValue: str = "0xaeeb825d"
+    NotPayable: str = "0x1574f9f3"
+    OutputLimit: str = "0xc9726517"
+    PoolAlreadyInitialized: str = "0x7983c051"
+    PoolIsPaused: str = "0x21081abf"
+    RestrictedZeroAddress: str = "0xf0dd15fd"
+    ReturnData: str = "0xdcc81126"
+    SweepFailed: str = "0x9eec2ff8"
+    TransferFailed: str = "0x90b8ec18"
+    Unauthorized: str = "0x82b42900"
+    UnexpectedSuccess: str = "0x8bb0a34b"
+    UnsafeCastToUint112: str = "0x10d62a2e"
+    UnsafeCastToUint128: str = "0x1e15f2a2"
+    UnsafeCastToInt128: str = "0xa5353be5"
+    UnsupportedToken: str = "0x6a172882"
 
 
 _hyperdrive_errors = HyperdriveErrors()
