0053b6dfa30b3c736cc396f4887d40ec9cbee7f1
Dylan <dpaiton@gmail.com>
2024-02-02 15:09:19 -0800

    move interface fixtures back to ethpy

--- lib/agent0/agent0/test_fixtures/__init__.py
+++ lib/agent0/agent0/test_fixtures/__init__.py
@@ -1,4 +1,3 @@
 """Test fixtures for agent0."""
 
 from .chain import chain
-from .interface import hyperdrive_read_interface, hyperdrive_read_write_interface
--- lib/ethpy/ethpy/test_fixtures/__init__.py
+++ lib/ethpy/ethpy/test_fixtures/__init__.py
@@ -1,4 +1,5 @@
 """Test fixtures for ethpy"""
 
 from .deployed_pool import DeployedHyperdrivePool, init_local_hyperdrive_pool, local_hyperdrive_pool
+from .interface import hyperdrive_read_interface, hyperdrive_read_write_interface
 from .local_chain import local_chain
