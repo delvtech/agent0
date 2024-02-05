80007fa7ab69416186f6f778dc59481ca7a0eb29
Sheng Lundquist <sheng@delv.tech>
2024-02-05 12:50:48 -0800

    Fixing gym registeration

--- lib/hypergym/hypergym/__init__.py
+++ lib/hypergym/hypergym/__init__.py
@@ -1,8 +1,10 @@
 from gymnasium.envs.registration import register
 
+from .gym_environments.simple_hyperdrive_env import SimpleHyperdriveEnv
+
 # Register simple hyperdrive env to gym
 register(
-    id="hypergym/gym_environments/simple_hyperdrive_env",
-    entry_point="hypergym.gym_environments/simple_hyperdrive_env:SimpleHyperdriveEnv",
+    id="hypergym/simple_hyperdrive_env",
+    entry_point="hypergym.gym_environments.simple_hyperdrive_env:SimpleHyperdriveEnv",
     max_episode_steps=300,
 )
