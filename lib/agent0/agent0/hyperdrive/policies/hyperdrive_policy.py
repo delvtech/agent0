0a60077d117fd7651a301640312cfd038b049acd
Sheng Lundquist <sheng@delv.tech>
2024-02-13 15:17:32 -0800

    Adding post action stage to bot runner (#1320)

--- lib/agent0/agent0/hyperdrive/policies/hyperdrive_policy.py
+++ lib/agent0/agent0/hyperdrive/policies/hyperdrive_policy.py
@@ -4,7 +4,7 @@ from ethpy.hyperdrive import HyperdriveReadInterface
 
 from agent0.base import Trade
 from agent0.base.policies import BasePolicy
-from agent0.hyperdrive import HyperdriveMarketAction, HyperdriveWallet
+from agent0.hyperdrive import HyperdriveMarketAction, HyperdriveWallet, TradeResult
 
 
 class HyperdriveBasePolicy(BasePolicy[HyperdriveReadInterface, HyperdriveWallet]):
@@ -29,3 +29,18 @@ class HyperdriveBasePolicy(BasePolicy[HyperdriveReadInterface, HyperdriveWallet]
             and the second element defines if the agent is done trading.
         """
         raise NotImplementedError
+
+    def post_action(self, interface: HyperdriveReadInterface, trade_results: list[TradeResult]) -> None:
+        """Function that gets called after actions have been executed. This allows the policy
+        to e.g., do additional bookkeeping based on the results of the executed actions.
+        Arguments
+        ---------
+        interface: MarketInterface
+            The trading market interface.
+        trade_results: list[HyperdriveTradeResult]
+            A list of HyperdriveTradeResult objects, one for each trade made by the agent.
+            The order of the list matches the original order of `agent.action`.
+            HyperdriveTradeResult contains any information about the trade,
+            as well as any errors that the trade resulted in.
+        """
+        # Default post action is noop
