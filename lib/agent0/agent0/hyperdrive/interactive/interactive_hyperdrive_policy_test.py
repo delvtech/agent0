8fe278bfcc2c6da2642404bec1002e1eb57d1a5a
Sheng Lundquist <sheng@delv.tech>
2024-02-16 11:13:31 -0800

    Adding test for policy state

--- lib/agent0/agent0/hyperdrive/interactive/interactive_hyperdrive_policy_test.py
+++ lib/agent0/agent0/hyperdrive/interactive/interactive_hyperdrive_policy_test.py
@@ -1,11 +1,15 @@
 """Tests various interactive hyperdrive policies."""
 
 import pytest
+from ethpy.hyperdrive import HyperdriveReadInterface
 from fixedpointmath import FixedPoint
 
+from agent0.base import Trade
+from agent0.hyperdrive import HyperdriveMarketAction, HyperdriveWallet
 from agent0.hyperdrive.interactive.chain import LocalChain
 from agent0.hyperdrive.interactive.interactive_hyperdrive import InteractiveHyperdrive
-from agent0.hyperdrive.policies import PolicyZoo
+from agent0.hyperdrive.interactive.interactive_hyperdrive_policy import InteractiveHyperdrivePolicy
+from agent0.hyperdrive.policies import HyperdriveBasePolicy, PolicyZoo
 
 
 @pytest.mark.anvil
@@ -35,3 +39,66 @@ def test_policy_config_none_rng(chain: LocalChain):
         policy_config=agent_policy,
     )
     assert alice.agent.policy.rng is not None
+
+
+@pytest.mark.anvil
+def test_snapshot_policy_state(chain: LocalChain):
+    """Tests proper saving/loading of policy state during snapshotting."""
+
+    # Define dummy class for deep state copy
+    class _InnerState:
+        inner_var: int
+
+        def __init__(self):
+            self.inner_var = 1
+            self.inner_list = [1]
+
+    class _SubPolicy(HyperdriveBasePolicy):
+        inner_state: _InnerState
+        outer_var: int
+
+        def __init__(self, policy_config: HyperdriveBasePolicy.Config):
+            self.inner_state = _InnerState()
+            self.outer_var = 2
+            self.outer_list = [2]
+            super().__init__(policy_config)
+
+        def action(
+            self, interface: HyperdriveReadInterface, wallet: HyperdriveWallet
+        ) -> tuple[list[Trade[HyperdriveMarketAction]], bool]:
+            # pylint: disable=missing-return-doc
+            return [], False
+
+    # Initialize agent with sub policy
+    interactive_hyperdrive = InteractiveHyperdrive(chain)
+    agent = interactive_hyperdrive.init_agent(policy=_SubPolicy)
+    # Snapshot state
+    chain.save_snapshot()
+
+    # Sanity check and type narrowing
+    assert isinstance(agent.agent.policy, InteractiveHyperdrivePolicy)
+    assert agent.agent.policy.sub_policy is not None
+    assert isinstance(agent.agent.policy.sub_policy, _SubPolicy)
+    assert agent.agent.policy.sub_policy.outer_var == 2
+    assert agent.agent.policy.sub_policy.outer_list == [2]
+    assert agent.agent.policy.sub_policy.inner_state.inner_var == 1
+    assert agent.agent.policy.sub_policy.inner_state.inner_list == [1]
+
+    # Change inner state variables
+    agent.agent.policy.sub_policy.outer_var = 22
+    agent.agent.policy.sub_policy.outer_list.append(222)
+    agent.agent.policy.sub_policy.inner_state.inner_var = 11
+    agent.agent.policy.sub_policy.inner_state.inner_list.append(111)
+    assert agent.agent.policy.sub_policy.outer_var == 22
+    assert agent.agent.policy.sub_policy.outer_list == [2, 222]
+    assert agent.agent.policy.sub_policy.inner_state.inner_var == 11
+    assert agent.agent.policy.sub_policy.inner_state.inner_list == [1, 111]
+
+    # Load snapshot
+    chain.load_snapshot()
+
+    # Ensure inner states were restored
+    assert agent.agent.policy.sub_policy.outer_var == 2
+    assert agent.agent.policy.sub_policy.outer_list == [1]
+    assert agent.agent.policy.sub_policy.inner_state.inner_var == 1
+    assert agent.agent.policy.sub_policy.inner_state.inner_list == [2]
