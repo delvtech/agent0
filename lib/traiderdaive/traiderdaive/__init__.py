ce98eb48fbce056adda98767153e6f14b3c3dbd5
Sheng Lundquist <sheng@delv.tech>
2024-02-20 10:20:02 -0800

    Adding into init

--- lib/traiderdaive/traiderdaive/__init__.py
+++ lib/traiderdaive/traiderdaive/__init__.py
@@ -2,11 +2,18 @@
 
 from gymnasium.envs.registration import register
 
+from .gym_environments.full_hyperdrive_env import FullHyperdriveEnv
 from .gym_environments.simple_hyperdrive_env import SimpleHyperdriveEnv
 
 # Register simple hyperdrive env to gym
 register(
     id="traiderdaive/simple_hyperdrive_env",
     entry_point="traiderdaive.gym_environments.simple_hyperdrive_env:SimpleHyperdriveEnv",
-    max_episode_steps=300,
+    max_episode_steps=1000,
+)
+
+register(
+    id="traiderdaive/full_hyperdrive_env",
+    entry_point="traiderdaive.gym_environments.simple_hyperdrive_env:FullHyperdriveEnv",
+    max_episode_steps=1000,
 )
