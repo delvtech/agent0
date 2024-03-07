4be7c6c5d8719551f0675bc87cb20562120f00dd
Sheng Lundquist <sheng@delv.tech>
2024-03-06 17:09:16 -0800

    Updating dataclass docstrings for interactive hyperdrive

--- src/agent0/core/hyperdrive/interactive/event_types.py
+++ src/agent0/core/hyperdrive/interactive/event_types.py
@@ -8,237 +8,151 @@ from fixedpointmath import FixedPoint
 
 @dataclass
 class OpenLong:
-    """Dataclass mirroring OpenLong event in Hyperdrive.
-
-    Attributes
-    ----------
-    trader: ChecksumAddress
-        The address of the trader.
-    asset_id: int
-        The encoded asset id for this long.
-    maturity_time: int
-        The maturity time for the opened long
-    base_amount: FixedPoint
-        The amount of longs opened in units of base.
-    vault_share_amount: FixedPoint
-        The amount of longs opened in units of shares.
-    as_base: bool
-        If the input amount for the trade was in base or shares.
-    bond_amount: FixedPoint
-        The amount of longs opened in units of bonds.
-    """
+    """Dataclass mirroring OpenLong event in Hyperdrive."""
 
     trader: ChecksumAddress
+    """The address of the trader."""
     asset_id: int
+    """The encoded asset id for this long."""
     maturity_time: int
+    """The maturity time for the opened long."""
     base_amount: FixedPoint
+    """The amount of longs opened in units of base."""
     vault_share_amount: FixedPoint
+    """The amount of longs opened in units of shares."""
     as_base: bool
+    """If the input amount for the trade was in base or shares."""
     bond_amount: FixedPoint
+    """The amount of longs opened in units of bonds."""
 
 
 @dataclass
 class CloseLong:
-    """Dataclass mirroring CloseLong event in Hyperdrive.
-
-    Attributes
-    ----------
-    trader: ChecksumAddress
-        The address of the trader.
-    asset_id: int
-        The encoded asset id for this long.
-    maturity_time: int
-        The maturity time for the closed long
-    base_amount: FixedPoint
-        The amount of longs closed in units of base.
-    vault_share_amount: FixedPoint
-        The amount of longs closed in units of shares.
-    as_base: bool
-        If the input amount for the trade was in base or shares.
-    bond_amount: FixedPoint
-        The amount of longs closed in units of bonds.
-    """
+    """Dataclass mirroring CloseLong event in Hyperdrive."""
 
     trader: ChecksumAddress
+    """The address of the trader."""
     asset_id: int
+    """The encoded asset id for this long."""
     maturity_time: int
+    """The maturity time for the closed long."""
     base_amount: FixedPoint
+    """The amount of longs closed in units of base."""
     vault_share_amount: FixedPoint
+    """The amount of longs closed in units of shares."""
     as_base: bool
+    """If the input amount for the trade was in base or shares."""
     bond_amount: FixedPoint
+    """The amount of longs closed in units of bonds."""
 
 
 @dataclass
 class OpenShort:
-    """Dataclass mirroring OpenShort event in Hyperdrive.
-
-    Attributes
-    ----------
-    trader: ChecksumAddress
-        The address of the trader.
-    asset_id: int
-        The encoded asset id for this short.
-    maturity_time: int
-        The maturity time for the opened short
-    base_amount: FixedPoint
-        The amount spent from opening the short, in units of base.
-    vault_share_amount: FixedPoint
-        The amount spent from opening the short, in units of shares.
-    as_base: bool
-        If the input amount for the trade was in base or shares.
-    base_proceeds: FixedPoint
-        The amount of base in the underlying short when shorting bonds.
-    bond_amount: FixedPoint
-        The amount of shorts opened in units of bonds.
-    """
-
     # pylint: disable=too-many-instance-attributes
-
+    """Dataclass mirroring OpenShort event in Hyperdrive."""
     trader: ChecksumAddress
+    """The address of the trader."""
     asset_id: int
+    """The encoded asset id for this short."""
     maturity_time: int
+    """The maturity time for the opened short."""
     base_amount: FixedPoint
+    """The amount spent from opening the short, in units of base."""
     vault_share_amount: FixedPoint
+    """The amount spent from opening the short, in units of shares."""
     as_base: bool
+    """If the input amount for the trade was in base or shares."""
     base_proceeds: FixedPoint
+    """The amount of base in the underlying short when shorting bonds."""
     bond_amount: FixedPoint
+    """The amount of shorts opened in units of bonds."""
 
 
 @dataclass
 class CloseShort:
-    """Dataclass mirroring CloseShort event in Hyperdrive
-
-    Attributes
-    ----------
-    trader: ChecksumAddress
-        The address of the trader.
-    asset_id: int
-        The encoded asset id for this short.
-    maturity_time: int
-        The maturity time for the closed short
-    base_amount: FixedPoint
-        The amount retrieved from closing the short, in units of base.
-    vault_share_amount: FixedPoint
-        The amount retrieved from closing the short, in units of shares.
-    as_base: bool
-        If the input amount for the trade was in base or shares.
-    bond_amount: FixedPoint
-        The amount of shorts closed in units of bonds.
-    """
+    """Dataclass mirroring CloseShort event in Hyperdrive."""
 
     trader: ChecksumAddress
+    """The address of the trader."""
     asset_id: int
+    """The encoded asset id for this short."""
     maturity_time: int
+    """The maturity time for the closed short."""
     base_amount: FixedPoint
+    """The amount retrieved from closing the short, in units of base."""
     vault_share_amount: FixedPoint
+    """The amount retrieved from closing the short, in units of shares."""
     as_base: bool
+    """If the input amount for the trade was in base or shares."""
     bond_amount: FixedPoint
+    """The amount of shorts closed in units of bonds."""
 
 
 @dataclass
 class AddLiquidity:
-    """Dataclass mirroring AddLiquidity event in Hyperdrive
-
-    Attributes
-    ----------
-    provider: ChecksumAddress
-        The address of the lp provider.
-    lp_amount: FixedPoint
-        The amount of liquidity added in units of lp.
-    base_amount: FixedPoint
-        The amount of liquidity added, in units of base.
-    vault_share_amount: FixedPoint
-        The amount of liquidity added, in units of shares.
-    as_base: bool
-        If the input amount for the trade was in base or shares.
-    lp_share_price: FixedPoint
-        The lp share price for this trade.
-    """
+    """Dataclass mirroring AddLiquidity event in Hyperdrive."""
 
     provider: ChecksumAddress
+    """The address of the lp provider."""
     lp_amount: FixedPoint
+    """The amount of liquidity added in units of lp."""
     base_amount: FixedPoint
+    """The amount of liquidity added, in units of base."""
     vault_share_amount: FixedPoint
+    """The amount of liquidity added, in units of shares."""
     as_base: bool
+    """If the input amount for the trade was in base or shares."""
     lp_share_price: FixedPoint
+    """The lp share price for this trade."""
 
 
 @dataclass
 class RemoveLiquidity:
-    """Dataclass mirroring RemoveLiquidity event in Hyperdrive
-
-    Attributes
-    ----------
-    provider: ChecksumAddress
-        The address of the lp provider.
-    lp_amount: FixedPoint
-        The amount of liquidity removed in units of lp.
-    base_amount: FixedPoint
-        The amount of liquidity removed, in units of base.
-    vault_share_amount: FixedPoint
-        The amount of liquidity removed, in units of shares.
-    as_base: bool
-        If the input amount for the trade was in base or shares.
-    withdrawal_share_amount: FixedPoint
-        The amount of withdrawal shares received from removing liquidity.
-    lp_share_price: FixedPoint
-        The lp share price for this trade.
-    """
+    """Dataclass mirroring RemoveLiquidity event in Hyperdrive."""
 
     provider: ChecksumAddress
+    """The address of the lp provider."""
     lp_amount: FixedPoint
+    """The amount of liquidity removed in units of lp."""
     base_amount: FixedPoint
+    """The amount of liquidity removed, in units of base."""
     vault_share_amount: FixedPoint
+    """The amount of liquidity removed, in units of shares."""
     as_base: bool
+    """If the input amount for the trade was in base or shares."""
     withdrawal_share_amount: FixedPoint
+    """The amount of withdrawal shares received from removing liquidity."""
     lp_share_price: FixedPoint
+    """The lp share price for this trade."""
 
 
 @dataclass
 class RedeemWithdrawalShares:
-    """Dataclass mirroring RedeemWithdrawalShares event in Hyperdrive
-
-    Attributes
-    ----------
-    provider: ChecksumAddress
-        The address of the lp provider.
-    withdrawal_share_amount: FixedPoint
-        The amount of withdrawal shares redeemed, in units of withdrawal shares.
-    base_amount: FixedPoint
-        The amount of withdrawal shares redeemed, in units of base.
-    vault_share_amount: FixedPoint
-        The amount of withdrawal shares redeemed, in units of shares.
-    as_base: bool
-        If the input amount for the trade was in base or shares.
-    """
+    """Dataclass mirroring RedeemWithdrawalShares event in Hyperdrive."""
 
     provider: ChecksumAddress
+    """The address of the lp provider."""
     withdrawal_share_amount: FixedPoint
+    """The amount of withdrawal shares redeemed, in units of withdrawal shares."""
     base_amount: FixedPoint
+    """The amount of withdrawal shares redeemed, in units of base."""
     vault_share_amount: FixedPoint
+    """The amount of withdrawal shares redeemed, in units of shares."""
     as_base: bool
+    """If the input amount for the trade was in base or shares."""
 
 
 @dataclass
 class CreateCheckpoint:
-    """Dataclass mirroring CreateCheckpoint event in Hyperdrive
-
-    Attributes
-    ----------
-    checkpoint_time: int
-        The seconds epoch time for this checkpoint.
-    vault_share_price: FixedPoint
-        The share price at the checkpoint.
-    matured_shorts: FixedPoint
-        The amount of shorts that matured within this checkpoint
-    matured_longs: FixedPoint
-        The amount of longs that matured within this checkpoint
-    lp_share_price: FixedPoint
-        The lp share price at the checkpoint
-    """
+    """Dataclass mirroring CreateCheckpoint event in Hyperdrive."""
 
     checkpoint_time: int
+    """The seconds epoch time for this checkpoint."""
     vault_share_price: FixedPoint
+    """The share price at the checkpoint."""
     matured_shorts: FixedPoint
+    """The amount of shorts that matured within this checkpoint."""
     matured_longs: FixedPoint
+    """The amount of longs that matured within this checkpoint."""
     lp_share_price: FixedPoint
+    """The lp share price at the checkpoint."""
