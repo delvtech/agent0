8cf8cc632ae1a05f06fb1f0dede8c36fd0ff688c
Sheng Lundquist <sheng@delv.tech>
2024-03-07 11:25:35 -0800

    Updating entry point

--- src/agent0/traiderdaive/__init__.py
+++ src/agent0/traiderdaive/__init__.py
@@ -8,12 +8,12 @@ from .gym_environments.simple_hyperdrive_env import SimpleHyperdriveEnv
 # Register hyperdrive envs to gym
 register(
     id="traiderdaive/simple_hyperdrive_env",
-    entry_point="traiderdaive.gym_environments.simple_hyperdrive_env:SimpleHyperdriveEnv",
+    entry_point="agent0.traiderdaive.gym_environments.simple_hyperdrive_env:SimpleHyperdriveEnv",
     max_episode_steps=1000,
 )
 
 register(
     id="traiderdaive/full_hyperdrive_env",
-    entry_point="traiderdaive.gym_environments.full_hyperdrive_env:FullHyperdriveEnv",
+    entry_point="agent0.traiderdaive.gym_environments.full_hyperdrive_env:FullHyperdriveEnv",
     max_episode_steps=1000,
 )
