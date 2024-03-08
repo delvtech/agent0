4788c94adc5c8dd60ac758b7c183be8d74e585b0
Dylan <dpaiton@gmail.com>
2024-03-08 09:44:17 +0900

    update to new import strategy

--- src/agent0/core/hyperdrive/policies/simple_lp.py
+++ src/agent0/core/hyperdrive/policies/simple_lp.py
@@ -7,16 +7,15 @@ from typing import TYPE_CHECKING
 
 from fixedpointmath import FixedPoint, FixedPointMath
 
-from agent0.base import Trade
-from agent0.hyperdrive import HyperdriveMarketAction, TradeResult
-from agent0.hyperdrive.agent import add_liquidity_trade, remove_liquidity_trade
+from agent0.core.base import Trade
+from agent0.core.hyperdrive import HyperdriveMarketAction, TradeResult
+from agent0.core.hyperdrive.agent import add_liquidity_trade, remove_liquidity_trade
 
 from .hyperdrive_policy import HyperdriveBasePolicy
 
 if TYPE_CHECKING:
-    from ethpy.hyperdrive import HyperdriveReadInterface
-
-    from agent0.hyperdrive import HyperdriveWallet
+    from agent0.core.hyperdrive import HyperdriveWallet
+    from agent0.ethpy.hyperdrive import HyperdriveReadInterface
 
 
 class SimpleLP(HyperdriveBasePolicy):
