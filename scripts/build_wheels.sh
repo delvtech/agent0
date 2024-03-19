5e8ffca9b42a422577410081843fa60484a02856
Sheng Lundquist <sheng@delv.tech>
2024-03-19 09:57:36 -0700

    Build and upload wheel hotfix. Version bump to 0.16.2 (#1368)
    - Fixing build wheels script.
    - Adding missing requirement for base install.
    - Version bump to 0.16.2 to kick off building wheel.
    - Added correct permissions to pypi for upload

--- scripts/build_wheels.sh
+++ scripts/build_wheels.sh
@@ -3,11 +3,14 @@
 # agent0 install
 
 echo "install required packages for building wheels"
-python -m pip install --upgrade pip uv build
-uv pip install agent0@.[all]
+python -m pip install --upgrade pip uv
+uv venv .venv -p 3.10
+source .venv/bin/activate
+uv pip install 'agent0[all]@.' build
 
 echo "build the wheel for the current platform"
 python -m build
 
 # Move remaining wheels into wheelhouse folder
+mkdir -p wheelhouse/
 mv dist/* wheelhouse/
