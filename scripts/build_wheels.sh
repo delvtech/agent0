835f2568ef4f96c33b3e0f1f46675d7d5f65c65b
Sheng Lundquist <sheng@delv.tech>
2024-03-19 09:35:53 -0700

    Installing build with uv

--- scripts/build_wheels.sh
+++ scripts/build_wheels.sh
@@ -3,10 +3,10 @@
 # agent0 install
 
 echo "install required packages for building wheels"
-python -m pip install --upgrade pip uv build
+python -m pip install --upgrade pip uv
 uv venv .venv -p 3.10
 source .venv/bin/activate
-uv pip install 'agent0[all]@.'
+uv pip install 'agent0[all]@.' build
 
 echo "build the wheel for the current platform"
 python -m build
