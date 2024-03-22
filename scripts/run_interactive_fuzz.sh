a33ae425067d60a7252c3e18b5b37fdb95ebdc86
Dylan <dpaiton@gmail.com>
2024-03-22 15:20:54 -0700

    fix script paths

--- scripts/run_interactive_fuzz.sh
+++ scripts/run_interactive_fuzz.sh
@@ -1,10 +1,10 @@
 # TODO Run these in the background, assuming these will handle how to notify or log errors
 
 echo "HYPERDRIVE BALANCE"
-python lib/agent0/agent0/interactive_fuzz/fuzz_hyperdrive_balance.py;
+python src/agent0/core/interactive_fuzz/fuzz_hyperdrive_balance.py;
 
 echo "PATH INDEPENDENCE"
-python lib/agent0/agent0/interactive_fuzz/fuzz_path_independence.py;
+python src/agent0/core/interactive_fuzz/fuzz_path_independence.py;
 
 echo "PROFIT CHECK"
-python lib/agent0/agent0/interactive_fuzz/fuzz_profit_check.py;
+python src/agent0/core/interactive_fuzz/fuzz_profit_check.py;
