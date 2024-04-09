384f0c7f2288813ee5d897c0d3b2c8481da99d04
Sheng Lundquist <sheng@delv.tech>
2024-04-08 17:56:16 -0700

    Update hotfix (#1396)

--- scripts/run_acquire_data.py
+++ scripts/run_acquire_data.py
@@ -11,5 +11,5 @@ if __name__ == "__main__":
         # This is the start block needed based on the devnet image, which corresponds
         # to the block that the contract was deployed.
         # TODO ideally would gather this from the deployer
-        start_block=45,
+        start_block=48,
     )
