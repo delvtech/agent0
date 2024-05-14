3227ef2142c246ea0503666760e2d413d71da0eb
Sheng Lundquist <sheng@delv.tech>
2024-05-13 17:14:37 -0700

    Updating unit tests for chainsync

--- src/agent0/chainsync/db/hyperdrive/interface_test.py
+++ src/agent0/chainsync/db/hyperdrive/interface_test.py
@@ -6,9 +6,6 @@ from decimal import Decimal
 import numpy as np
 import pytest
 
-from agent0.chainsync.db.base import get_latest_block_number_from_table
-from agent0.ethpy.hyperdrive import BASE_TOKEN_SYMBOL
-
 from .interface import (
     add_checkpoint_info,
     add_pool_config,
@@ -17,7 +14,6 @@ from .interface import (
     get_all_traders,
     get_checkpoint_info,
     get_latest_block_number_from_pool_info_table,
-    get_latest_block_number_from_table,
     get_latest_block_number_from_trade_event,
     get_pool_config,
     get_pool_info,
@@ -238,16 +234,16 @@ class TestPoolInfoInterface:
     def test_latest_block_number(self, db_session):
         """Testing latest block number call"""
         timestamp_1 = datetime.fromtimestamp(1628472000)
-        pool_info_1 = PoolInfo(block_number=1, timestamp=timestamp_1)
+        pool_info_1 = PoolInfo(block_number=1, hyperdrive_address="a", timestamp=timestamp_1)
         add_pool_infos([pool_info_1], db_session)
 
         latest_block_number = get_latest_block_number_from_pool_info_table(db_session)
         assert latest_block_number == 1
 
         timestamp_1 = datetime.fromtimestamp(1628472002)
-        pool_info_1 = PoolInfo(block_number=2, timestamp=timestamp_1)
+        pool_info_1 = PoolInfo(block_number=2, hyperdrive_address="a", timestamp=timestamp_1)
         timestamp_2 = datetime.fromtimestamp(1628472004)
-        pool_info_2 = PoolInfo(block_number=3, timestamp=timestamp_2)
+        pool_info_2 = PoolInfo(block_number=3, hyperdrive_address="a", timestamp=timestamp_2)
         add_pool_infos([pool_info_1, pool_info_2], db_session)
 
         latest_block_number = get_latest_block_number_from_pool_info_table(db_session)
@@ -257,11 +253,11 @@ class TestPoolInfoInterface:
     def test_get_pool_info(self, db_session):
         """Testing retrieval of pool info via interface"""
         timestamp_1 = datetime.fromtimestamp(1628472000)
-        pool_info_1 = PoolInfo(block_number=0, timestamp=timestamp_1)
+        pool_info_1 = PoolInfo(block_number=0, hyperdrive_address="a", timestamp=timestamp_1)
         timestamp_2 = datetime.fromtimestamp(1628472002)
-        pool_info_2 = PoolInfo(block_number=1, timestamp=timestamp_2)
+        pool_info_2 = PoolInfo(block_number=1, hyperdrive_address="a", timestamp=timestamp_2)
         timestamp_3 = datetime.fromtimestamp(1628472004)
-        pool_info_3 = PoolInfo(block_number=2, timestamp=timestamp_3)
+        pool_info_3 = PoolInfo(block_number=2, hyperdrive_address="a", timestamp=timestamp_3)
         add_pool_infos([pool_info_1, pool_info_2, pool_info_3], db_session)
 
         pool_info_df = get_pool_info(db_session)
@@ -274,11 +270,11 @@ class TestPoolInfoInterface:
     def test_block_query_pool_info(self, db_session):
         """Testing retrieval of pool info via interface"""
         timestamp_1 = datetime.fromtimestamp(1628472000)
-        pool_info_1 = PoolInfo(block_number=0, timestamp=timestamp_1)
+        pool_info_1 = PoolInfo(block_number=0, hyperdrive_address="a", timestamp=timestamp_1)
         timestamp_2 = datetime.fromtimestamp(1628472002)
-        pool_info_2 = PoolInfo(block_number=1, timestamp=timestamp_2)
+        pool_info_2 = PoolInfo(block_number=1, hyperdrive_address="a", timestamp=timestamp_2)
         timestamp_3 = datetime.fromtimestamp(1628472004)
-        pool_info_3 = PoolInfo(block_number=2, timestamp=timestamp_3)
+        pool_info_3 = PoolInfo(block_number=2, hyperdrive_address="a", timestamp=timestamp_3)
         add_pool_infos([pool_info_1, pool_info_2, pool_info_3], db_session)
         pool_info_df = get_pool_info(db_session, start_block=1)
         np.testing.assert_array_equal(
@@ -302,132 +298,6 @@ class TestPoolInfoInterface:
         )
 
 
-class TestWalletDeltaInterface:
-    """Testing postgres interface for walletinfo table"""
-
-    @pytest.mark.docker
-    def test_latest_block_number(self, db_session):
-        """Testing retrieval of wallet info via interface"""
-        wallet_delta_1 = WalletDelta(block_number=1, transaction_hash="a", delta=Decimal("3.0"))
-        add_wallet_deltas([wallet_delta_1], db_session)
-        latest_block_number = get_latest_block_number_from_table(WalletDelta, db_session)
-        assert latest_block_number == 1
-        wallet_delta_2 = WalletDelta(block_number=2, transaction_hash="a", delta=Decimal("3.2"))
-        wallet_delta_3 = WalletDelta(block_number=3, transaction_hash="a", delta=Decimal("3.4"))
-        add_wallet_deltas([wallet_delta_2, wallet_delta_3], db_session)
-        latest_block_number = get_latest_block_number_from_table(WalletDelta, db_session)
-        assert latest_block_number == 3
-
-    @pytest.mark.docker
-    def test_get_wallet_delta(self, db_session):
-        """Testing retrievals of walletinfo via interface"""
-        wallet_delta_1 = WalletDelta(block_number=0, transaction_hash="a", delta=Decimal("3.1"))
-        wallet_delta_2 = WalletDelta(block_number=1, transaction_hash="a", delta=Decimal("3.2"))
-        wallet_delta_3 = WalletDelta(block_number=2, transaction_hash="a", delta=Decimal("3.3"))
-        add_wallet_deltas([wallet_delta_1, wallet_delta_2, wallet_delta_3], db_session)
-        # Since we're testing this in isolation without poolinfo, we can't return timestamp here
-        # otherwise no data is returned.
-        wallet_delta_df = get_wallet_deltas(db_session, return_timestamp=False)
-        np.testing.assert_array_equal(wallet_delta_df["delta"], np.array([3.1, 3.2, 3.3]))
-
-    @pytest.mark.docker
-    def test_block_query_wallet_delta(self, db_session):
-        """Testing querying by block number of wallet info via interface"""
-        wallet_delta_1 = WalletDelta(block_number=0, transaction_hash="a", delta=Decimal("3.1"))
-        wallet_delta_2 = WalletDelta(block_number=1, transaction_hash="a", delta=Decimal("3.2"))
-        wallet_delta_3 = WalletDelta(block_number=2, transaction_hash="a", delta=Decimal("3.3"))
-        add_wallet_deltas([wallet_delta_1, wallet_delta_2, wallet_delta_3], db_session)
-        wallet_delta_df = get_wallet_deltas(db_session, start_block=1, return_timestamp=False)
-        np.testing.assert_array_equal(wallet_delta_df["delta"], np.array([3.2, 3.3]))
-        wallet_delta_df = get_wallet_deltas(db_session, start_block=-1, return_timestamp=False)
-        np.testing.assert_array_equal(wallet_delta_df["delta"], np.array([3.3]))
-        wallet_delta_df = get_wallet_deltas(db_session, end_block=1, return_timestamp=False)
-        np.testing.assert_array_equal(wallet_delta_df["delta"], np.array([3.1]))
-        wallet_delta_df = get_wallet_deltas(db_session, end_block=-1, return_timestamp=False)
-        np.testing.assert_array_equal(wallet_delta_df["delta"], np.array([3.1, 3.2]))
-        wallet_delta_df = get_wallet_deltas(db_session, start_block=1, end_block=-1, return_timestamp=False)
-        np.testing.assert_array_equal(wallet_delta_df["delta"], np.array([3.2]))
-
-    @pytest.mark.docker
-    def test_get_agents(self, db_session):
-        """Testing helper function to get current wallet values"""
-        wallet_delta_1 = WalletDelta(block_number=0, transaction_hash="a", wallet_address="addr_1")
-        wallet_delta_2 = WalletDelta(block_number=1, transaction_hash="b", wallet_address="addr_1")
-        wallet_delta_3 = WalletDelta(block_number=2, transaction_hash="c", wallet_address="addr_2")
-        add_wallet_deltas([wallet_delta_1, wallet_delta_2, wallet_delta_3], db_session)
-        agents = get_all_traders(db_session).to_list()
-        assert len(agents) == 2
-        assert "addr_1" in agents
-        assert "addr_2" in agents
-
-
-class TestCurrentWalletInterface:
-    """Testing postgres interface for CurrentWallet table"""
-
-    @pytest.mark.docker
-    def test_latest_block_number(self, db_session):
-        """Testing retrieval of wallet info via interface"""
-        wallet_info_1 = CurrentWallet(block_number=1, value=Decimal("3.0"))
-        add_current_wallet([wallet_info_1], db_session)
-        latest_block_number = get_latest_block_number_from_table(CurrentWallet, db_session)
-        assert latest_block_number == 1
-        wallet_info_2 = CurrentWallet(block_number=2, value=Decimal("3.2"))
-        wallet_info_3 = CurrentWallet(block_number=3, value=Decimal("3.4"))
-        add_current_wallet([wallet_info_2, wallet_info_3], db_session)
-        latest_block_number = get_latest_block_number_from_table(CurrentWallet, db_session)
-        assert latest_block_number == 3
-
-    @pytest.mark.docker
-    def test_get_current_wallet(self, db_session):
-        """Testing retrieval of walletinfo via interface"""
-        wallet_info_1 = CurrentWallet(block_number=0, wallet_address="a", value=Decimal("3.1"))
-        wallet_info_2 = CurrentWallet(block_number=1, wallet_address="b", value=Decimal("3.2"))
-        wallet_info_3 = CurrentWallet(block_number=2, wallet_address="c", value=Decimal("3.3"))
-        add_current_wallet([wallet_info_1, wallet_info_2, wallet_info_3], db_session)
-        wallet_info_df = get_current_wallet(db_session)
-        # Sort by value to make order invariant
-        wallet_info_df = wallet_info_df.sort_values(by=["value"])
-        np.testing.assert_array_equal(wallet_info_df["value"], np.array([3.1, 3.2, 3.3]))
-
-    @pytest.mark.docker
-    def test_block_query_wallet_info(self, db_session):
-        """Testing querying by block number of wallet info via interface"""
-        wallet_info_1 = CurrentWallet(block_number=0, wallet_address="a", value=Decimal("3.1"))
-        wallet_info_2 = CurrentWallet(block_number=1, wallet_address="b", value=Decimal("3.2"))
-        wallet_info_3 = CurrentWallet(block_number=2, wallet_address="c", value=Decimal("3.3"))
-        add_current_wallet([wallet_info_1, wallet_info_2, wallet_info_3], db_session)
-        wallet_info_df = get_current_wallet(db_session, end_block=1)
-        np.testing.assert_array_equal(wallet_info_df["value"], np.array([3.1]))
-        wallet_info_df = get_current_wallet(db_session, end_block=-1)
-        # Sort by value to make order invariant
-        wallet_info_df = wallet_info_df.sort_values(by=["value"])
-        np.testing.assert_array_equal(wallet_info_df["value"], np.array([3.1, 3.2]))
-
-    @pytest.mark.docker
-    def test_current_wallet_info(self, db_session):
-        """Testing helper function to get current wallet values"""
-        wallet_info_1 = CurrentWallet(
-            block_number=0, wallet_address="addr", token_type=BASE_TOKEN_SYMBOL, value=Decimal("3.1")
-        )
-        wallet_info_2 = CurrentWallet(block_number=1, wallet_address="addr", token_type="LP", value=Decimal("5.1"))
-        add_current_wallet([wallet_info_1, wallet_info_2], db_session)
-        wallet_info_df = get_current_wallet(db_session).reset_index()
-        # Sort by value to make order invariant
-        wallet_info_df = wallet_info_df.sort_values(by=["value"])
-        np.testing.assert_array_equal(wallet_info_df["token_type"], [BASE_TOKEN_SYMBOL, "LP"])
-        np.testing.assert_array_equal(wallet_info_df["value"], [3.1, 5.1])
-        # E.g., block 2, wallet base tokens gets updated to 6.1
-        wallet_info_3 = CurrentWallet(
-            block_number=2, wallet_address="addr", token_type=BASE_TOKEN_SYMBOL, value=Decimal("6.1")
-        )
-        add_current_wallet([wallet_info_3], db_session)
-        wallet_info_df = get_current_wallet(db_session).reset_index()
-        # Sort by value to make order invariant
-        wallet_info_df = wallet_info_df.sort_values(by=["value"])
-        np.testing.assert_array_equal(wallet_info_df["token_type"], ["LP", BASE_TOKEN_SYMBOL])
-        np.testing.assert_array_equal(wallet_info_df["value"], [5.1, 6.1])
-
-
 class TestHyperdriveEventsInterface:
     """Testing postgres interface for walletinfo table"""
 
@@ -447,3 +317,21 @@ class TestHyperdriveEventsInterface:
         assert latest_block_number == 2
         latest_block_number = get_latest_block_number_from_trade_event(db_session, "b")
         assert latest_block_number == 3
+
+    @pytest.mark.docker
+    def test_get_agents(self, db_session):
+        """Testing helper function to get current wallet values"""
+        wallet_delta_1 = TradeEvent(
+            block_number=0, hyperdrive_address="a", transaction_hash="a", wallet_address="addr_1"
+        )
+        wallet_delta_2 = TradeEvent(
+            block_number=1, hyperdrive_address="a", transaction_hash="b", wallet_address="addr_1"
+        )
+        wallet_delta_3 = TradeEvent(
+            block_number=2, hyperdrive_address="a", transaction_hash="c", wallet_address="addr_2"
+        )
+        add_trade_events([wallet_delta_1, wallet_delta_2, wallet_delta_3], db_session)
+        agents = get_all_traders(db_session).to_list()
+        assert len(agents) == 2
+        assert "addr_1" in agents
+        assert "addr_2" in agents
--- src/agent0/chainsync/db/hyperdrive/schema_test.py
+++ src/agent0/chainsync/db/hyperdrive/schema_test.py
@@ -4,63 +4,21 @@ from datetime import datetime
 from decimal import Decimal
 
 import pytest
+from sqlalchemy.orm import Session
 
 from .schema import CheckpointInfo, PoolConfig, PoolInfo
 
 # These tests are using fixtures defined in conftest.py
 
 
-class TestTransactionTable:
-    """CRUD tests for transaction table"""
-
-    @pytest.mark.docker
-    def test_create_transaction(self, db_session):
-        """Create and entry"""
-        transaction = HyperdriveTransaction(block_number=1, transaction_hash="a", event_value=Decimal("3.2"))
-        db_session.add(transaction)
-        db_session.commit()
-
-        retrieved_transaction = db_session.query(HyperdriveTransaction).filter_by(block_number=1).first()
-        assert retrieved_transaction is not None
-        # event_value retrieved from postgres is in Decimal, cast to float
-        assert float(retrieved_transaction.event_value) == 3.2
-
-    @pytest.mark.docker
-    def test_update_transaction(self, db_session):
-        """Update an entry"""
-        transaction = HyperdriveTransaction(block_number=1, transaction_hash="a", event_value=Decimal("3.2"))
-        db_session.add(transaction)
-        db_session.commit()
-
-        transaction.event_value = Decimal("5.0")
-        db_session.commit()
-
-        updated_transaction = db_session.query(HyperdriveTransaction).filter_by(block_number=1).first()
-        # event_value retrieved from postgres is in Decimal, cast to float
-        assert float(updated_transaction.event_value) == 5.0
-
-    @pytest.mark.docker
-    def test_delete_transaction(self, db_session):
-        """Delete an entry"""
-        transaction = HyperdriveTransaction(block_number=1, transaction_hash="a", event_value=Decimal("3.2"))
-        db_session.add(transaction)
-        db_session.commit()
-
-        db_session.delete(transaction)
-        db_session.commit()
-
-        deleted_transaction = db_session.query(HyperdriveTransaction).filter_by(block_number=1).first()
-        assert deleted_transaction is None
-
-
 class TestCheckpointTable:
     """CRUD tests for checkpoint table"""
 
     @pytest.mark.docker
-    def test_create_checkpoint(self, db_session):
+    def test_create_checkpoint(self, db_session: Session):
         """Create and entry"""
         vault_share_price = Decimal("1.1")
-        checkpoint = CheckpointInfo(checkpoint_time=1, vault_share_price=vault_share_price)
+        checkpoint = CheckpointInfo(checkpoint_time=1, hyperdrive_address="a", vault_share_price=vault_share_price)
         db_session.add(checkpoint)
         db_session.commit()
 
@@ -69,10 +27,10 @@ class TestCheckpointTable:
         assert retrieved_checkpoint.vault_share_price == vault_share_price
 
     @pytest.mark.docker
-    def test_update_checkpoint(self, db_session):
+    def test_update_checkpoint(self, db_session: Session):
         """Update an entry"""
         vault_share_price = Decimal("1.1")
-        checkpoint = CheckpointInfo(checkpoint_time=1, vault_share_price=vault_share_price)
+        checkpoint = CheckpointInfo(checkpoint_time=1, hyperdrive_address="a", vault_share_price=vault_share_price)
         db_session.add(checkpoint)
         db_session.commit()
 
@@ -80,13 +38,14 @@ class TestCheckpointTable:
         db_session.commit()
 
         updated_checkpoint = db_session.query(CheckpointInfo).filter_by(checkpoint_time=1).first()
+        assert updated_checkpoint is not None
         assert updated_checkpoint.vault_share_price == Decimal("5.0")
 
     @pytest.mark.docker
-    def test_delete_checkpoint(self, db_session):
+    def test_delete_checkpoint(self, db_session: Session):
         """Delete an entry"""
         vault_share_price = Decimal("1.1")
-        checkpoint = CheckpointInfo(checkpoint_time=1, vault_share_price=vault_share_price)
+        checkpoint = CheckpointInfo(checkpoint_time=1, hyperdrive_address="a", vault_share_price=vault_share_price)
         db_session.add(checkpoint)
         db_session.commit()
 
@@ -101,7 +60,7 @@ class TestPoolConfigTable:
     """CRUD tests for poolconfig table"""
 
     @pytest.mark.docker
-    def test_create_pool_config(self, db_session):
+    def test_create_pool_config(self, db_session: Session):
         """Create and entry"""
         pool_config = PoolConfig(hyperdrive_address="0", initial_vault_share_price=Decimal("3.2"))
         db_session.add(pool_config)
@@ -109,10 +68,11 @@ class TestPoolConfigTable:
 
         retrieved_pool_config = db_session.query(PoolConfig).filter_by(hyperdrive_address="0").first()
         assert retrieved_pool_config is not None
+        assert retrieved_pool_config.initial_vault_share_price is not None
         assert float(retrieved_pool_config.initial_vault_share_price) == 3.2
 
     @pytest.mark.docker
-    def test_delete_pool_config(self, db_session):
+    def test_delete_pool_config(self, db_session: Session):
         """Delete an entry"""
         pool_config = PoolConfig(hyperdrive_address="0", initial_vault_share_price=Decimal("3.2"))
         db_session.add(pool_config)
@@ -129,10 +89,10 @@ class TestPoolInfoTable:
     """CRUD tests for poolinfo table"""
 
     @pytest.mark.docker
-    def test_create_pool_info(self, db_session):
+    def test_create_pool_info(self, db_session: Session):
         """Create and entry"""
         timestamp = datetime.fromtimestamp(1628472000)
-        pool_info = PoolInfo(block_number=1, timestamp=timestamp)
+        pool_info = PoolInfo(block_number=1, hyperdrive_address="a", timestamp=timestamp)
         db_session.add(pool_info)
         db_session.commit()
 
@@ -141,10 +101,10 @@ class TestPoolInfoTable:
         assert retrieved_pool_info.timestamp == timestamp
 
     @pytest.mark.docker
-    def test_update_pool_info(self, db_session):
+    def test_update_pool_info(self, db_session: Session):
         """Update an entry"""
         timestamp = datetime.fromtimestamp(1628472000)
-        pool_info = PoolInfo(block_number=1, timestamp=timestamp)
+        pool_info = PoolInfo(block_number=1, hyperdrive_address="a", timestamp=timestamp)
         db_session.add(pool_info)
         db_session.commit()
 
@@ -154,13 +114,14 @@ class TestPoolInfoTable:
         db_session.commit()
 
         updated_pool_info = db_session.query(PoolInfo).filter_by(block_number=1).first()
+        assert updated_pool_info is not None
         assert updated_pool_info.timestamp == new_timestamp
 
     @pytest.mark.docker
-    def test_delete_pool_info(self, db_session):
+    def test_delete_pool_info(self, db_session: Session):
         """Delete an entry"""
         timestamp = datetime.fromtimestamp(1628472000)
-        pool_info = PoolInfo(block_number=1, timestamp=timestamp)
+        pool_info = PoolInfo(block_number=1, hyperdrive_address="a", timestamp=timestamp)
         db_session.add(pool_info)
         db_session.commit()
 
@@ -169,42 +130,3 @@ class TestPoolInfoTable:
 
         deleted_pool_info = db_session.query(PoolInfo).filter_by(block_number=1).first()
         assert deleted_pool_info is None
-
-
-class TestWalletDeltaTable:
-    """CRUD tests for WalletDelta table"""
-
-    @pytest.mark.docker
-    def test_create_wallet_delta(self, db_session):
-        """Create and entry"""
-        wallet_delta = WalletDelta(block_number=1, transaction_hash="a", delta=Decimal("3.2"))
-        db_session.add(wallet_delta)
-        db_session.commit()
-
-        retrieved_wallet_delta = db_session.query(WalletDelta).filter_by(block_number=1).first()
-        assert retrieved_wallet_delta is not None
-        # tokenValue retrieved from postgres is in Decimal, cast to float
-        assert float(retrieved_wallet_delta.delta) == 3.2
-
-    @pytest.mark.docker
-    def test_update_wallet_delta(self, db_session):
-        """Update an entry"""
-        wallet_delta = WalletDelta(block_number=1, transaction_hash="a", delta=Decimal("3.2"))
-        db_session.add(wallet_delta)
-        db_session.commit()
-        wallet_delta.delta = Decimal("5.0")
-        db_session.commit()
-        updated_wallet_delta = db_session.query(WalletDelta).filter_by(block_number=1).first()
-        # delta retrieved from postgres is in Decimal, cast to float
-        assert float(updated_wallet_delta.delta) == 5.0
-
-    @pytest.mark.docker
-    def test_delete_wallet_delta(self, db_session):
-        """Delete an entry"""
-        wallet_delta = WalletDelta(block_number=1, transaction_hash="a", delta=Decimal("3.2"))
-        db_session.add(wallet_delta)
-        db_session.commit()
-        db_session.delete(wallet_delta)
-        db_session.commit()
-        deleted_wallet_delta = db_session.query(WalletDelta).filter_by(block_number=1).first()
-        assert deleted_wallet_delta is None
