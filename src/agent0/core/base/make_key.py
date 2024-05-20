b23d1609c9e6910c319324ab39fa94d244c61bec
Sheng Lundquist <sheng@delv.tech>
2024-05-20 13:54:08 -0700

    Fixing call to parsing private key

--- src/agent0/core/base/make_key.py
+++ src/agent0/core/base/make_key.py
@@ -24,5 +24,5 @@ def make_private_key(extra_entropy: str = "SOME STRING") -> str:
     """
     extra_key_bytes = text_if_str(to_bytes, extra_entropy)
     key_bytes = keccak(os.urandom(32) + extra_key_bytes)
-    key = Account()._parsePrivateKey(key_bytes)  # pylint: disable=protected-access
+    key = Account()._parse_private_key(key_bytes)  # pylint: disable=protected-access
     return str(key)
