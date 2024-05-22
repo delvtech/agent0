1e33d8369d14f4541cc6a7c931517aa1fd1bdaeb
Sheng Lundquist <sheng@delv.tech>
2024-05-22 09:11:32 -0700

    Epsilon should be in scaled value

--- src/agent0/hyperfuzz/system_fuzz/invariant_checks.py
+++ src/agent0/hyperfuzz/system_fuzz/invariant_checks.py
@@ -402,7 +402,7 @@ def _check_initial_lp_profitable(pool_state: PoolState, epsilon: FixedPoint | No
 
     # There's a weird type error for unary operator - for None type,
     # so we do multiplication here
-    if difference_in_wei < (-1 * epsilon):
+    if difference_in_wei < (-1 * epsilon.scaled_value):
         exception_message = f"{lp_rate=} is expected to be >= {vault_rate=}, {difference_in_wei=}"
         exception_data["invariance_check:lp_rate"] = lp_rate
         exception_data["invariance_check:vault_rate"] = vault_rate
