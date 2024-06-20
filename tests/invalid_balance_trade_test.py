97ab6c88583d91fa6a65d02c26dffe544654af60
Sheng Lundquist <sheng@delv.tech>
2024-06-20 13:24:46 -0700

    Unused import

--- tests/invalid_balance_trade_test.py
+++ tests/invalid_balance_trade_test.py
@@ -7,7 +7,7 @@ from typing import TYPE_CHECKING
 import pytest
 from fixedpointmath import FixedPoint
 from utils import expect_failure_with_funded_bot, expect_failure_with_non_funded_bot
-from web3.exceptions import ContractCustomError, ContractLogicError, ContractPanicError
+from web3.exceptions import ContractCustomError, ContractPanicError
 
 from agent0.core.base import Trade
 from agent0.core.hyperdrive import HyperdriveMarketAction, HyperdriveWallet
