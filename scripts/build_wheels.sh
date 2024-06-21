443f16361c4189d999f7ecd3c7f4b009d12c3828
Dylan Paiton <dpaiton@users.noreply.github.com>
2024-06-20 17:52:31 -0700

    v0.21.3; fix gh release (#1554)

--- scripts/build_wheels.sh
+++ scripts/build_wheels.sh
@@ -10,10 +10,3 @@ pip install '.[all]' build
 
 echo "build the wheel for the current platform"
 python -m build
-
-# Export dependency versions used to build the wheel
-FREEZE_FILE="dist/frozen-requirements.txt"
-timestamp="$(date)"
-version="$(git describe --abbrev=12 --always)"
-echo -e "# Generated at $timestamp ($version)\n" > $FREEZE_FILE
-pip3 freeze | tee -a $FREEZE_FILE
