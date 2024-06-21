88e09bf46c9c0b56dd19444da84faa5d8fc46a41
Dylan <dpaiton@gmail.com>
2024-06-20 17:44:21 -0700

    v0.21.3; fix gh release

---
+++ scripts/generate_deps.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# agent0 install
+echo "install required packages for building wheels"
+python -m pip install --upgrade pip
+python -m venv --upgrade-deps .venv
+source .venv/bin/activate
+pip install agent0
+
+# Export dependency versions used to build the wheel
+FREEZE_FILE="dist/frozen-requirements.txt"
+timestamp="$(date)"
+version="$(git describe --abbrev=12 --always)"
+echo -e "# Generated at $timestamp ($version)\n" > $FREEZE_FILE
+pip3 freeze | tee -a $FREEZE_FILE
