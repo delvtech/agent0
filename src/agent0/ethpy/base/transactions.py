6c2e504f2466b207e337526a2af4c1b5b3e8a9fb
Sheng Lundquist <sheng@delv.tech>
2024-08-22 14:06:07 -0700

    Fixing hex conversion of value when 0 (#1664)

--- src/agent0/ethpy/base/transactions.py
+++ src/agent0/ethpy/base/transactions.py
@@ -482,6 +482,7 @@ def build_transaction(
         # TODO new web3 version gas estimation seems to underestimate gas.
         # We explicitly do an rpc call to estimate gas here to get an accurate gas estimate.
         # RPC call to estimate gas
+
         result = web3.provider.make_request(
             method=RPCEndpoint("eth_estimateGas"),
             params=[
@@ -489,7 +490,7 @@ def build_transaction(
                     "from": str(raw_txn["from"]),
                     "to": str(raw_txn["to"]),
                     # Convert integer to hex string
-                    "value": HexBytes(raw_txn["value"]).hex(),
+                    "value": hex(raw_txn["value"]),
                     "data": str(raw_txn["data"]),
                 }
             ],
