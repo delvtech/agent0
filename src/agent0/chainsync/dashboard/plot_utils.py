9735657d7d3ecbb1360a5bf7f9a01ed585eac4b5
Sheng Lundquist <sheng@delv.tech>
2024-09-10 15:20:11 -0700

    Reducing plot data keeps both points around a change (#1678)

--- src/agent0/chainsync/dashboard/plot_utils.py
+++ src/agent0/chainsync/dashboard/plot_utils.py
@@ -28,16 +28,13 @@ def reduce_plot_data(data: pd.DataFrame, x_column_name: str, y_column_name: str)
         return plot_data
 
     # Filter out rows with no change
-    plot_data_diff = plot_data[y_column_name].diff()
 
-    # We always keep the first and last rows
-    # Diff always puts the first row as nan,
-    # we explicitly set the last row as nan
-    if len(plot_data_diff) > 0:
-        # pandas doesn't play nice with types
-        plot_data_diff.iloc[-1] = float("nan")  # type: ignore
+    # We want both points before and after the change
+    # so we look at the diffs in both directions, and plot it
+    # if either one is not 0
+    data_diff = plot_data[y_column_name].diff()
+    reverse_data_diff = plot_data[y_column_name][::-1].diff()[::-1]
 
     # Filter out intermediate rows with no difference
-    # Note that nans will always not equal 0
-    plot_data_idx = plot_data_diff != 0
+    plot_data_idx = (data_diff != 0) | (reverse_data_diff != 0)
     return plot_data[plot_data_idx]
