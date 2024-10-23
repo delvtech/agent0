0c8bd34a325bea2fa3672cf828da783c8cfe1e86
Sheng Lundquist <sheng@delv.tech>
2024-10-23 10:59:03 -0700

    Check invariance on every iteration in fuzz bots

--- src/agent0/hyperfuzz/system_fuzz/run_fuzz_bots.py
+++ src/agent0/hyperfuzz/system_fuzz/run_fuzz_bots.py
@@ -321,8 +321,8 @@ def run_fuzz_bots(
 
                 trades.append(agent_trade)
 
-                # Check invariance after every trade, only if a trade has been made
-                if check_invariance and len(agent_trade) > 0:
+                # Check invariance on every iteration
+                if check_invariance:
                     latest_block = pool.interface.get_block("latest")
                     latest_block_number = latest_block.get("number", None)
                     if latest_block_number is None:
