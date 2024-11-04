5e33dddac81840b52c369e61e8afb9191ca5728f
Dylan <dpaiton@gmail.com>
2024-11-04 13:49:24 -0600

    iterate on algorithm

--- src/agent0/utils/block_before_timestamp.py
+++ src/agent0/utils/block_before_timestamp.py
@@ -18,36 +18,47 @@ def block_before_timestamp(web3: Web3, block_timestamp: Timestamp | int) -> Bloc
     BlockNumber
       The closest block number to the given block time.
     """
-    # Use a binary search to match the block time to the block number.
+    # Get the current block number and timestamp
     current_block = web3.eth.get_block("latest")
     current_block_number = current_block.get("number", None)
     assert current_block_number is not None
+    if current_block_number < 3:
+        raise ValueError("The current block number must be >= 2.")
     current_block_timestamp = current_block.get("timestamp", None)
     assert current_block_timestamp is not None
 
     # Estimate the average block time
     earlier_block_number = current_block_number // 2
-    blocks_skipped = current_block_number - earlier_block_number
-    assert blocks_skipped > 0
-    earlier_blcok_timestamp = web3.eth.get_block(earlier_block_number).get("timestamp", None)
-    assert earlier_blcok_timestamp is not None
-    avg_time_between_blocks = (current_block_timestamp - earlier_blcok_timestamp) / blocks_skipped
+    earlier_block_delta = current_block_number - earlier_block_number
+    if earlier_block_delta <= 0:
+        raise ValueError("Error estimating the delta blocks.")
+    earlier_block_timestamp = web3.eth.get_block(earlier_block_number).get("timestamp", None)
+    assert earlier_block_timestamp is not None
+    avg_time_between_blocks = (current_block_timestamp - earlier_block_timestamp) / earlier_block_delta
 
-    # How far back the user wants to search
+    # Estimate the block number corresponding to the user provided timestamp
     delta_time = current_block_timestamp - block_timestamp
     estimated_block_number = current_block_number - (delta_time // avg_time_between_blocks)
 
     # Use a binary search to find the block
     left = int(max(0, estimated_block_number - 100))  # search 100 blocks before estimated block
     right = int(min(current_block_number, estimated_block_number + 100))  # search 100 blocks after estimated block
-    while left <= right:
+    while left < right:
         mid = int((left + right) // 2)
         block = web3.eth.get_block(BlockNumber(mid))
         search_block_timestamp = block.get("timestamp", None)
         assert search_block_timestamp is not None
-        time_error = search_block_timestamp - block_timestamp
-        if time_error >= 0:
+        if search_block_timestamp > block_timestamp:
+            # The mid point is later than the block we want
+            # The user could enter a time that is greater than the nearest block
+            # timestamp but less than the next block timestamp. Therefore, set
+            # upper bound to mid.
+            right = mid
+        elif search_block_timestamp < block_timestamp:
+            # The mid point is earlier than the block we want, set lower bound
+            # to mid + 1
             left = mid + 1
         else:
-            right = mid - 1
+            # The mid point is the right block
+            return BlockNumber(mid)
     return BlockNumber(left)
--- src/agent0/utils/block_before_timestamp_test.py
+++ src/agent0/utils/block_before_timestamp_test.py
@@ -12,27 +12,58 @@ def test_block_before_timestamp(fast_chain_fixture: LocalChain):
     """Test that ensures block_before_timestamp always returns a block number that is closest to but before the timestamp."""
 
     # Define test constants
-    MAX_BLOCK_DECREMENT = 30
     NUM_FUZZ_RUNS = 10_000
     TOLERANCE = 1
 
-    # Advance time to get some blocks
-    fast_chain_fixture.mine_blocks(NUM_FUZZ_RUNS * MAX_BLOCK_DECREMENT)
-    fast_chain_fixture.block_number()
+    # Advance the chain NUM_FUZZ_RUNS blocks
+    initial_block_number = fast_chain_fixture.block_number()
+    initial_block_time = fast_chain_fixture.block_number()
+
+    print(f"{initial_block_number=}")
+    print(f"{initial_block_time=}")
+
+    fast_chain_fixture.mine_blocks(1)
+    initial_block_plus_one_time = fast_chain_fixture.block_time()
+
+    print(f"{initial_block_plus_one_time=}")
+
+    time_between_blocks = initial_block_plus_one_time - initial_block_time
+    assert time_between_blocks >= 1
+    fast_chain_fixture.mine_blocks(NUM_FUZZ_RUNS - 1)
+    chain_block_number = fast_chain_fixture.block_number()
+    chain_block_time = fast_chain_fixture.block_time()
+
+    print(f"{chain_block_number=}")
+    print(f"{chain_block_time=}")
+    print(f"{time_between_blocks=}")
 
     # Start out a few blocks behind the latest
-    test_block_number = fast_chain_fixture.block_number() - 3
-    assert test_block_number > 0
     hyperdrive_interface = LocalHyperdrive(fast_chain_fixture, LocalHyperdrive.Config()).interface
     for _ in range(NUM_FUZZ_RUNS):
-        block_time = hyperdrive_interface.get_block_timestamp(hyperdrive_interface.get_block(test_block_number))
-        approx_block = block_before_timestamp(hyperdrive_interface.web3, block_time)
+        # Grab a random block in the past & get the time
+        test_block_number = int(np.random.randint(initial_block_number + 2, chain_block_number - 1))
+        test_block_time = hyperdrive_interface.get_block_timestamp(hyperdrive_interface.get_block(test_block_number))
+
+        print(f"{test_block_number=}")
+        print(f"{test_block_time=}")
+
+        # Add a random amount of time that is less than the time between blocks
+        time_delta = int(np.random.randint(0, time_between_blocks - 1))
+
+        print(f"{time_delta=}")
+
+        # Find the block that was closest to this timestamp
+        inferred_block = block_before_timestamp(hyperdrive_interface.web3, test_block_time + time_delta)
+        inferred_block_time = hyperdrive_interface.get_block_timestamp(hyperdrive_interface.get_block(inferred_block))
+
+        print(f"{inferred_block=}")
+        print(f"{inferred_block_time=}")
 
         assert (
-            approx_block <= test_block_number
-        ), "Approximate block number should be less than or equal to test block number."
+            inferred_block <= test_block_number
+        ), f"Inferred block number = {inferred_block} should be less than or equal to test block number = {test_block_number}."
         assert (
-            test_block_number - approx_block <= TOLERANCE
-        ), f"Approximate block number should be within tolerance={TOLERANCE} of test block number."
+            test_block_number - inferred_block <= TOLERANCE
+        ), f"Inferred block number should be within tolerance={TOLERANCE} of test block number."
 
         test_block_number -= np.random.randint(1, 30)
