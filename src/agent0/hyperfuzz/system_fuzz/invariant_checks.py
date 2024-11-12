e25c6caa0cb3ecfba3d3074bb05ca5409ac8492c
Dylan <dpaiton@gmail.com>
2024-11-12 07:04:42 +0700

    turn assert into runtime check

--- src/agent0/hyperfuzz/system_fuzz/invariant_checks.py
+++ src/agent0/hyperfuzz/system_fuzz/invariant_checks.py
@@ -288,7 +288,8 @@ def _check_negative_interest(interface: HyperdriveReadInterface, pool_state: Poo
     current_vault_share_price = pool_state.pool_info.vault_share_price
 
     deploy_block = interface.get_deploy_block()
-    assert deploy_block is not None  # type narrowing
+    if deploy_block is None:  # type narrowing
+        raise ValueError("Deploy block not found.")
     earliest_block_time = interface.get_block_timestamp(interface.get_block(deploy_block))
     if interface.hyperdrive_name == "ElementDAO 182 Day ezETH Hyperdrive":
         lookback_timestamp = max(earliest_block_time, current_block_time - 60 * 60 * 12)  # 12 hours ago
